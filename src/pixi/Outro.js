21e45171aecb9962ecc8b6c3a991c34fa6b6b257
Chad Engler <englercj@live.com>
2013-08-18 16:47:00 -0700

    Add AMD export, fixes #267

--- src/pixi/Outro.js
+++ src/pixi/Outro.js
@@ -2,14 +2,14 @@
  * @author Mat Groves http://matgroves.com/ @Doormat23
  */
 
- if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports) {
-      exports = module.exports = PIXI;
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = PIXI;
+        }
+        exports.PIXI = PIXI;
+    } else if (typeof define !== 'undefined' && define.amd) {
+        define(PIXI);
+    } else {
+        root.PIXI = PIXI;
     }
-    exports.PIXI = PIXI;
-  } else {
-    root.PIXI = PIXI;
-  }
-
-
 }).call(this);
