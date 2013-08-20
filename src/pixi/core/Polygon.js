fbc088afd1f08de8a71253714640815d46a76cc1
G. Bodenschatz <coding@46halbe.de>
2013-08-20 23:08:25 +0200

    [DOC] Small changes
    - fix typo in comment
    - add comment for constructor assignment (as in Point, Rectangle ...)

--- src/pixi/core/Polygon.js
+++ src/pixi/core/Polygon.js
@@ -6,7 +6,7 @@
  * @class Polygon
  * @constructor
  * @param points* {Array<Point>|Array<Number>|Point...|Number...} This can be an array of Points that form the polygon,
- *      a flat array of numbers that will be interpreted as [x,y, x,y, ...], or the arugments passed can be
+ *      a flat array of numbers that will be interpreted as [x,y, x,y, ...], or the arguments passed can be
  *      all the points of the polygon e.g. `new PIXI.Polygon(new PIXI.Point(), new PIXI.Point(), ...)`, or the
  *      arguments passed can be flat x,y values e.g. `new PIXI.Polygon(x,y, x,y, x,y, ...)` where `x` and `y` are
  *      Numbers.
@@ -73,5 +73,5 @@ PIXI.Polygon.prototype.contains = function(x, y)
     return inside;
 }
 
+// constructor
 PIXI.Polygon.prototype.constructor = PIXI.Polygon;
-
