drkibitz/node-pixi

Squashed commit of the following (jshint):
commit 09d68fb83686b82a2cbac0d99abcc7e0899d6724
Author: Dr. Kibitz <info@drkibitz.com>
Date:   Tue Sep 10 13:14:22 2013 -0700

    ignore W069 just in 4 methods

commit 931afab48f374194a808bedac4e31dd9c20d2ae4
Author: Dr. Kibitz <info@drkibitz.com>
Date:   Tue Sep 10 13:14:03 2013 -0700

    fix for warning W055

commit 43ad62a89a1c83e8da85dd9f9bb6e8a7c0acd59d
Author: Dr. Kibitz <info@drkibitz.com>
Date:   Tue Sep 10 13:13:46 2013 -0700

    rename HEXtoRGB to hex2rgb

commit adc2a1e83823061709dbf1d4b037bebeddc7281c
Author: Dr. Kibitz <info@drkibitz.com>
Date:   Tue Sep 10 13:13:02 2013 -0700

    Changed some jshint settings

commit 0d8eb1ef71332b076a0f9bd565633c92395ed5ec
Author: Dr. Kibitz <info@drkibitz.com>
Date:   Sun Sep 1 16:16:53 2013 -0700

    unittest-refactor branch compatible jshint setup

commit 3e91e79dab7c34dbb60bf364124d2ef7df93ec6d
Author: Dr. Kibitz <info@drkibitz.com>
Date:   Sun Sep 1 11:15:34 2013 -0700

    Final progress, jshint passed

commit c0f3750a06616195cf76149091dce4a1da7717ab
Author: Dr. Kibitz <info@drkibitz.com>
Date:   Sun Sep 1 10:31:36 2013 -0700

    More progress

commit b2118949bb47cbeffaf53d2cb1a978c8f9ffdf49
Author: Dr. Kibitz <info@drkibitz.com>
Date:   Sun Sep 1 02:38:10 2013 -0700

    jshint progress

Conflicts:
	Gruntfile.js
	src/pixi/InteractionManager.js

src/pixi/textures/RenderTexture.js
@@ -10,19 +10,19 @@
 
  RenderTexture takes snapshot of DisplayObject passed to render method. If DisplayObject is passed to render method, position and rotation of it will be ignored. For example:
 
-	var renderTexture = new PIXI.RenderTexture(800, 600);
-	var sprite = PIXI.Sprite.fromImage("spinObj_01.png");
-	sprite.position.x = 800/2;
-	sprite.position.y = 600/2;
-	sprite.anchor.x = 0.5;
-	sprite.anchor.y = 0.5;
-	renderTexture.render(sprite);
+    var renderTexture = new PIXI.RenderTexture(800, 600);
+    var sprite = PIXI.Sprite.fromImage("spinObj_01.png");
+    sprite.position.x = 800/2;
+    sprite.position.y = 600/2;
+    sprite.anchor.x = 0.5;
+    sprite.anchor.y = 0.5;
+    renderTexture.render(sprite);
 
  Sprite in this case will be rendered to 0,0 position. To render this sprite at center DisplayObjectContainer should be used:
 
-	var doc = new PIXI.DisplayObjectContainer();
-	doc.addChild(sprite);
-	renderTexture.render(doc);  // Renders to center of renderTexture
+    var doc = new PIXI.DisplayObjectContainer();
+    doc.addChild(sprite);
+    renderTexture.render(doc);  // Renders to center of renderTexture
 
  @class RenderTexture
  @extends Texture
@@ -32,23 +32,23 @@
  */
 PIXI.RenderTexture = function(width, height)
 {
-	PIXI.EventTarget.call( this );
+    PIXI.EventTarget.call( this );
 
-	this.width = width || 100;
-	this.height = height || 100;
+    this.width = width || 100;
+    this.height = height || 100;
 
-	this.indetityMatrix = PIXI.mat3.create();
+    this.indetityMatrix = PIXI.mat3.create();
 
-	this.frame = new PIXI.Rectangle(0, 0, this.width, this.height);
+    this.frame = new PIXI.Rectangle(0, 0, this.width, this.height);
 
-	if(PIXI.gl)
-	{
-		this.initWebGL();
-	}
-	else
-	{
-		this.initCanvas();
-	}
+    if(PIXI.gl)
+    {
+        this.initWebGL();
+    }
+    else
+    {
+        this.initCanvas();
+    }
 }
 
 PIXI.RenderTexture.prototype = Object.create( PIXI.Texture.prototype );
@@ -62,39 +62,39 @@ PIXI.RenderTexture.prototype.constructor = PIXI.RenderTexture;
  */
 PIXI.RenderTexture.prototype.initWebGL = function()
 {
-	var gl = PIXI.gl;
-	this.glFramebuffer = gl.createFramebuffer();
+    var gl = PIXI.gl;
+    this.glFramebuffer = gl.createFramebuffer();
 
-   	gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
 
     this.glFramebuffer.width = this.width;
     this.glFramebuffer.height = this.height;
 
-	this.baseTexture = new PIXI.BaseTexture();
+    this.baseTexture = new PIXI.BaseTexture();
 
-	this.baseTexture.width = this.width;
-	this.baseTexture.height = this.height;
+    this.baseTexture.width = this.width;
+    this.baseTexture.height = this.height;
 
     this.baseTexture._glTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, this.baseTexture._glTexture);
 
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,  this.width,  this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,  this.width,  this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
 
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-	this.baseTexture.isRender = true;
+    this.baseTexture.isRender = true;
 
-	gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
-	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.baseTexture._glTexture, 0);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.baseTexture._glTexture, 0);
 
-	// create a projection matrix..
-	this.projection = new PIXI.Point(this.width/2 , this.height/2);
+    // create a projection matrix..
+    this.projection = new PIXI.Point(this.width/2 , this.height/2);
 
-	// set the correct render function..
-	this.render = this.renderWebGL;
+    // set the correct render function..
+    this.render = this.renderWebGL;
 
 
 }
@@ -103,25 +103,25 @@ PIXI.RenderTexture.prototype.initWebGL = function()
 PIXI.RenderTexture.prototype.resize = function(width, height)
 {
 
-	this.width = width;
-	this.height = height;
+    this.width = width;
+    this.height = height;
 
-	if(PIXI.gl)
-	{
-		this.projection.x = this.width/2
-		this.projection.y = this.height/2;
+    if(PIXI.gl)
+    {
+        this.projection.x = this.width/2
+        this.projection.y = this.height/2;
 
-		var gl = PIXI.gl;
-		gl.bindTexture(gl.TEXTURE_2D, this.baseTexture._glTexture);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,  this.width,  this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-	}
-	else
-	{
+        var gl = PIXI.gl;
+        gl.bindTexture(gl.TEXTURE_2D, this.baseTexture._glTexture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,  this.width,  this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    }
+    else
+    {
 
-		this.frame.width = this.width
-		this.frame.height = this.height;
-		this.renderer.resize(this.width, this.height);
-	}
+        this.frame.width = this.width
+        this.frame.height = this.height;
+        this.renderer.resize(this.width, this.height);
+    }
 }
 
 /**
@@ -132,12 +132,12 @@ PIXI.RenderTexture.prototype.resize = function(width, height)
  */
 PIXI.RenderTexture.prototype.initCanvas = function()
 {
-	this.renderer = new PIXI.CanvasRenderer(this.width, this.height, null, 0);
+    this.renderer = new PIXI.CanvasRenderer(this.width, this.height, null, 0);
 
-	this.baseTexture = new PIXI.BaseTexture(this.renderer.view);
-	this.frame = new PIXI.Rectangle(0, 0, this.width, this.height);
+    this.baseTexture = new PIXI.BaseTexture(this.renderer.view);
+    this.frame = new PIXI.Rectangle(0, 0, this.width, this.height);
 
-	this.render = this.renderCanvas;
+    this.render = this.renderCanvas;
 }
 
 /**
@@ -150,67 +150,67 @@ PIXI.RenderTexture.prototype.initCanvas = function()
  */
 PIXI.RenderTexture.prototype.renderWebGL = function(displayObject, position, clear)
 {
-	var gl = PIXI.gl;
+    var gl = PIXI.gl;
 
-	// enable the alpha color mask..
-	gl.colorMask(true, true, true, true);
+    // enable the alpha color mask..
+    gl.colorMask(true, true, true, true);
 
-	gl.viewport(0, 0, this.width, this.height);
+    gl.viewport(0, 0, this.width, this.height);
 
-	gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
 
-	if(clear)
-	{
-		gl.clearColor(0,0,0, 0);
-		gl.clear(gl.COLOR_BUFFER_BIT);
-	}
+    if(clear)
+    {
+        gl.clearColor(0,0,0, 0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    }
 
-	// THIS WILL MESS WITH HIT TESTING!
-	var children = displayObject.children;
+    // THIS WILL MESS WITH HIT TESTING!
+    var children = displayObject.children;
 
-	//TODO -? create a new one??? dont think so!
-	var originalWorldTransform = displayObject.worldTransform;
-	displayObject.worldTransform = PIXI.mat3.create();//sthis.indetityMatrix;
-	// modify to flip...
-	displayObject.worldTransform[4] = -1;
-	displayObject.worldTransform[5] = this.projection.y * 2;
+    //TODO -? create a new one??? dont think so!
+    var originalWorldTransform = displayObject.worldTransform;
+    displayObject.worldTransform = PIXI.mat3.create();//sthis.indetityMatrix;
+    // modify to flip...
+    displayObject.worldTransform[4] = -1;
+    displayObject.worldTransform[5] = this.projection.y * 2;
 
 
-	if(position)
-	{
-		displayObject.worldTransform[2] = position.x;
-		displayObject.worldTransform[5] -= position.y;
-	}
+    if(position)
+    {
+        displayObject.worldTransform[2] = position.x;
+        displayObject.worldTransform[5] -= position.y;
+    }
 
-	PIXI.visibleCount++;
-	displayObject.vcount = PIXI.visibleCount;
+    PIXI.visibleCount++;
+    displayObject.vcount = PIXI.visibleCount;
 
-	for(var i=0,j=children.length; i<j; i++)
-	{
-		children[i].updateTransform();
-	}
+    for(var i=0,j=children.length; i<j; i++)
+    {
+        children[i].updateTransform();
+    }
 
-	var renderGroup = displayObject.__renderGroup;
+    var renderGroup = displayObject.__renderGroup;
 
-	if(renderGroup)
-	{
-		if(displayObject == renderGroup.root)
-		{
-			renderGroup.render(this.projection);
-		}
-		else
-		{
-			renderGroup.renderSpecific(displayObject, this.projection);
-		}
-	}
-	else
-	{
-		if(!this.renderGroup)this.renderGroup = new PIXI.WebGLRenderGroup(gl);
-		this.renderGroup.setRenderable(displayObject);
-		this.renderGroup.render(this.projection);
-	}
+    if(renderGroup)
+    {
+        if(displayObject == renderGroup.root)
+        {
+            renderGroup.render(this.projection);
+        }
+        else
+        {
+            renderGroup.renderSpecific(displayObject, this.projection);
+        }
+    }
+    else
+    {
+        if(!this.renderGroup)this.renderGroup = new PIXI.WebGLRenderGroup(gl);
+        this.renderGroup.setRenderable(displayObject);
+        this.renderGroup.render(this.projection);
+    }
 
-	displayObject.worldTransform = originalWorldTransform;
+    displayObject.worldTransform = originalWorldTransform;
 }
 
 
@@ -224,23 +224,23 @@ PIXI.RenderTexture.prototype.renderWebGL = function(displayObject, position, cle
  */
 PIXI.RenderTexture.prototype.renderCanvas = function(displayObject, position, clear)
 {
-	var children = displayObject.children;
+    var children = displayObject.children;
 
-	displayObject.worldTransform = PIXI.mat3.create();
+    displayObject.worldTransform = PIXI.mat3.create();
 
-	if(position)
-	{
-		displayObject.worldTransform[2] = position.x;
-		displayObject.worldTransform[5] = position.y;
-	}
+    if(position)
+    {
+        displayObject.worldTransform[2] = position.x;
+        displayObject.worldTransform[5] = position.y;
+    }
 
 
-	for(var i=0,j=children.length; i<j; i++)
-	{
-		children[i].updateTransform();
-	}
+    for(var i=0,j=children.length; i<j; i++)
+    {
+        children[i].updateTransform();
+    }
 
-	if(clear)this.renderer.context.clearRect(0,0, this.width, this.height);
+    if(clear)this.renderer.context.clearRect(0,0, this.width, this.height);
 
     this.renderer.renderDisplayObject(displayObject);
 

pixijs/pixijs

src/pixi/textures/RenderTexture.js
@@ -10,19 +10,19 @@
 
  RenderTexture takes snapshot of DisplayObject passed to render method. If DisplayObject is passed to render method, position and rotation of it will be ignored. For example:
 
-	var renderTexture = new PIXI.RenderTexture(800, 600);
-	var sprite = PIXI.Sprite.fromImage("spinObj_01.png");
-	sprite.position.x = 800/2;
-	sprite.position.y = 600/2;
-	sprite.anchor.x = 0.5;
-	sprite.anchor.y = 0.5;
-	renderTexture.render(sprite);
+    var renderTexture = new PIXI.RenderTexture(800, 600);
+    var sprite = PIXI.Sprite.fromImage("spinObj_01.png");
+    sprite.position.x = 800/2;
+    sprite.position.y = 600/2;
+    sprite.anchor.x = 0.5;
+    sprite.anchor.y = 0.5;
+    renderTexture.render(sprite);
 
  Sprite in this case will be rendered to 0,0 position. To render this sprite at center DisplayObjectContainer should be used:
 
-	var doc = new PIXI.DisplayObjectContainer();
-	doc.addChild(sprite);
-	renderTexture.render(doc);  // Renders to center of renderTexture
+    var doc = new PIXI.DisplayObjectContainer();
+    doc.addChild(sprite);
+    renderTexture.render(doc);  // Renders to center of renderTexture
 
  @class RenderTexture
  @extends Texture
@@ -32,23 +32,23 @@
  */
 PIXI.RenderTexture = function(width, height)
 {
-	PIXI.EventTarget.call( this );
+    PIXI.EventTarget.call( this );
 
-	this.width = width || 100;
-	this.height = height || 100;
+    this.width = width || 100;
+    this.height = height || 100;
 
-	this.identityMatrix = PIXI.mat3.create();
+    this.identityMatrix = PIXI.mat3.create();
 
-	this.frame = new PIXI.Rectangle(0, 0, this.width, this.height);
+    this.frame = new PIXI.Rectangle(0, 0, this.width, this.height);
 
-	if(PIXI.gl)
-	{
-		this.initWebGL();
-	}
-	else
-	{
-		this.initCanvas();
-	}
+    if(PIXI.gl)
+    {
+        this.initWebGL();
+    }
+    else
+    {
+        this.initCanvas();
+    }
 }
 
 PIXI.RenderTexture.prototype = Object.create( PIXI.Texture.prototype );
@@ -62,39 +62,39 @@ PIXI.RenderTexture.prototype.constructor = PIXI.RenderTexture;
  */
 PIXI.RenderTexture.prototype.initWebGL = function()
 {
-	var gl = PIXI.gl;
-	this.glFramebuffer = gl.createFramebuffer();
+    var gl = PIXI.gl;
+    this.glFramebuffer = gl.createFramebuffer();
 
-   	gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
 
     this.glFramebuffer.width = this.width;
     this.glFramebuffer.height = this.height;
 
-	this.baseTexture = new PIXI.BaseTexture();
+    this.baseTexture = new PIXI.BaseTexture();
 
-	this.baseTexture.width = this.width;
-	this.baseTexture.height = this.height;
+    this.baseTexture.width = this.width;
+    this.baseTexture.height = this.height;
 
     this.baseTexture._glTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, this.baseTexture._glTexture);
 
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,  this.width,  this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,  this.width,  this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
 
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-	this.baseTexture.isRender = true;
+    this.baseTexture.isRender = true;
 
-	gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
-	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.baseTexture._glTexture, 0);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.baseTexture._glTexture, 0);
 
-	// create a projection matrix..
-	this.projection = new PIXI.Point(this.width/2 , this.height/2);
+    // create a projection matrix..
+    this.projection = new PIXI.Point(this.width/2 , this.height/2);
 
-	// set the correct render function..
-	this.render = this.renderWebGL;
+    // set the correct render function..
+    this.render = this.renderWebGL;
 
 
 }
@@ -103,25 +103,25 @@ PIXI.RenderTexture.prototype.initWebGL = function()
 PIXI.RenderTexture.prototype.resize = function(width, height)
 {
 
-	this.width = width;
-	this.height = height;
+    this.width = width;
+    this.height = height;
 
-	if(PIXI.gl)
-	{
-		this.projection.x = this.width/2
-		this.projection.y = this.height/2;
+    if(PIXI.gl)
+    {
+        this.projection.x = this.width/2
+        this.projection.y = this.height/2;
 
-		var gl = PIXI.gl;
-		gl.bindTexture(gl.TEXTURE_2D, this.baseTexture._glTexture);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,  this.width,  this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-	}
-	else
-	{
+        var gl = PIXI.gl;
+        gl.bindTexture(gl.TEXTURE_2D, this.baseTexture._glTexture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,  this.width,  this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    }
+    else
+    {
 
-		this.frame.width = this.width
-		this.frame.height = this.height;
-		this.renderer.resize(this.width, this.height);
-	}
+        this.frame.width = this.width
+        this.frame.height = this.height;
+        this.renderer.resize(this.width, this.height);
+    }
 }
 
 /**
@@ -132,12 +132,12 @@ PIXI.RenderTexture.prototype.resize = function(width, height)
  */
 PIXI.RenderTexture.prototype.initCanvas = function()
 {
-	this.renderer = new PIXI.CanvasRenderer(this.width, this.height, null, 0);
+    this.renderer = new PIXI.CanvasRenderer(this.width, this.height, null, 0);
 
-	this.baseTexture = new PIXI.BaseTexture(this.renderer.view);
-	this.frame = new PIXI.Rectangle(0, 0, this.width, this.height);
+    this.baseTexture = new PIXI.BaseTexture(this.renderer.view);
+    this.frame = new PIXI.Rectangle(0, 0, this.width, this.height);
 
-	this.render = this.renderCanvas;
+    this.render = this.renderCanvas;
 }
 
 /**
@@ -150,67 +150,67 @@ PIXI.RenderTexture.prototype.initCanvas = function()
  */
 PIXI.RenderTexture.prototype.renderWebGL = function(displayObject, position, clear)
 {
-	var gl = PIXI.gl;
+    var gl = PIXI.gl;
 
-	// enable the alpha color mask..
-	gl.colorMask(true, true, true, true);
+    // enable the alpha color mask..
+    gl.colorMask(true, true, true, true);
 
-	gl.viewport(0, 0, this.width, this.height);
+    gl.viewport(0, 0, this.width, this.height);
 
-	gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.glFramebuffer );
 
-	if(clear)
-	{
-		gl.clearColor(0,0,0, 0);
-		gl.clear(gl.COLOR_BUFFER_BIT);
-	}
+    if(clear)
+    {
+        gl.clearColor(0,0,0, 0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    }
 
-	// THIS WILL MESS WITH HIT TESTING!
-	var children = displayObject.children;
+    // THIS WILL MESS WITH HIT TESTING!
+    var children = displayObject.children;
 
-	//TODO -? create a new one??? dont think so!
-	var originalWorldTransform = displayObject.worldTransform;
-	displayObject.worldTransform = PIXI.mat3.create();//sthis.identityMatrix;
-	// modify to flip...
-	displayObject.worldTransform[4] = -1;
-	displayObject.worldTransform[5] = this.projection.y * 2;
+    //TODO -? create a new one??? dont think so!
+    var originalWorldTransform = displayObject.worldTransform;
+    displayObject.worldTransform = PIXI.mat3.create();//sthis.identityMatrix;
+    // modify to flip...
+    displayObject.worldTransform[4] = -1;
+    displayObject.worldTransform[5] = this.projection.y * 2;
 
 
-	if(position)
-	{
-		displayObject.worldTransform[2] = position.x;
-		displayObject.worldTransform[5] -= position.y;
-	}
+    if(position)
+    {
+        displayObject.worldTransform[2] = position.x;
+        displayObject.worldTransform[5] -= position.y;
+    }
 
-	PIXI.visibleCount++;
-	displayObject.vcount = PIXI.visibleCount;
+    PIXI.visibleCount++;
+    displayObject.vcount = PIXI.visibleCount;
 
-	for(var i=0,j=children.length; i<j; i++)
-	{
-		children[i].updateTransform();
-	}
+    for(var i=0,j=children.length; i<j; i++)
+    {
+        children[i].updateTransform();
+    }
 
-	var renderGroup = displayObject.__renderGroup;
+    var renderGroup = displayObject.__renderGroup;
 
-	if(renderGroup)
-	{
-		if(displayObject == renderGroup.root)
-		{
-			renderGroup.render(this.projection);
-		}
-		else
-		{
-			renderGroup.renderSpecific(displayObject, this.projection);
-		}
-	}
-	else
-	{
-		if(!this.renderGroup)this.renderGroup = new PIXI.WebGLRenderGroup(gl);
-		this.renderGroup.setRenderable(displayObject);
-		this.renderGroup.render(this.projection);
-	}
+    if(renderGroup)
+    {
+        if(displayObject == renderGroup.root)
+        {
+            renderGroup.render(this.projection);
+        }
+        else
+        {
+            renderGroup.renderSpecific(displayObject, this.projection);
+        }
+    }
+    else
+    {
+        if(!this.renderGroup)this.renderGroup = new PIXI.WebGLRenderGroup(gl);
+        this.renderGroup.setRenderable(displayObject);
+        this.renderGroup.render(this.projection);
+    }
 
-	displayObject.worldTransform = originalWorldTransform;
+    displayObject.worldTransform = originalWorldTransform;
 }
 
 
@@ -224,23 +224,23 @@ PIXI.RenderTexture.prototype.renderWebGL = function(displayObject, position, cle
  */
 PIXI.RenderTexture.prototype.renderCanvas = function(displayObject, position, clear)
 {
-	var children = displayObject.children;
+    var children = displayObject.children;
 
-	displayObject.worldTransform = PIXI.mat3.create();
+    displayObject.worldTransform = PIXI.mat3.create();
 
-	if(position)
-	{
-		displayObject.worldTransform[2] = position.x;
-		displayObject.worldTransform[5] = position.y;
-	}
+    if(position)
+    {
+        displayObject.worldTransform[2] = position.x;
+        displayObject.worldTransform[5] = position.y;
+    }
 
 
-	for(var i=0,j=children.length; i<j; i++)
-	{
-		children[i].updateTransform();
-	}
+    for(var i=0,j=children.length; i<j; i++)
+    {
+        children[i].updateTransform();
+    }
 
-	if(clear)this.renderer.context.clearRect(0,0, this.width, this.height);
+    if(clear)this.renderer.context.clearRect(0,0, this.width, this.height);
 
     this.renderer.renderDisplayObject(displayObject);
 