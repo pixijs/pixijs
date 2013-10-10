52ac1a77ff7338941e49fc0c17caf7e5a2f94164
Claudius Coenen <coenen@meso.net>
2013-10-10 19:12:50 +0200

    swapChild implemented via addChildAt

--- src/pixi/display/DisplayObjectContainer.js
+++ src/pixi/display/DisplayObjectContainer.js
@@ -226,43 +226,19 @@ PIXI.DisplayObjectContainer.prototype.addChildAt = function(child, index)
  */
 PIXI.DisplayObjectContainer.prototype.swapChildren = function(child, child2)
 {
-	/*
-	 * this funtion needs to be recoded.. 
-	 * can be done a lot faster..
-	 */
-	return;
-	
-	// need to fix this function :/
-	/*
 	// TODO I already know this??
 	var index = this.children.indexOf( child );
 	var index2 = this.children.indexOf( child2 );
 	
 	if ( index !== -1 && index2 !== -1 ) 
 	{
-		// cool
-		
-		/*
-		if(this.stage)
-		{
-			// this is to satisfy the webGL batching..
-			// TODO sure there is a nicer way to achieve this!
-			this.stage.__removeChild(child);
-			this.stage.__removeChild(child2);
-			
-			this.stage.__addChild(child);
-			this.stage.__addChild(child2);
-		}
-		
-		// swap the positions..
-		this.children[index] = child2;
-		this.children[index2] = child;
-		
+		this.addChildAt(child2, index);
+		this.addChildAt(child, index2);
 	}
 	else
 	{
 		throw new Error(child + " Both the supplied DisplayObjects must be a child of the caller " + this);
-	}*/
+	}
 }
 
 /**
