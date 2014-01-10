32449402e402180f1543225ac48b79c12a3e2776
Weeve Ferrelaine <WeeveFerrelaine@yahoo.com>
2014-01-10 04:42:20 -0600

    Added mathematical functions to the Point object

--- src/pixi/core/Point.js
+++ src/pixi/core/Point.js
@@ -38,6 +38,95 @@ PIXI.Point.prototype.clone = function()
     return new PIXI.Point(this.x, this.y);
 };
 
+/**
+ *
+ * Subtracts given (Point/Vector) from the point
+ *
+ * @method sub
+ * @param B {Point} (Point/Vector) to do element wise subtraction
+ * @return {Point} A new Point resultant of the subtraction of B
+ */
+PIXI.Point.sub = function (B)
+{
+	return new PIXI.Point(this.x - B.x,this.y - B.y);
+}
+
+/**
+ *
+ * Adds given (Point/Vector) to the point
+ *
+ * @method add
+ * @param B {Point} (Point/Vector) to do element wise addition
+ * @return {Point} A new Point resultant of the addition of B
+ */
+PIXI.Point.add = function (B)
+{
+	return new PIXI.Point(this.x + B.x,this.y + B.y);
+}
+
+/**
+ *
+ * Multiplies given (Point/Vector) with the point
+ *
+ * @method mul
+ * @param B {Scalar} Number to do element wise multiplication
+ * @return {Point} A new Point resultant of the multiplication of B
+ */
+PIXI.Point.mul = function (B)
+{
+	return new PIXI.Point(this.x * B,this.y * B);
+}
+
+/**
+ *
+ * Divides given (Point/Vector) from the point
+ *
+ * @method div
+ * @param B {Scalar} Number to do element wise division
+ * @return {Point} A new Point resultant of the division of B
+ */
+PIXI.Point.div = function (B)
+{
+	return new PIXI.Point(this.x / B,this.y / B);
+}
+
+/**
+ *
+ * Dot Product of the given (Point/Vector) and the point
+ *
+ * @method dot
+ * @param B {Point} (Point/Vector) to project along
+ * @return {Scalar} Number resultant of the projection onto B
+ */
+PIXI.Point.dot = function (B)
+{
+	return (this.x*B.x)+(this.y*B.y);
+}
+
+/**
+ *
+ * Magnitude of the point
+ *
+ * @method magnitude
+ * @return {Scalar} Number equivelant to the scalar length of the point
+ */
+PIXI.Point.magnitude = function()
+{
+	return Math.sqrt(this.x^2 + this.y^2);
+}
+
+/**
+ *
+ * Unit (Point/Vector) of the point
+ *
+ * @method unit
+ * @return {Point} A new Point with a length(magnitude) of one
+ */
+PIXI.Point.unit = function()
+{
+	return this.Div(this.Magnitude());
+}
+
 // constructor
 PIXI.Point.prototype.constructor = PIXI.Point;
 
