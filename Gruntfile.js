d3b1c45ad1defe5afd71aa8b0a2dd3457f211b8d
mattdesl <dave.des@gmail.com>
2014-03-04 12:12:02 -0500

    Adding grunt-watch into Gruntfile and package.json. watch uses source maps for development

--- Gruntfile.js
+++ Gruntfile.js
@@ -185,10 +185,11 @@ module.exports = function(grunt) {
                 }
             }
         },
+        //Watches and builds for _development_ (source maps)
         watch: {
             scripts: {
                 files: ['<%= dirs.src %>/**/*.js'],
-                tasks: ['concat'],
+                tasks: ['concat_sourcemap'],
                 options: {
                     spawn: false,
                 }
@@ -214,6 +215,6 @@ module.exports = function(grunt) {
     grunt.registerTask('travis', ['build', 'test']);
 
     grunt.registerTask('default', ['build', 'test']);
-
-    grunt.registerTask('debug-watch', ['concat', 'watch']);
+    
+    grunt.registerTask('debug-watch', ['concat_sourcemap', 'watch:debug']);
 };
