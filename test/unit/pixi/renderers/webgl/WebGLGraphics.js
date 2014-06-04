e46c70ddcf75d9d1339be2c1cfa860d731dbcefc
Dimitri Larue <siderax@gmail.com>
2014-06-04 13:01:46 +0200

    Add method to create and render a rounded rectangle

--- test/unit/pixi/renderers/webgl/WebGLGraphics.js
+++ test/unit/pixi/renderers/webgl/WebGLGraphics.js
@@ -12,6 +12,7 @@ describe('renderers/wegbl/WebGLGraphics', function () {
         expect(WebGLGraphics).itself.to.respondTo('renderGraphics');
         expect(WebGLGraphics).itself.to.respondTo('updateGraphics');
         expect(WebGLGraphics).itself.to.respondTo('buildRectangle');
+        expect(WebGLGraphics).itself.to.respondTo('buildRoundedRectangle');
         expect(WebGLGraphics).itself.to.respondTo('buildCircle');
         expect(WebGLGraphics).itself.to.respondTo('buildLine');
         expect(WebGLGraphics).itself.to.respondTo('buildPoly');
