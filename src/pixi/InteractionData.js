78c23ff7b7c304ca00c181739c214e69dd5c80ca
Chad Engler <englercj@live.com>
2014-08-05 09:47:03 -0700

    Merge #317
    Adding support for pooling Point objects in
    PIXI.InteractionData.prototype.getLocalPosition
    
    In certain use cases, such as dragging, events are fired off in rapid
    succession.
    
    Creating a new PIXI.Point in each event can cause memory usage to build up
    and trigger a nasty garbage collection event.
    
    This commit allows you to pass in a point instead of allocating a new one
    each time the method is called.
    
    Sample usage would be something like this:
    
    var newPosition = new PIXI.Point();
    
    myContainer.mousemove = function(data) {
    	data.originalEvent.preventDefault();
    
    	this.data.getLocalPosition(this.parent, newPosition);
    
    	this.position.x = newPosition.x;
    	this.position.y = newPosition.y;
    }

--- src/pixi/InteractionData.js
+++ src/pixi/InteractionData.js
@@ -1,7 +1,7 @@
 /**
  * @author Mat Groves http://matgroves.com/ @Doormat23
  */
- 
+
 /**
  * Holds all information related to an Interaction event
  *
@@ -18,7 +18,7 @@ PIXI.InteractionData = function()
      */
     this.global = new PIXI.Point();
 
-   
+
     /**
      * The target Sprite that was interacted with
      *
@@ -41,9 +41,10 @@ PIXI.InteractionData = function()
  *
  * @method getLocalPosition
  * @param displayObject {DisplayObject} The DisplayObject that you would like the local coords off
+ * @param [point] {Point} A Point object in which to store the value, optional (otherwise will create a new point)
  * @return {Point} A point containing the coordinates of the InteractionData position relative to the DisplayObject
  */
-PIXI.InteractionData.prototype.getLocalPosition = function(displayObject)
+PIXI.InteractionData.prototype.getLocalPosition = function(displayObject, point)
 {
     var worldTransform = displayObject.worldTransform;
     var global = this.global;
@@ -52,9 +53,14 @@ PIXI.InteractionData.prototype.getLocalPosition = function(displayObject)
     var a00 = worldTransform.a, a01 = worldTransform.b, a02 = worldTransform.tx,
         a10 = worldTransform.c, a11 = worldTransform.d, a12 = worldTransform.ty,
         id = 1 / (a00 * a11 + a01 * -a10);
+
+    point = point || new PIXI.Point();
+
+    point.x = a11 * id * global.x + -a01 * id * global.y + (a12 * a01 - a02 * a11) * id;
+    point.y = a00 * id * global.y + -a10 * id * global.x + (-a12 * a00 + a02 * a10) * id;
+
     // set the mouse coords...
-    return new PIXI.Point(a11 * id * global.x + -a01 * id * global.y + (a12 * a01 - a02 * a11) * id,
-                               a00 * id * global.y + -a10 * id * global.x + (-a12 * a00 + a02 * a10) * id);
+    return point;
 };
 
 // constructor
