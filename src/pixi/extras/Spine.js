1590291f08ed2f89f4e661af8a91c953cf7da71b
sitnarf <you@example.com>
2014-09-15 16:44:09 +0200

    Edited updateTransform in Spine, condition with attachment.name changed to attachment.rendererObject.name, because attachment.name never exists

--- src/pixi/extras/Spine.js
+++ src/pixi/extras/Spine.js
@@ -1426,7 +1426,7 @@ PIXI.Spine.prototype.updateTransform = function () {
         }
 
         if (attachment.rendererObject) {
-            if (!slot.currentSpriteName || slot.currentSpriteName != attachment.name) {
+            if (!slot.currentSpriteName || slot.currentSpriteName != attachment.rendererObject.name) {
                 var spriteName = attachment.rendererObject.name;
                 if (slot.currentSprite !== undefined) {
                     slot.currentSprite.visible = false;
