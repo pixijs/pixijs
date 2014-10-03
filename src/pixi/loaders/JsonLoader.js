e9ed0def5ec571cac315440f45e7af9585115c9c
Dario Segura <dario@getsetgames.com>
2014-10-03 16:45:41 -0400

    -[SPINE] Json loader checks if the spine file was previously loaded before starting a lengthy loading operation.

--- src/pixi/loaders/JsonLoader.js
+++ src/pixi/loaders/JsonLoader.js
@@ -147,50 +147,58 @@ PIXI.JsonLoader.prototype.onJSONLoaded = function () {
     }
     else if(this.json.bones)
     {
-		/* use a bit of hackery to load the atlas file, here we assume that the .json, .atlas and .png files
-		 * that correspond to the spine file are in the same base URL and that the .json and .atlas files
-		 * have the same name
-		*/
-		var atlasPath = this.url.substr(0, this.url.lastIndexOf('.')) + '.atlas';
-		var atlasLoader = new PIXI.JsonLoader(atlasPath, this.crossorigin);
-		// save a copy of the current object for future reference //
-		var originalLoader = this;
-		// before loading the file, replace the "onJSONLoaded" function for our own //
-		atlasLoader.onJSONLoaded = function()
+		/* check if the json was loaded before */
+		if (PIXI.AnimCache[this.url])
 		{
-			// at this point "this" points at the atlasLoader (JsonLoader) instance //
-			if(!this.ajaxRequest.responseText)
+			this.onLoaded();
+		}
+		else
+		{
+			/* use a bit of hackery to load the atlas file, here we assume that the .json, .atlas and .png files
+			 * that correspond to the spine file are in the same base URL and that the .json and .atlas files
+			 * have the same name
+			*/
+			var atlasPath = this.url.substr(0, this.url.lastIndexOf('.')) + '.atlas';
+			var atlasLoader = new PIXI.JsonLoader(atlasPath, this.crossorigin);
+			// save a copy of the current object for future reference //
+			var originalLoader = this;
+			// before loading the file, replace the "onJSONLoaded" function for our own //
+			atlasLoader.onJSONLoaded = function()
 			{
-				this.onError(); // FIXME: hmm, this is funny because we are not reponding to errors yet
-				return;
-			}
-			// create a new instance of a spine texture loader for this spine object //
-			var textureLoader = new PIXI.SpineTextureLoader(this.url.substring(0, this.url.lastIndexOf('/')));
-			// create a spine atlas using the loaded text and a spine texture loader instance //
-			var spineAtlas = new spine.Atlas(this.ajaxRequest.responseText, textureLoader);
-			// now we use an atlas attachment loader //
-			var attachmentLoader = new spine.AtlasAttachmentLoader(spineAtlas);
-			// spine animation
-			var spineJsonParser = new spine.SkeletonJson(attachmentLoader);
-			var skeletonData = spineJsonParser.readSkeletonData(originalLoader.json);
-			PIXI.AnimCache[originalLoader.url] = skeletonData;
-			// wait for textures to finish loading if needed
-			if (textureLoader.loadingCount > 0)
-			{
-				textureLoader.addEventListener('loadedBaseTexture', function(evt){
-					if (evt.content.content.loadingCount <= 0)
-					{
-						originalLoader.onLoaded();
-					}
-				});
-			}
-			else
-			{
-				originalLoader.onLoaded();
-			}
-		};
-		// start the loading //
-		atlasLoader.load();
+				// at this point "this" points at the atlasLoader (JsonLoader) instance //
+				if(!this.ajaxRequest.responseText)
+				{
+					this.onError(); // FIXME: hmm, this is funny because we are not reponding to errors yet
+					return;
+				}
+				// create a new instance of a spine texture loader for this spine object //
+				var textureLoader = new PIXI.SpineTextureLoader(this.url.substring(0, this.url.lastIndexOf('/')));
+				// create a spine atlas using the loaded text and a spine texture loader instance //
+				var spineAtlas = new spine.Atlas(this.ajaxRequest.responseText, textureLoader);
+				// now we use an atlas attachment loader //
+				var attachmentLoader = new spine.AtlasAttachmentLoader(spineAtlas);
+				// spine animation
+				var spineJsonParser = new spine.SkeletonJson(attachmentLoader);
+				var skeletonData = spineJsonParser.readSkeletonData(originalLoader.json);
+				PIXI.AnimCache[originalLoader.url] = skeletonData;
+				// wait for textures to finish loading if needed
+				if (textureLoader.loadingCount > 0)
+				{
+					textureLoader.addEventListener('loadedBaseTexture', function(evt){
+						if (evt.content.content.loadingCount <= 0)
+						{
+							originalLoader.onLoaded();
+						}
+					});
+				}
+				else
+				{
+					originalLoader.onLoaded();
+				}
+			};
+			// start the loading //
+			atlasLoader.load();
+		}
     }
     else
     {
