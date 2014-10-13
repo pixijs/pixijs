314d740e45fb9e5e0101be0826841a30e59e16ce
mathew groves <mat@goodboydigital.com>
2014-10-14 00:27:50 +0100

    doc type in movieclip

--- src/pixi/display/MovieClip.js
+++ src/pixi/display/MovieClip.js
@@ -190,7 +190,7 @@ PIXI.MovieClip.fromFrames = function(frames)
  * A short hand way of creating a movieclip from an array of image ids
  *
  * @static
- * @method fromFrames
+ * @method fromImages
  * @param frames {Array} the array of image ids the movieclip will use as its texture frames
  */
 PIXI.MovieClip.fromImages = function(images)
