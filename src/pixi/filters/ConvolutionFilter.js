1f435ffe49838c3ddb88a4df44b518e113381770
gordyr <gordonrankin82@gmail.com>
2014-10-16 08:15:37 +0100

    Added Convolution Filter

---
+++ src/pixi/filters/ConvolutionFilter.js
@@ -0,0 +1,12 @@
+/**
+ * The ConvolutionFilter class applies a matrix convolution filter effect. 
+ * A convolution combines pixels in the input image with neighboring pixels to produce an image. 
+ * A wide variety of image effects can be achieved through convolutions, including blurring, edge detection, sharpening, embossing, and beveling.
+ * 
+ * @class ConvolutionFilter
+ * @extends AbstractFilter
+ * @constructor
+ * @param texture {Array} An array of values used for matrix transformation.
+ * @param width {Number} Width of the object you are transforming
+ * @param height {Number} Height of the object you are transforming
+ */
