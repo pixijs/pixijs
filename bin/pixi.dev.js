0e88c3c27e26db5329f2b755090533f0c72eb33d
nkapravchuk <12zvvbcy36>
2015-01-19 10:51:15 +0200

    fix mask

--- bin/pixi.dev.js
+++ bin/pixi.dev.js
@@ -11419,12 +11419,12 @@ PIXI.CanvasGraphics.renderGraphicsMask = function(graphics, context)
         else if (data.type === PIXI.Graphics.RREC)
         {
         
-            var pts = shape.points;
-            var rx = pts[0];
-            var ry = pts[1];
-            var width = pts[2];
-            var height = pts[3];
-            var radius = pts[4];
+            var pts = shape;
+            var rx = pts.x;
+            var ry = pts.y;
+            var width = pts.width;
+            var height = pts.height;
+            var radius = pts.radius;
 
             var maxRadius = Math.min(width, height) / 2 | 0;
             radius = radius > maxRadius ? maxRadius : radius;
