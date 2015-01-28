9a620273411e91ed0a030dd7457fe268bdb528d3
Chad Engler <englercj@live.com>
2015-01-27 20:30:55 -0800

    bump version to v2.2.4

--- src/pixi/Pixi.js
+++ src/pixi/Pixi.js
@@ -36,7 +36,7 @@ PIXI.CANVAS_RENDERER = 1;
  * @property {String} VERSION
  * @static
  */
-PIXI.VERSION = "v2.2.3";
+PIXI.VERSION = "v2.2.4";
 
 /**
  * Various blend modes supported by pixi. IMPORTANT - The WebGL renderer only supports the NORMAL, ADD, MULTIPLY and SCREEN blend modes.
