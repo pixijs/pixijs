ec610372980861bbb9da7f8a5d722d902ca2f121
Chad Engler <englercj@live.com>
2015-02-26 07:17:40 -0800

    fix npm docs task

--- gulp/tasks/jsdoc.js
+++ gulp/tasks/jsdoc.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     exec = require('child_process').exec;
 
 gulp.task('jsdoc', function (done) {
-    exec('npm docs', function (err, stdout, stderr) {
+    exec('npm run docs', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
 
