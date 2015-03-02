9e40c19f1b3ad71279ab3a66d896257f2aaf94a6
Alvin <alvinsight@gmail.com>
2015-03-02 15:40:08 +0000

    Revert "Removed the namespaces"
    This reverts commit 2eae97c696d3a67b7a19297d981c1008b8aa990c.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
-var core = require('./core'),
-	math = core.math,
-	// plugins:
-	extras         = require('./extras'),
-	filters        = require('./filters'),
-	interaction    = require('./interaction'),
-	loaders        = require('./loaders'),
-	spine          = require('./spine'),
-	text           = require('./text'),
-    deprecation    = require('./deprecation');
+var core        = require('./core'),
+    deprecation = require('./deprecation');
 
-var modules = [];
-
-modules.push(math);
-modules.push(extras);
-modules.push(filters);
-modules.push(interaction);
-modules.push(loaders);
-modules.push(spine);
-modules.push(text);
-
-for (var i = 0; i < modules.length; i++) {
-
-	// Adds the module to the core, so you can do
-	// PIXI.Text instead of PIXI.text.Text
-	bundleUp(core,modules[i]);
-};
-
-function bundleUp(object1,object2){
-
-    for(var keys in object2){
-        object1[keys] = object2[keys];
-    }
-}
+// plugins:
+core.extras         = require('./extras');
+core.filters        = require('./filters');
+core.interaction    = require('./interaction');
+core.loaders        = require('./loaders');
+core.spine          = require('./spine');
+core.text           = require('./text');
 
 module.exports = core;
