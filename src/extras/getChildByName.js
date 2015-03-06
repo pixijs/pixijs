e0d8b1c18e2f0738d4768c345c042343b81566ad
Adi Mora <adi.mora@gamesys.co.uk>
2015-03-06 16:54:56 +0000

    added getChildByName to extras package

---
+++ src/extras/getChildByName.js
@@ -0,0 +1,27 @@
+var DisplayObject = require('../core/display/DisplayObject'),
+    Container = require('../core/display/Container')
+
+/**
+ * The instance name of the object.
+ *
+ * @member {string}
+ */
+DisplayObject.prototype.name = null;
+
+/**
+* Get
+*
+* @param name {string} the instance name
+* @return {DisplayObject}
+*/
+Container.prototype.getChildByName = function(val)
+{
+    for (i = 0; i < this.children.length; i++) {
+        if (this.children[i].name == val) {
+            return this.children[i];
+        }
+    }
+    return null;
+};
+
+module.exports = {};
