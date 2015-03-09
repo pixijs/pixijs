15c65e5e90461883c1076f6848185900302e372a
Chad Engler <englercj@live.com>
2015-03-09 09:29:33 -0700

    fix the font name parsing

--- src/text/BitmapText.js
+++ src/text/BitmapText.js
@@ -164,7 +164,7 @@ Object.defineProperties(BitmapText.prototype, {
             if (typeof value === 'string') {
                 value = value.split(' ');
 
-                this._style.fontName = value.slice(1).join(' ');
+                this._style.fontName = value.length === 1 ? value[0] : value.slice(1).join(' ');
                 this._style.fontSize = value.length >= 2 ? parseInt(value[0], 10) : BitmapText.fonts[this.fontName].size;
             }
             else {
