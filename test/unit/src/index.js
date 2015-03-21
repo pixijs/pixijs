7c219e4384f4a581dec8a62e6ace430b0bbb72a3
Dr. Kibtiz <info@drkibitz.com>
2015-03-21 01:56:00 -0700

    style fix

--- test/unit/src/index.js
+++ test/unit/src/index.js
@@ -2,10 +2,10 @@ describe('src', function () {
     'use strict';
 
     var expect = chai.expect;
-	var pixi = require('../../../src');
+    var pixi = require('../../../src');
 
     it('Module exists', function () {
         expect(pixi)
-	        .to.be.an('object');
+            .to.be.an('object');
     });
 });
