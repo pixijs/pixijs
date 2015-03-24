849e4c654a8ede2fb0a3a91a570a9a278ee1fb75
misha <amashukeli@gmail.com>
2015-03-24 17:25:16 +0400

    updated a graphics unit test

--- test/unit/pixi/primitives/Graphics.js
+++ test/unit/pixi/primitives/Graphics.js
@@ -37,7 +37,8 @@ describe('pixi/primitives/Graphics', function () {
         expect(obj).to.have.property('lineWidth', 0);
         expect(obj).to.have.property('width', 0);
         expect(obj).to.have.property('height', 0);
-        expect(obj).to.have.property('lineColor', 0);
+        expect(obj).to.have.property('strokeStyle', 0);
+        expect(obj).to.have.property('fillStyle', 0);
         expect(obj).to.have.deep.property('graphicsData.length', 0);
       //  expect(obj).to.have.deep.property('currentPath.points.length', 0);
     });
