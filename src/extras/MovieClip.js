9ff3e103fa2e90c6694beebedfc56597507f200f
Grzegorz Przybyłowicz <grzegorz.p@grzegorzp-MacBook-Pro.local>
2015-04-22 11:22:01 +0200

    MoiveClip currentFrame changed as property which returns current frame, the old variable currentFrame renamed to totalTime

--- src/extras/MovieClip.js
+++ src/extras/MovieClip.js
@@ -56,13 +56,13 @@ function MovieClip(textures)
     this.onComplete = null;
 
     /**
-     * The MovieClips current frame index (this may not have to be a whole number)
+     * Elapsed time since animation has been started, used internally to display current texture
      *
      * @member {number}
      * @default 0
      * @readonly
      */
-    this.currentFrame = 0;
+    this.totalTime = 0;
 
     /**
      * Indicates if the MovieClip is currently playing
@@ -111,7 +111,20 @@ Object.defineProperties(MovieClip.prototype, {
         {
             this._textures = value;
 
-            this.texture = this._textures[Math.floor(this.currentFrame) % this._textures.length];
+            this.texture = this._textures[Math.floor(this.totalTime) % this._textures.length];
+        }
+    },
+
+    /**
+    * The MovieClips current frame index
+    *
+    * @member {number}
+    * @readonly
+    */
+    currentFrame: {
+        get: function ()
+        {
+            return Math.floor(this.totalTime) % this._texture.length;
         }
     }
 
@@ -156,9 +169,9 @@ MovieClip.prototype.gotoAndStop = function (frameNumber)
 {
     this.stop();
 
-    this.currentFrame = frameNumber;
+    this.totalTime = frameNumber;
 
-    var round = Math.floor(this.currentFrame);
+    var round = Math.floor(this.totalTime);
     this._texture = this._textures[round % this._textures.length];
 };
 
@@ -169,7 +182,7 @@ MovieClip.prototype.gotoAndStop = function (frameNumber)
  */
 MovieClip.prototype.gotoAndPlay = function (frameNumber)
 {
-    this.currentFrame = frameNumber;
+    this.totalTime = frameNumber;
     this.play();
 };
 
@@ -180,16 +193,16 @@ MovieClip.prototype.gotoAndPlay = function (frameNumber)
 MovieClip.prototype.update = function (deltaTime)
 {
 
-    this.currentFrame += this.animationSpeed * deltaTime;
+    this.totalTime += this.animationSpeed * deltaTime;
 
-    var floor = Math.floor(this.currentFrame);
+    var floor = Math.floor(this.totalTime);
 
     if (floor < 0)
     {
         if (this.loop)
         {
-            this.currentFrame += this._textures.length;
-            this._texture = this._textures[this.currentFrame];
+            this.totalTime += this._textures.length;
+            this._texture = this._textures[this.totalTime];
         }
         else
         {
