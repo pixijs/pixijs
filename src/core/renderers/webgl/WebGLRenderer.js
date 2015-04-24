6c3fe117a136b87467fdf9fb9250c42dbb97f0c9
Chad Engler <englercj@live.com>
2015-04-24 07:32:29 -0700

    resize gl viewport in webgl; fixes #1712

--- src/core/renderers/webgl/WebGLRenderer.js
+++ src/core/renderers/webgl/WebGLRenderer.js
@@ -338,6 +338,8 @@ WebGLRenderer.prototype.resize = function (width, height)
 {
     SystemRenderer.prototype.resize.call(this, width, height);
 
+    this.gl.viewport(0, 0, this.width, this.height);
+
     this.filterManager.resize(width, height);
     this.renderTarget.resize(width, height);
 
