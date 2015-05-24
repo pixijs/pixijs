87ac6a5406b46f6aa2d7780c3dd19bc0ca9fb95f
Mat Groves <mat@goodboydigital.com>
2015-05-24 17:24:56 +0100

    fixed up 2d interaction of 3D plane

--- src/flip/math/index.js
+++ src/flip/math/index.js
@@ -5,7 +5,8 @@
 var glMat = require('gl-matrix'),
 	Point3d = require('./Point3d'),
 	vec3 = glMat.vec3,
-	vec4 = glMat.mat4,
+	mat4 = glMat.mat4,
+	mat3 = glMat.mat3,
 	tempPoint = new Point3d();
 
 module.exports = {
@@ -35,22 +36,22 @@ module.exports = {
 	{
 	    var tempP = vec3.create();
 
-	//   console.log(renderer.width)
+	     var combinedMatrix = window.combinedMatrix//mat4.multiply(mat4.create(), perspectiveMatrix, projection3d);
+	     if(!combinedMatrix)return [[0,0,0], [0,0,0]]
+	     var inverse = mat4.invert(mat4.create(), combinedMatrix);
+
+	  	
 	    // get the near plane..
 	    tempP[0] = (point.x / (renderer.width * 0.5)) - 1;
 	    tempP[1] = 1 - (point.y / (renderer.height * 0.5));
 	    tempP[2] = 0;
 
-	    var combinedMatrix = renderer.plugins.sprite3d.combinedMatrix
-	   if(!combinedMatrix)return [[0,0,0], [0,0,0]]
-	    
-	    var inverse = mat4.invert(mat4.create(), combinedMatrix);
 	    var origin = vec3.transformMat4(vec3.create(), tempP, inverse);
 
 	    // get the far plane
 	    tempP[2] = 0.99;
 
-	    var tempP = vec3.transformMat4(vec3.create(), tempP, inverse);
+	    tempP = this.projectionTransformMat4(vec3.create(), tempP, inverse);
 
 	    // now calculate the origin..
 	    var direction = vec3.subtract(vec3.create(), tempP, origin);
@@ -60,11 +61,21 @@ module.exports = {
 	    return [origin, direction];
 	},
 
+	projectionTransformMat4:function(out, a, m) 
+	{
+	    var x = a[0], y = a[1], z = a[2],
+	        w = m[3] * x + m[7] * y + m[11] * z + m[15];
+	    w = w || 1.0;
+	    out[0] = (m[0] * x + m[4] * y + m[8] * z + m[12]) / w;
+	    out[1] = (m[1] * x + m[5] * y + m[9] * z + m[13]) / w;
+	    out[2] = (m[2] * x + m[6] * y + m[10] * z + m[14]) / w;
+	    return out;
+	},
+
 	get2DContactPoint:function(ray, container)
 	{
 	    var transposeInverse = mat3.normalFromMat4(mat3.create(), container.worldTransform3d);
 
-//	    console.log(container.worldTransform3d)
 	    var normal = [
 	        transposeInverse[6],
 	        transposeInverse[7],
@@ -77,7 +88,7 @@ module.exports = {
 	        container.worldTransform3d[14],
 	    ]
 	   
-	    var t = intersectPlane(normal, position, ray[0], ray[1], 1000000);
+	    var t = this.intersectPlane(normal, position, ray[0], ray[1], 1000000);
 	    
 	    if(t)
 	    {
