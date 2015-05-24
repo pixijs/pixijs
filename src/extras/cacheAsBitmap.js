e09eb5d3e3f1c1a43a11e1e9176cf4a02a7a7c67
Mat Groves <mat@goodboydigital.com>
2015-05-24 10:07:15 +0100

    added Alpha to cachAsBitmap

--- src/extras/cacheAsBitmap.js
+++ src/extras/cacheAsBitmap.js
@@ -87,6 +87,8 @@ DisplayObject.prototype._renderCachedWebGL = function (renderer)
     
     this._initCachedDisplayObject( renderer );
 
+    this._cachedSprite.worldAlpha = this.worldAlpha;
+    
     renderer.setObjectRenderer(renderer.plugins.sprite);
     renderer.plugins.sprite.render( this._cachedSprite );
 };
