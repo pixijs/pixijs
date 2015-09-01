f3f0f191714e333fe7fbbcb7921690b4956edac4
Mike Reinstein <reinstein.mike@gmail.com>
2015-09-01 07:48:34 -0700

    fixed renderTexture instantiation example

--- src/core/textures/RenderTexture.js
+++ src/core/textures/RenderTexture.js
@@ -17,7 +17,8 @@ var BaseTexture = require('./BaseTexture'),
  * and rotation of the given Display Objects is ignored. For example:
  *
  * ```js
- * var renderTexture = new PIXI.RenderTexture(800, 600);
+ * var renderer = PIXI.autoDetectRenderer(1024, 1024, { view: canvas, ratio: 1 });
+ * var renderTexture = new PIXI.RenderTexture(renderer, 800, 600);
  * var sprite = PIXI.Sprite.fromImage("spinObj_01.png");
  *
  * sprite.position.x = 800/2;
