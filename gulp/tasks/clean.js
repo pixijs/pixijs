546f20a0637ce1163bad7b7f7f7559ace87be9e1
Steffen Baer <stbaer.mail@gmail.com>
2015-09-13 22:49:18 +0800

    updated del version and clean taksk

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -1,6 +1,6 @@
 var gulp    = require('gulp'),
     del     = require('del');
 
-gulp.task('clean', function (done) {
-    return del(paths.out, done);
+gulp.task('clean', function () {
+    return del(paths.out);
 });
