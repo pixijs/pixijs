68aae0b42b38e530b4919e63ffdb5dc64647dc17
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2016-01-01 03:46:18 +0300

    backout

--- src/loaders/textureParser.js
+++ src/loaders/textureParser.js
@@ -1,20 +1,16 @@
-var core = require('../core'), Resource = require('resource-loader').Resource;
+var core = require('../core');
 
 module.exports = function ()
 {
     return function (resource, next)
     {
         // create a new texture if the data is an Image object
-        // middleware can be executed before or after image is loaded
-        if (resource.loadType === Resource.LOAD_TYPE.IMAGE)
+        if (resource.data && resource.isImage)
         {
-            if (!resource.data) {
-                resource.prepareImage();
-            }
             var baseTexture = new core.BaseTexture(resource.data, null, core.utils.getResolutionOfUrl(resource.url));
             baseTexture.imageUrl = resource.url;
             resource.texture = new core.Texture(baseTexture);
-            // lets also add the frame to pixi's global cache for fromFrame and fromImage functions
+            // lets also add the frame to pixi's global cache for fromFrame and fromImage fucntions
             core.utils.BaseTextureCache[resource.url] = baseTexture;
             core.utils.TextureCache[resource.url] = resource.texture;
         }
