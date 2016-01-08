8aff51ebb0ce7aa69e37809b42c777f2409bf40d
SirKnightDragoon <dragoon@dnastorm.com>
2016-01-08 08:39:47 -0500

    redo

--- src/interaction/InteractionManager.js
+++ src/interaction/InteractionManager.js
@@ -385,6 +385,7 @@ InteractionManager.prototype.processInteractive = function (point, displayObject
             //Tests if the child is in the hitArea
             if(children[i].parent.hitArea)
             {
+                displayObject.worldTransform.applyInverse(point,  this._tempPoint);
                 if(!displayObject.hitArea.contains(this._tempPoint.x, this._tempPoint.y)) continue;
             }
             
