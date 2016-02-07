03f5a238b6067ffda97fb62ea6b7d549ea4dfc55
Mat Groves <mat@goodboydigital.com>
2016-02-07 10:02:28 +0000

    increase batch size

--- src/core/const.js
+++ src/core/const.js
@@ -215,7 +215,7 @@ var CONST = {
 
     // TODO: maybe change to SPRITE.BATCH_SIZE: 2000
     // TODO: maybe add PARTICLE.BATCH_SIZE: 15000
-    SPRITE_BATCH_SIZE: 2000, //nice balance between mobile and desktop machines
+    SPRITE_BATCH_SIZE: 4096, //nice balance between mobile and desktop machines
     SPRITE_MAX_TEXTURES: 32//this is the MAXIMUM - various gpus will have there own limits.
 };
 
