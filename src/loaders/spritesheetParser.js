a1e0340bc8ae2da38647353c02f6fa0ca44d8485
Lukas Taegert <lukas.taegert@tngtech.com>
2016-03-10 18:08:05 +0100

    Some formatting, remove logs

--- src/loaders/spritesheetParser.js
+++ src/loaders/spritesheetParser.js
@@ -5,10 +5,13 @@ async = require('async');
 
 const BATCH_SIZE = 1000;
 
-module.exports = function() {
-    return function(resource, next) {
+module.exports = function ()
+{
+    return function (resource, next)
+    {
         // skip if no data, its not json, or it isn't spritesheet data
-        if (!resource.data || !resource.isJson || !resource.data.frames) {
+        if (!resource.data || !resource.isJson || !resource.data.frames)
+        {
             return next();
         }
 
@@ -17,47 +20,57 @@ module.exports = function() {
             loadType: Resource.LOAD_TYPE.IMAGE,
             metadata: resource.metadata.imageMetadata
         };
+
         var route = path.dirname(resource.url.replace(this.baseUrl, ''));
+
         var resolution = core.utils.getResolutionOfUrl(resource.url);
 
         // load the image for this sheet
-        this.add(resource.name + '_image', route + '/' + resource.data.meta.image, loadOptions, function(res) {
+        this.add(resource.name + '_image', route + '/' + resource.data.meta.image, loadOptions, function (res)
+        {
             resource.textures = {};
 
             var frames = resource.data.frames;
             var frameKeys = Object.keys(frames);
             var batchIndex = 0;
 
-            function shouldProcessNextBatch() {
+            function shouldProcessNextBatch()
+            {
                 return batchIndex * BATCH_SIZE < frameKeys.length;
             }
 
-            function getTimerLabel() {
+            function getTimerLabel()
+            {
                 return 'PARSING SHEET: ' + resource.name + ' BATCH: ' + batchIndex + '\n';
             }
 
-            function processNextBatch(done) {
-                console.time(getTimerLabel());
+            function processNextBatch(done)
+            {
                 const initialFrameIndex = batchIndex * BATCH_SIZE;
                 var frameIndex = initialFrameIndex;
 
-                while (frameIndex - initialFrameIndex < BATCH_SIZE && frameIndex < frameKeys.length) {
+                while (frameIndex - initialFrameIndex < BATCH_SIZE && frameIndex < frameKeys.length)
+                {
                     var frame = frames[frameKeys[frameIndex]];
                     var rect = frame.frame;
 
-                    if (rect) {
+                    if (rect)
+                    {
                         var size = null;
                         var trim = null;
 
-                        if (frame.rotated) {
+                        if (frame.rotated)
+                        {
                             size = new core.Rectangle(rect.x, rect.y, rect.h, rect.w);
                         }
-                        else {
+                        else
+                        {
                             size = new core.Rectangle(rect.x, rect.y, rect.w, rect.h);
                         }
 
                         //  Check to see if the sprite is trimmed
-                        if (frame.trimmed) {
+                        if (frame.trimmed)
+                        {
                             trim = new core.Rectangle(
                                 frame.spriteSourceSize.x / resolution,
                                 frame.spriteSourceSize.y / resolution,
@@ -67,7 +80,8 @@ module.exports = function() {
                         }
 
                         // flip the width and height!
-                        if (frame.rotated) {
+                        if (frame.rotated)
+                        {
                             var temp = size.width;
                             size.width = size.height;
                             size.height = temp;
@@ -85,7 +99,6 @@ module.exports = function() {
                     }
                     frameIndex++;
                 }
-                console.timeEnd(getTimerLabel());
                 batchIndex++;
                 setTimeout(done, 0);
             }
