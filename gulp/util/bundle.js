19e188757ccb8160c477c8bc925c084af642e8bd
Matt Karl <matt.karl@jibo.com>
2016-04-13 21:34:50 -0400

    Added missing polyfills

--- gulp/util/bundle.js
+++ gulp/util/bundle.js
@@ -62,7 +62,7 @@ function createBundler(args) {
     args = args || {};
     args.debug = false;
     args.standalone = 'PIXI';
-    args.builtins = ['_process'];
+    args.builtins = ['_process', 'fs', 'path'];
 
     var bundle = browserify(paths.jsEntry, args),
         argv = require('minimist')(process.argv.slice(2)),
