8f49725e5b32faf1c7365fc8df59b44dfc1f187b
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2016-06-17 13:09:32 +0300

    TilingSprite is fixed

--- src/extras/TilingSprite.js
+++ src/extras/TilingSprite.js
@@ -103,6 +103,14 @@ TilingSprite.prototype.constructor = TilingSprite;
 module.exports = TilingSprite;
 
 /**
+ * TilingSprite doesnt need that
+ * @private
+ */
+TilingSprite.prototype._onTextureUpdate = function () {
+
+};
+
+/**
  * Renders the object using the WebGL renderer
  *
  * @param renderer {PIXI.WebGLRenderer}
