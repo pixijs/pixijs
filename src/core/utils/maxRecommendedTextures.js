19e4c239d8283ef41b5c29c371d83e5dcba120b6
Jeremie Pelletier <jpelletier@cortexmedia.ca>
2016-09-06 11:04:55 -0400

    Allow more than two texture units on mobile

--- src/core/utils/maxRecommendedTextures.js
+++ src/core/utils/maxRecommendedTextures.js
@@ -5,16 +5,16 @@ var  Device = require('ismobilejs');
 var maxRecommendedTextures = function(max)
 {
 
-	if(Device.tablet || Device.phone)
-	{
+	// if(Device.tablet || Device.phone)
+	// {
 		// check if the res is iphone 6 or higher..
-		return 2;
-	}
-	else
-	{
+	// 	return 2;
+	// }
+	// else
+	// {
 		// desktop should be ok
 		return max;
-	}
+	// }
 };
 
 module.exports = maxRecommendedTextures;
