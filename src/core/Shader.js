34cb48644936241ba73ea37b55f819eaf3843197
Mat Groves <mat@goodboydigital.com>
2016-09-08 20:39:22 +0100

    add attributes to shader

--- src/core/Shader.js
+++ src/core/Shader.js
@@ -25,9 +25,10 @@ function checkPrecision(src) {
  * @param gl {WebGLRenderingContext} The current WebGL rendering context
  * @param vertexSrc {string|string[]} The vertex shader source as an array of strings.
  * @param fragmentSrc {string|string[]} The fragment shader source as an array of strings.
+ * @param attributeLocations {Object} An attribute location map that lets you manually set the attribute locations.
  */
-var Shader = function(gl, vertexSrc, fragmentSrc) {
-    GLShader.call(this, gl, checkPrecision(vertexSrc), checkPrecision(fragmentSrc));
+var Shader = function(gl, vertexSrc, fragmentSrc, attributeLocations) {
+    GLShader.call(this, gl, checkPrecision(vertexSrc), checkPrecision(fragmentSrc), attributeLocations);
 };
 
 Shader.prototype = Object.create(GLShader.prototype);
