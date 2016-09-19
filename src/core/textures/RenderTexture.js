2a59ea60b217d0db619a78e52c6ca55851b8e12e
Steffen Baer <stbaer.mail@gmail.com>
2016-09-19 08:42:16 +0800

    fix RenderTexture, incomplete merge

--- src/core/textures/RenderTexture.js
+++ src/core/textures/RenderTexture.js
@@ -55,15 +55,7 @@ class RenderTexture extends Texture
             const resolution = arguments[4] || 1;
 
             // we have an old render texture..
-<<<<<<< HEAD
-<<<<<<< HEAD
-            console.warn(`v4 RenderTexture now expects a new BaseRenderTexture. Please use RenderTexture.create(${width}, ${height})`);
-=======
             console.warn(`v4 RenderTexture now expects a new BaseRenderTexture. Please use RenderTexture.create(${width}, ${height})`);  // jshint ignore:line
->>>>>>> some more es6 changes
-=======
-            console.warn(`v4 RenderTexture now expects a new BaseRenderTexture. Please use RenderTexture.create(${width}, ${height})`);  // jshint ignore:line
->>>>>>> Revert "Merge branch 'dev-es6' into dev-es6-sb"
             _legacyRenderer = arguments[0];
 
             frame = null;
