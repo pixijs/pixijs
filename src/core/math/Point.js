2fe40cde48ab51d00f439636f41b1b5a198375df
Steffen Baer <stbaer.mail@gmail.com>
2016-09-24 10:47:53 +0800

    Point default args

--- src/core/math/Point.js
+++ src/core/math/Point.js
@@ -9,19 +9,19 @@
  */
 class Point
 {
-    constructor(x, y)
+    constructor(x=0, y=0)
     {
         /**
          * @member {number}
          * @default 0
          */
-        this.x = x || 0;
+        this.x = x;
 
         /**
          * @member {number}
          * @default 0
          */
-        this.y = y || 0;
+        this.y = y;
     }
 
     /**
