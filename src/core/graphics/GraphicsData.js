32f2fa5b627cb4b7a5a9e60d96345175a5881c49
Steffen Baer <stbaer.mail@gmail.com>
2016-09-24 11:02:14 +0800

    jsdoc comment style

--- src/core/graphics/GraphicsData.js
+++ src/core/graphics/GraphicsData.js
@@ -15,54 +15,54 @@ class GraphicsData
 {
     constructor(lineWidth, lineColor, lineAlpha, fillColor, fillAlpha, fill, shape)
     {
-        /*
+        /**
          * @member {number} the width of the line to draw
          */
         this.lineWidth = lineWidth;
 
-        /*
+        /**
          * @member {number} the color of the line to draw
          */
         this.lineColor = lineColor;
 
-        /*
+        /**
          * @member {number} the alpha of the line to draw
          */
         this.lineAlpha = lineAlpha;
 
-        /*
+        /**
          * @member {number} cached tint of the line to draw
          */
         this._lineTint = lineColor;
 
-        /*
+        /**
          * @member {number} the color of the fill
          */
         this.fillColor = fillColor;
 
-        /*
+        /**
          * @member {number} the alpha of the fill
          */
         this.fillAlpha = fillAlpha;
 
-        /*
+        /**
          * @member {number} cached tint of the fill
          */
         this._fillTint = fillColor;
 
-        /*
+        /**
          * @member {boolean} whether or not the shape is filled with a colour
          */
         this.fill = fill;
 
         this.holes = [];
 
-        /*
+        /**
          * @member {PIXI.Circle|PIXI.Ellipse|PIXI.Polygon|PIXI.Rectangle|PIXI.RoundedRectangle} The shape object to draw.
          */
         this.shape = shape;
 
-        /*
+        /**
          * @member {number} The type of the shape, see the Const.Shapes file for all the existing types,
          */
         this.type = shape.type;
