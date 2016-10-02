fdc9f7a8c3249bce93275f7ad50713346225082d
Lord Noriyuki <lordnoriyuki@gmail.com>
2016-10-01 17:17:05 -0700

    Local personal changes

--- src/core/renderers/webgl/WebGLRenderer.js
+++ src/core/renderers/webgl/WebGLRenderer.js
@@ -243,7 +243,7 @@ WebGLRenderer.prototype.render = function (displayObject, renderTexture, clear, 
 
     if(clear !== undefined ? clear : this.clearBeforeRender)
     {
-        this._activeRenderTarget.clear();
+        this._activeRenderTarget.clear(this._backgroundColorRgba); // @CHANGED LORD NORIYUKI
     }
 
     displayObject.renderWebGL(this);
