fb2fdf2eba56cbe5479b60d5e1767764c6c50a13
stafford <stafford.williams@gmail.com>
2016-10-05 13:20:58 +1100

    specify addChildAt expectations

--- test/core/Container.js
+++ test/core/Container.js
@@ -47,4 +47,42 @@ describe('PIXI.Container', function ()
             expect(triggeredRemoved).to.be.true;
         });
     });
+
+    describe('addChildAt', function ()
+    {
+        it('should allow placements at start', function ()
+        {
+            var container = new PIXI.Container();
+            var child = new PIXI.DisplayObject();
+
+            container.addChild(new PIXI.DisplayObject());
+            container.addChildAt(child, 0);
+
+            expect(container.children.length).to.be.equals(2);
+            expect(container.children[0]).to.be.equals(child);
+        });
+
+        it('should allow placements at end', function ()
+        {
+            var container = new PIXI.Container();
+            var child = new PIXI.DisplayObject();
+
+            container.addChild(new PIXI.DisplayObject());
+            container.addChildAt(child, 1);
+
+            expect(container.children.length).to.be.equals(2);
+            expect(container.children[1]).to.be.equals(child);
+        });
+
+        it('should throw on out-of-bounds', function ()
+        {
+            var container = new PIXI.Container();
+            var child = new PIXI.DisplayObject();
+
+            container.addChild(new PIXI.DisplayObject());
+
+            expect(function () { container.addChildAt(child, -1); }).to.throw('The index -1 supplied is out of bounds 1');
+            expect(function () { container.addChildAt(child, 2); }).to.throw('The index 2 supplied is out of bounds 1');
+        });
+    });
 });
