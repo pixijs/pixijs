e4624a52ba9fd8724c16fe2e5af3c760b9359ebf
AndrzejŻak <endru3@poczta.fm>
2016-10-16 17:39:14 +0200

    Fixing lint

--- test/core/Circle.js
+++ test/core/Circle.js
@@ -9,8 +9,8 @@ describe('PIXI.Circle', function ()
         expect(circ1.x).to.equal(0);
         expect(circ1.y).to.equal(0);
         expect(circ1.radius).to.equal(0);
-        
-        const circ2 = new PIXI.Circle(10,10,5);
+
+        const circ2 = new PIXI.Circle(10, 10, 5);
 
         expect(circ2.x).to.equal(10);
         expect(circ2.y).to.equal(10);
@@ -37,24 +37,22 @@ describe('PIXI.Circle', function ()
     {
         const circ1 = new PIXI.Circle(10, 10, 5);
 
-        expect(circ1.contains(10,10)).to.be.true;
-        expect(circ1.contains(10,15)).to.be.true;
-        expect(circ1.contains(15,10)).to.be.true;
-        expect(circ1.contains(5,10)).to.be.true;
-        expect(circ1.contains(15,10)).to.be.true;
+        expect(circ1.contains(10, 10)).to.be.true;
+        expect(circ1.contains(10, 15)).to.be.true;
+        expect(circ1.contains(15, 10)).to.be.true;
+        expect(circ1.contains(5, 10)).to.be.true;
+        expect(circ1.contains(15, 10)).to.be.true;
 
-        expect(circ1.contains(6,7)).to.be.true;
-        expect(circ1.contains(7,6)).to.be.true;
-        expect(circ1.contains(7,7)).to.be.true;
-        expect(circ1.contains(13,14)).to.be.true;
-        expect(circ1.contains(14,13)).to.be.true;
+        expect(circ1.contains(6, 7)).to.be.true;
+        expect(circ1.contains(7, 6)).to.be.true;
+        expect(circ1.contains(7, 7)).to.be.true;
+        expect(circ1.contains(13, 14)).to.be.true;
+        expect(circ1.contains(14, 13)).to.be.true;
 
-        expect(circ1.contains(14,14)).to.be.false;
-        expect(circ1.contains(10,16)).to.be.false;
-        expect(circ1.contains(11,15)).to.be.false;
-        expect(circ1.contains(0,0)).to.be.false;
-        
-        
+        expect(circ1.contains(14, 14)).to.be.false;
+        expect(circ1.contains(10, 16)).to.be.false;
+        expect(circ1.contains(11, 15)).to.be.false;
+        expect(circ1.contains(0, 0)).to.be.false;
     });
 
     it('should return framing rectangle', function ()
@@ -67,5 +65,4 @@ describe('PIXI.Circle', function ()
         expect(rect1.right).to.equal(15);
         expect(rect1.bottom).to.equal(15);
     });
-
 });
--- test/core/Rectangle.js
+++ test/core/Rectangle.js
@@ -47,39 +47,38 @@ describe('PIXI.Rectangle', function ()
     {
         const rect1 = new PIXI.Rectangle(10, 10, 10, 10);
 
-        expect(rect1.contains(10,10)).to.be.true;
-        expect(rect1.contains(10,19)).to.be.true;
-        expect(rect1.contains(19,10)).to.be.true;
-        expect(rect1.contains(15,15)).to.be.true;
-        expect(rect1.contains(10,9)).to.be.false;
-        expect(rect1.contains(9,10)).to.be.false;
-        expect(rect1.contains(10,20)).to.be.false;
-        expect(rect1.contains(20,10)).to.be.false;
-        expect(rect1.contains(21,21)).to.be.false;
-        
-        const rect2 = new PIXI.Rectangle(10, 10, 10, 0);
-        
-        expect(rect2.contains(10,10)).to.be.false;
-        expect(rect2.contains(20,20)).to.be.false;
-        expect(rect2.contains(15,15)).to.be.false;
-        expect(rect2.contains(10,9)).to.be.false;
-        expect(rect2.contains(9,10)).to.be.false;
-        expect(rect2.contains(21,21)).to.be.false;
+        expect(rect1.contains(10, 10)).to.be.true;
+        expect(rect1.contains(10, 19)).to.be.true;
+        expect(rect1.contains(19, 10)).to.be.true;
+        expect(rect1.contains(15, 15)).to.be.true;
+        expect(rect1.contains(10, 9)).to.be.false;
+        expect(rect1.contains(9, 10)).to.be.false;
+        expect(rect1.contains(10, 20)).to.be.false;
+        expect(rect1.contains(20, 10)).to.be.false;
+        expect(rect1.contains(21, 21)).to.be.false;
 
+        const rect2 = new PIXI.Rectangle(10, 10, 10, 0);
+
+        expect(rect2.contains(10, 10)).to.be.false;
+        expect(rect2.contains(20, 20)).to.be.false;
+        expect(rect2.contains(15, 15)).to.be.false;
+        expect(rect2.contains(10, 9)).to.be.false;
+        expect(rect2.contains(9, 10)).to.be.false;
+        expect(rect2.contains(21, 21)).to.be.false;
     });
 
     it('should make rectangle grow', function ()
     {
         const rect = new PIXI.Rectangle(10, 10, 10, 10);
 
-        rect.pad(1,1);
+        rect.pad(1, 1);
 
         expect(rect.left).to.equal(9);
         expect(rect.top).to.equal(9);
         expect(rect.right).to.equal(21);
         expect(rect.bottom).to.equal(21);
-        
-        rect.pad(10,10);
+
+        rect.pad(10, 10);
 
         expect(rect.left).to.equal(-1);
         expect(rect.top).to.equal(-1);
