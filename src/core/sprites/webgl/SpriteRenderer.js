cc58b0e3ae669bbc8fcf0899c060b72b35b33ada
Stafford Williams <stafford.williams@gmail.com>
2016-11-16 02:28:38 +1100

    allow immediate destroy

--- src/core/sprites/webgl/SpriteRenderer.js
+++ src/core/sprites/webgl/SpriteRenderer.js
@@ -442,11 +442,20 @@ export default class SpriteRenderer extends ObjectRenderer
     {
         for (let i = 0; i < this.vaoMax; i++)
         {
-            this.vertexBuffers[i].destroy();
-            this.vaos[i].destroy();
+            if (this.vertexBuffers[i])
+            {
+                this.vertexBuffers[i].destroy();
+            }
+            if (this.vaos[i])
+            {
+                this.vaos[i].destroy();
+            }
         }
 
-        this.indexBuffer.destroy();
+        if (this.indexBuffer)
+        {
+            this.indexBuffer.destroy();
+        }
 
         this.renderer.off('prerender', this.onPrerender, this);
 
--- test/core/SpriteRenderer.js
+++ test/core/SpriteRenderer.js
@@ -11,10 +11,23 @@ describe('SpriteRenderer', () =>
         };
         const renderer = new PIXI.SpriteRenderer(webgl);
 
+        // simulate onContextChange
         renderer.vertexBuffers = [destroyable, destroyable];
         renderer.vaos = [destroyable, destroyable];
         renderer.indexBuffer = destroyable;
         renderer.shader = destroyable;
+
+        expect(() => renderer.destroy()).to.not.throw();
+    });
+
+    it('can be destroyed immediately', () =>
+    {
+        const webgl = {
+            on: sinon.stub(),
+            off: sinon.stub(),
+        };
+
+        const renderer = new PIXI.SpriteRenderer(webgl);
 
         expect(() => renderer.destroy()).to.not.throw();
     });
