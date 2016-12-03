cf48efd2794f79c97802633bd83e71ba305f7f54
Stafford Williams <staff0rd@users.noreply.github.com>
2016-12-04 02:42:32 +1100

    Reduce Graphics.getlocalBounds test (#3397)
    * remove superfluous comments
    
    * reduced graphics.getLocalBounds test

--- test/core/getLocalBounds.js
+++ test/core/getLocalBounds.js
@@ -38,9 +38,7 @@ describe('getLocalBounds', function ()
 
         const graphics = new PIXI.Graphics();
 
-        graphics.beginFill(0xFF0000).drawCircle(0, 0, 10);// texture);
-
-        graphics.scale.set(2);
+        graphics.beginFill(0xFF0000).drawCircle(0, 0, 10);
 
         parent.addChild(graphics);
 
@@ -56,7 +54,7 @@ describe('getLocalBounds', function ()
     {
         const parent = new PIXI.Container();
 
-        const container = new PIXI.Container();// Graphics().beginFill(0xFF0000).drawCircle(0, 0, 10, 10);//texture);
+        const container = new PIXI.Container();
 
         parent.addChild(container);
 
@@ -72,9 +70,9 @@ describe('getLocalBounds', function ()
     {
         const parent = new PIXI.Container();
 
-        const container = new PIXI.Container();// Graphics().beginFill(0xFF0000).drawCircle(0, 0, 10, 10);//texture);
+        const container = new PIXI.Container();
 
-        const graphics = new PIXI.Graphics().beginFill(0xFF0000).drawRect(0, 0, 10, 10);// texture);
+        const graphics = new PIXI.Graphics().beginFill(0xFF0000).drawRect(0, 0, 10, 10);
 
         parent.addChild(container);
         container.addChild(graphics);
