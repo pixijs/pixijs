0605bdd555058a4f0a86516d3d00a4abf4e5e1d3
Stafford Williams <stafford.williams@gmail.com>
2016-12-03 22:18:14 +1100

    fixes #3274

--- src/core/graphics/Graphics.js
+++ src/core/graphics/Graphics.js
@@ -691,6 +691,7 @@ export default class Graphics extends Container
             this.lineWidth = 0;
             this.filling = false;
 
+            this.boundsDirty = -1;
             this.dirty++;
             this.clearDirty++;
             this.graphicsData.length = 0;
