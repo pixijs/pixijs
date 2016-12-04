6e87dc8422dac23028aae05bf272ef8eb7ae0e21
finscn <fins@163.com>
2016-12-05 03:48:20 +0800

    Update TransformStatic.js
    remove the trailing-spaces.

--- src/core/display/TransformStatic.js
+++ src/core/display/TransformStatic.js
@@ -135,7 +135,7 @@ export default class TransformStatic extends TransformBase
             // concat the parent matrix with the objects transform.
             const pt = parentTransform.worldTransform;
             const wt = this.worldTransform;
-            
+
             wt.a = (lt.a * pt.a) + (lt.b * pt.c);
             wt.b = (lt.a * pt.b) + (lt.b * pt.d);
             wt.c = (lt.c * pt.a) + (lt.d * pt.c);
