3544ebe652a4a36647dd32c3d2426ae6db011d6a
Matt Karl <bigtimebuddy@users.noreply.github.com>
2017-01-17 15:24:10 -0500

    Namespace Doc Updates (#3603)
    * Update namespaces in docs
    
    * Replaces method with memberof
    
    * Fixes linting errors

--- src/accessibility/AccessibilityManager.js
+++ src/accessibility/AccessibilityManager.js
@@ -31,7 +31,7 @@ const DIV_HOOK_ZINDEX = 2;
  * An instance of this class is automatically created by default, and can be found at renderer.plugins.accessibility
  *
  * @class
- * @memberof PIXI
+ * @memberof PIXI.accessibility
  */
 export default class AccessibilityManager
 {
