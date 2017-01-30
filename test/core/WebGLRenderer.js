b0aec8b342a6179da7a3a6429b8803fc83ef312a
Matt Karl <matt.karl@jibo.com>
2017-01-30 12:34:54 -0500

    Better check for WebGL in tests

--- test/core/WebGLRenderer.js
+++ test/core/WebGLRenderer.js
@@ -1,17 +1,19 @@
 'use strict';
 
-if (PIXI.utils.isWebGLSupported())
+function isWebGLSupported(fn)
 {
-    describe('PIXI.WebGLRenderer', function ()
-    {
-        it('setting option legacy should disable VAOs and set minimum SPRITE_MAX_TEXTURES to 1', function ()
-        {
-            const renderer = new PIXI.WebGLRenderer(1, 1, { legacy: true });
-
-            expect(PIXI.glCore.VertexArrayObject.FORCE_NATIVE).to.equal(true);
-            expect(renderer.plugins.sprite.MAX_TEXTURES).to.equal(1);
-
-            renderer.destroy();
-        });
-    });
+    return PIXI.utils.isWebGLSupported() ? fn : function () {}; // eslint-disable-line no-empty-function
 }
+
+describe('PIXI.WebGLRenderer', isWebGLSupported(function ()
+{
+    it('setting option legacy should disable VAOs and set minimum SPRITE_MAX_TEXTURES to 1', function ()
+    {
+        const renderer = new PIXI.WebGLRenderer(1, 1, { legacy: true });
+
+        expect(PIXI.glCore.VertexArrayObject.FORCE_NATIVE).to.equal(true);
+        expect(renderer.plugins.sprite.MAX_TEXTURES).to.equal(1);
+
+        renderer.destroy();
+    });
+}));
