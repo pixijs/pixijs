95c0bacae8a08891d91ae4c4044c82a3b4b1a7dc
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2017-02-26 20:23:04 +0300

    default value for settings.SCALE_MODE was not respected in loader (#3787)

--- src/core/textures/Texture.js
+++ src/core/textures/Texture.js
@@ -424,8 +424,8 @@ export default class Texture extends EventEmitter
      * @return {PIXI.Texture} Output texture
      */
     static fromLoader(source, imageUrl, name)
-    {
-        const baseTexture = new BaseTexture(source, null, getResolutionOfUrl(imageUrl));
+        {
+        const baseTexture = new BaseTexture(source, undefined, getResolutionOfUrl(imageUrl));
         const texture = new Texture(baseTexture);
 
         baseTexture.imageUrl = imageUrl;
