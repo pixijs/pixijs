68209d68da8e71bbf6e65ee8ab964ceafa47a309
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2017-03-10 02:21:38 +0300

    fix canvas

--- src/mesh/canvas/CanvasMeshRenderer.js
+++ src/mesh/canvas/CanvasMeshRenderer.js
@@ -135,14 +135,24 @@ export default class MeshSpriteRenderer
         const textureWidth = base.width;
         const textureHeight = base.height;
 
-        const ut = mesh.uvTransform.mapCoord;
 
-        const u0 = ((uvs[index0] * ut.a) + (uvs[index0 + 1] * ut.c) + ut.tx) * base.width;
-        const u1 = ((uvs[index1] * ut.a) + (uvs[index1 + 1] * ut.c) + ut.tx) * base.width;
-        const u2 = ((uvs[index2] * ut.a) + (uvs[index2 + 1] * ut.c) + ut.tx) * base.width;
-        const v0 = ((uvs[index0] * ut.b) + (uvs[index0 + 1] * ut.d) + ut.ty) * base.height;
-        const v1 = ((uvs[index1] * ut.b) + (uvs[index1 + 1] * ut.d) + ut.ty) * base.height;
-        const v2 = ((uvs[index2] * ut.b) + (uvs[index2 + 1] * ut.d) + ut.ty) * base.height;
+        let u0 = uvs[index0] * base.width;
+        let u1 = uvs[index1] * base.width;
+        let u2 = uvs[index2] * base.width;
+        let v0 = uvs[index0 + 1] * base.height;
+        let v1 = uvs[index1 + 1] * base.height;
+        let v2 = uvs[index2 + 1] * base.height;
+
+        if (mesh.uploadUvTransform)
+        {
+            const ut = mesh._uvTransform.mapCoord;
+            u0 = ((uvs[index0] * ut.a) + (uvs[index0 + 1] * ut.c) + ut.tx) * base.width;
+            u1 = ((uvs[index1] * ut.a) + (uvs[index1 + 1] * ut.c) + ut.tx) * base.width;
+            u2 = ((uvs[index2] * ut.a) + (uvs[index2 + 1] * ut.c) + ut.tx) * base.width;
+            v0 = ((uvs[index0] * ut.b) + (uvs[index0 + 1] * ut.d) + ut.ty) * base.height;
+            v1 = ((uvs[index1] * ut.b) + (uvs[index1 + 1] * ut.d) + ut.ty) * base.height;
+            v2 = ((uvs[index2] * ut.b) + (uvs[index2 + 1] * ut.d) + ut.ty) * base.height;
+        }
 
         let x0 = vertices[index0];
         let x1 = vertices[index1];
