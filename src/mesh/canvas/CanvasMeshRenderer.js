c72e51b50567204da6b7013e267b1b8cceb11f26
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2017-03-10 09:46:28 +0300

    trailing spaces

--- src/mesh/canvas/CanvasMeshRenderer.js
+++ src/mesh/canvas/CanvasMeshRenderer.js
@@ -145,7 +145,7 @@ export default class MeshSpriteRenderer
         if (mesh.uploadUvTransform)
         {
             const ut = mesh._uvTransform.mapCoord;
-            
+
             u0 = ((uvs[index0] * ut.a) + (uvs[index0 + 1] * ut.c) + ut.tx) * base.width;
             u1 = ((uvs[index1] * ut.a) + (uvs[index1 + 1] * ut.c) + ut.tx) * base.width;
             u2 = ((uvs[index2] * ut.a) + (uvs[index2 + 1] * ut.c) + ut.tx) * base.width;
