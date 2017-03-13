06d4abff1cdb9f9a55cd45263f0197f9c317da8c
Andrew Start <start.andrew@gmail.com>
2017-03-13 11:38:27 -0400

    Fix preparation of destroyed items (#3802)
    * Added isDestroyed to DisplayObject, BaseTexture. Don't prepare destroyed items.
    
    * isDestroyed -> _destroyed, marked as private

--- src/core/textures/BaseTexture.js
+++ src/core/textures/BaseTexture.js
@@ -203,6 +203,15 @@ export default class BaseTexture extends EventEmitter
         }
 
         /**
+         * If the object has been destroyed via destroy(). If true, it should not be used.
+         *
+         * @member {boolean}
+         * @private
+         * @readonly
+         */
+        this._destroyed = false;
+
+        /**
          * Fired when a not-immediately-available source finishes loading.
          *
          * @protected
@@ -598,6 +607,8 @@ export default class BaseTexture extends EventEmitter
         this.source = null;
 
         this.dispose();
+
+        this._destroyed = true;
     }
 
     /**
