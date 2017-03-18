f8c43d99b282d0d38cafcfc30a099ab72af271fb
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2017-03-18 20:32:18 +0300

    huge improvements of Plane
    mesh trim
    
    ultimate rope - RopeSprite
    
    scale thingy
    
    no trim fix
    
    fix texture update
    
    Fix some bugs  about new mesh moudel. (#3849)
    
    * Fix `uvs of NineSlicePlane`
    
    * Add `refresh` when anchor update
    
    * fix doc
    
    * Update NineSlicePlane.js
    
    * Remove useless code
    
    * Add default value (0) of _width/_height
    
    * Add default value (0) of _width/_height
    * remove useless `refresh()`
    
    Rope works, calculated points are preserved.
    
    one can override Plane vertices generation and post-process
    
    brace next line

--- src/mesh/index.js
+++ src/mesh/index.js
@@ -7,3 +7,4 @@ export { default as CanvasMeshRenderer } from './canvas/CanvasMeshRenderer';
 export { default as Plane } from './Plane';
 export { default as NineSlicePlane } from './NineSlicePlane';
 export { default as Rope } from './Rope';
+export { default as RopePoint } from './RopePoint';
