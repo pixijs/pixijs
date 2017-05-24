81d14c3cad1a1fc9d294ab6819a4a69d420cc19f
finscn <fins@163.com>
2017-05-24 23:52:47 +0800

    Update MeshRenderer.js

--- src/mesh/webgl/MeshRenderer.js
+++ src/mesh/webgl/MeshRenderer.js
@@ -106,10 +106,6 @@ export default class MeshRenderer extends core.ObjectRenderer
 
         glData.vertexBuffer.upload(mesh.vertices);
 
-        const isTrimmed = texture.trim && (texture.trim.width < texture.orig.width
-            || texture.trim.height < texture.orig.height);
-        const shader = isTrimmed ? this.shaderTrim : this.shader;
-
         renderer.bindShader(shader);
 
         shader.uniforms.uSampler = renderer.bindTexture(texture);
