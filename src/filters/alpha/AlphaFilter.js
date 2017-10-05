737410246c94326ba3632f91cf021b65ffb71138
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2017-10-05 17:04:22 +0300

    better docs

--- src/filters/alpha/AlphaFilter.js
+++ src/filters/alpha/AlphaFilter.js
@@ -3,7 +3,17 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 /**
- * Simplest filter - applies alpha. Very handy.
+ * Simplest filter - applies alpha
+ *
+ * Use this instead of Container's alpha property to avoid visual layering of individual elements.
+ * AlphaFilter applies alpha evenly across the entire display object and any opaque elements it contains.
+ * If elements are not opaque, they will blend with each other anyway.
+ *
+ * Very handy if you want to use common features of all filters:
+ *
+ * 1. Assign a blendMode to this filter, blend all elements inside display object with background.
+ *
+ * 2. To use clipping in display coordinates, assign a filterArea to the same container that has this filter.
  *
  * @class
  * @extends PIXI.Filter
