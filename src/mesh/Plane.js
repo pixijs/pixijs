b973b42688220af0ed74dcb2c3ebc4dd1c3abb4f
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2017-10-08 22:33:04 +0300

    dirty as in Rope (#4354)

--- src/mesh/Plane.js
+++ src/mesh/Plane.js
@@ -94,6 +94,8 @@ export default class Plane extends Mesh
         this.uvs = new Float32Array(uvs);
         this.colors = new Float32Array(colors);
         this.indices = new Uint16Array(indices);
+
+        this.dirty++;
         this.indexDirty++;
 
         this.multiplyUvs();
