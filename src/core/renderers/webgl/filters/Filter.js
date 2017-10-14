9e121fd8df57be51a2900bd0e6471f5bddbc4f64
Sascha Gehlich <sascha@gehlich.us>
2017-10-14 17:35:20 +0200

    Rename `sample` variable in default fragment shader
    `sample` is a reserved word in OpenGL

--- src/core/renderers/webgl/filters/Filter.js
+++ src/core/renderers/webgl/filters/Filter.js
@@ -183,7 +183,7 @@ export default class Filter
 
             'void main(void){',
             '   vec4 masky = texture2D(filterSampler, vFilterCoord);',
-            '   vec4 sample = texture2D(uSampler, vTextureCoord);',
+            '   vec4 _sample = texture2D(uSampler, vTextureCoord);',
             '   vec4 color;',
             '   if(mod(vFilterCoord.x, 1.0) > 0.5)',
             '   {',
@@ -194,8 +194,8 @@ export default class Filter
             '     color = vec4(0.0, 1.0, 0.0, 1.0);',
             '   }',
             // '   gl_FragColor = vec4(mod(vFilterCoord.x, 1.5), vFilterCoord.y,0.0,1.0);',
-            '   gl_FragColor = mix(sample, masky, 0.5);',
-            '   gl_FragColor *= sample.a;',
+            '   gl_FragColor = mix(_sample, masky, 0.5);',
+            '   gl_FragColor *= _sample.a;',
             '}',
         ].join('\n');
     }
