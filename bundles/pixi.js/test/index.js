7f69cd1dc4bd976396c2a07444f5053ae3f9e03f
Matt Karl <matt@mattkarl.com>
2017-10-26 15:03:17 -0400

    Broke up tests

--- bundles/pixi.js/test/index.js
+++ bundles/pixi.js/test/index.js
@@ -3,17 +3,10 @@
 /* eslint-disable global-require */
 require('../');
 
-PIXI.utils.skipHello(); // hide banner
-
 describe('PIXI', function ()
 {
     it('should exist as a global object', function ()
     {
         expect(PIXI).to.be.an('object');
     });
-    require('./core');
-    require('./interaction');
-    require('./loaders');
-    require('./renders');
-    require('./prepare');
 });
