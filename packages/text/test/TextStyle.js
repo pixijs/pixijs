f8575db37d7d9c7bac73bac807462e28669179e0
Matt Karl <matt@mattkarl.com>
2017-10-31 12:45:42 -0400

    Fixes cherry-picking (#4385)

--- packages/text/test/TextStyle.js
+++ packages/text/test/TextStyle.js
@@ -53,8 +53,8 @@ describe('PIXI.TextStyle', function ()
 
     it('should not shared array / object references between different instances', function ()
     {
-        const defaultStyle = new PIXI.TextStyle();
-        const style = new PIXI.TextStyle();
+        const defaultStyle = new TextStyle();
+        const style = new TextStyle();
 
         expect(defaultStyle.fillGradientStops.length).to.equal(style.fillGradientStops.length);
         style.fillGradientStops.push(0);
