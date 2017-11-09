1d92aaa71fe3f799e3659b3f076ad7cf1d0527e7
Matt Karl <matt@mattkarl.com>
2017-11-08 20:16:38 -0500

    Fixes linting

--- packages/loaders/src/index.js
+++ packages/loaders/src/index.js
@@ -1,5 +1,5 @@
 /**
- * Reference to **{@link https://github.com/englercj/resource-loader 
+ * Reference to **{@link https://github.com/englercj/resource-loader
  * resource-loader}**'s Resource class.
  * @see http://englercj.github.io/resource-loader/Resource.html
  * @class Resource
