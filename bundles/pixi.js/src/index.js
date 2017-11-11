5658c6c706073376ec199162d58fe5d817cec0f7
Matt Karl <matt@mattkarl.com>
2017-11-11 12:35:32 -0500

    pixi-modern as a dependency is slow and bloated, undoing

--- bundles/pixi.js/src/index.js
+++ bundles/pixi.js/src/index.js
@@ -1,33 +1,94 @@
 import '@pixi/polyfill';
 
-export * from 'pixi-modern';
+export * from '@pixi/core';
+export * from '@pixi/app';
+export * from '@pixi/sprite';
+export * from '@pixi/spritesheet';
+export * from '@pixi/text';
+export * from '@pixi/text-bitmap';
+export * from '@pixi/graphics';
+export * from '@pixi/sprite-animated';
+export * from '@pixi/sprite-tiling';
+export * from '@pixi/math';
+export * from '@pixi/constants';
+export * from '@pixi/display';
+
+import { Renderer } from '@pixi/core';
+import * as interaction from '@pixi/interaction';
+import * as extract from '@pixi/extract';
+import * as prepare from '@pixi/prepare';
+import * as mesh from '@pixi/mesh';
+import { SpriteRenderer } from '@pixi/sprite';
+import { TilingSpriteRenderer } from '@pixi/sprite-tiling';
+import { GraphicsRenderer } from '@pixi/graphics';
+import * as accessibility from '@pixi/accessibility';
+import * as loaders from '@pixi/loaders';
+import * as filters from './filters';
+import * as utils from '@pixi/utils';
+import * as ticker from '@pixi/ticker';
+import { settings } from '@pixi/settings';
+
+Renderer.registerPlugin('accessibility', accessibility.AccessibilityManager);
+Renderer.registerPlugin('extract', extract.Extract);
+Renderer.registerPlugin('graphics', GraphicsRenderer);
+Renderer.registerPlugin('interaction', interaction.InteractionManager);
+Renderer.registerPlugin('mesh', mesh.MeshRenderer);
+Renderer.registerPlugin('prepare', prepare.Prepare);
+Renderer.registerPlugin('sprite', SpriteRenderer);
+Renderer.registerPlugin('tilingSprite', TilingSpriteRenderer);
+
+import '@pixi/mixin-cache-as-bitmap';
+import '@pixi/mixin-get-child-by-name';
+import '@pixi/mixin-get-global-position';
+import '@pixi/mixin-app-loader';
+
+utils.mixins.performMixins();
+
+/**
+ * Alias for {@link PIXI.loaders.shared}.
+ * @name loader
+ * @memberof PIXI
+ * @type {PIXI.loader.Loader}
+ */
+export const loader = loaders.shared;
+
+export {
+    accessibility,
+    extract,
+    filters,
+    interaction,
+    loaders,
+    mesh,
+    prepare,
+    utils,
+    ticker,
+    settings,
+};
+
 export * from '@pixi/canvas-renderer';
 export * from '@pixi/canvas-graphics';
 export * from '@pixi/canvas-mesh';
 export * from '@pixi/canvas-sprite';
 
-import { InteractionManager } from '@pixi/interaction';
+import { Application } from '@pixi/app';
 import { CanvasRenderer, autoDetectRenderer } from '@pixi/canvas-renderer';
 import { CanvasMeshRenderer } from '@pixi/canvas-mesh';
-import * as canvasExtract from '@pixi/canvas-extract';
 import { CanvasGraphicsRenderer } from '@pixi/canvas-graphics';
-import * as canvasPrepare from '@pixi/canvas-prepare';
 import { CanvasSpriteRenderer } from '@pixi/canvas-sprite';
-import { AccessibilityManager } from '@pixi/accessibility';
+import * as canvasExtract from '@pixi/canvas-extract';
+import * as canvasPrepare from '@pixi/canvas-prepare';
 
-CanvasRenderer.registerPlugin('accessibility', AccessibilityManager);
+CanvasRenderer.registerPlugin('accessibility', accessibility.AccessibilityManager);
 CanvasRenderer.registerPlugin('extract', canvasExtract.CanvasExtract);
 CanvasRenderer.registerPlugin('graphics', CanvasGraphicsRenderer);
-CanvasRenderer.registerPlugin('interaction', InteractionManager);
+CanvasRenderer.registerPlugin('interaction', interaction.InteractionManager);
 CanvasRenderer.registerPlugin('mesh', CanvasMeshRenderer);
 CanvasRenderer.registerPlugin('prepare', canvasPrepare.CanvasPrepare);
 CanvasRenderer.registerPlugin('sprite', CanvasSpriteRenderer);
 
-import { prepare, extract } from 'pixi-modern';
 Object.assign(prepare, canvasPrepare);
 Object.assign(extract, canvasExtract);
 
-import { Application } from '@pixi/app';
 Application.prototype.createRenderer = autoDetectRenderer;
 
 import '@pixi/canvas-sprite-tiling';
