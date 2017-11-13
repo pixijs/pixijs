34032818a98ab50c5f3b8583b9b39a5ea361943e
PaulB <github@superkeen.com>
2017-11-12 19:56:03 -0800

    Accessibility Enhancement: Allow Button Title and Hint Updates
    Previously if a Pixi game updated its accessibleTitle or accessibleHint after its initial creation, the change would not be reflected in the DOM.  Now, in these cases, it updates properly and also prioritizes any changes to the focused button to the scren reader.

--- src/accessibility/AccessibilityManager.js
+++ src/accessibility/AccessibilityManager.js
@@ -304,6 +304,18 @@ export default class AccessibilityManager
 
                     div.style.width = `${hitArea.width * sx}px`;
                     div.style.height = `${hitArea.height * sy}px`;
+
+                    // update button titles and hints if they exist and they've changed
+                    if (div.title !== child.accessibleTitle && child.accessibleTitle !== null)
+                    {
+                        div.title = child.accessibleTitle;
+                    }
+                    if (div.getAttribute('aria-label') !== child.accessibleHint
+                        && child.accessibleHint !== null
+                        && child.accessibleHint !== undefined)
+                    {
+                        div.setAttribute('aria-label', child.accessibleHint);
+                    }
                 }
             }
         }
@@ -365,6 +377,28 @@ export default class AccessibilityManager
             div.style.zIndex = DIV_TOUCH_ZINDEX;
             div.style.borderStyle = 'none';
 
+            // ARIA attributes ensure that button title and hint updates are announced properly
+            if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1)
+            {
+                // Chrome doesn't need aria-live to work as intended; in fact it just gets more confused.
+                div.setAttribute('aria-live', 'off');
+            }
+            else
+            {
+                div.setAttribute('aria-live', 'polite');
+            }
+
+            if (navigator.userAgent.match(/rv:.*Gecko\//))
+            {
+                // FireFox needs this to announce only the new button name
+                div.setAttribute('aria-relevant', 'additions');
+            }
+            else
+            {
+                // required by IE, other browsers don't much care
+                div.setAttribute('aria-relevant', 'text');
+            }
+
             div.addEventListener('click', this._onClick.bind(this));
             div.addEventListener('focus', this._onFocus.bind(this));
             div.addEventListener('focusout', this._onFocusOut.bind(this));
@@ -420,6 +454,10 @@ export default class AccessibilityManager
      */
     _onFocus(e)
     {
+        if (!e.target.getAttribute('aria-live', 'off'))
+        {
+            e.target.setAttribute('aria-live', 'assertive');
+        }
         const interactionManager = this.renderer.plugins.interaction;
 
         interactionManager.dispatchEvent(e.target.displayObject, 'mouseover', interactionManager.eventData);
@@ -433,6 +471,10 @@ export default class AccessibilityManager
      */
     _onFocusOut(e)
     {
+        if (!e.target.getAttribute('aria-live', 'off'))
+        {
+            e.target.setAttribute('aria-live', 'polite');
+        }
         const interactionManager = this.renderer.plugins.interaction;
 
         interactionManager.dispatchEvent(e.target.displayObject, 'mouseout', interactionManager.eventData);
