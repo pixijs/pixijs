824fbf33cab5dde600c9a811e616b04f7e929175
J.C <jcyuan.space@gmail.com>
2017-11-13 13:57:07 +0800

    Update TextMetrics.js

--- src/core/text/TextMetrics.js
+++ src/core/text/TextMetrics.js
@@ -106,7 +106,7 @@ export default class TextMetrics
         // Greedy wrapping algorithm that will wrap words as the line grows longer
         // than its horizontal bounds.
         let result = '';
-        let firstChar = text.charAt(0);
+        const firstChar = text.charAt(0);
         const lines = text.split('\n');
         const wordWrapWidth = style.wordWrapWidth;
         const characterCache = {};
@@ -143,7 +143,7 @@ export default class TextMetrics
                         }
                         else
                         {
-                            if (c === 0 && (j > 0 || firstChar == ' ')) {
+                            if (c === 0 && (j > 0 || firstChar === ' ')) {
                                 result += ' ';
                             }
 
