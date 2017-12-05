68f0e1bd3a969ce237e95dc58ab066e25382ca1a
Matt Karl <matt@mattkarl.com>
2017-12-05 15:46:55 -0500

    Adds documentation to CubeTexture

--- packages/core/src/textures/CubeTexture.js
+++ packages/core/src/textures/CubeTexture.js
@@ -10,8 +10,16 @@ import CubeResource from './resources/CubeResource';
  */
 export default class CubeTexture extends BaseTexture
 {
-    static from(...urls)
+    /**
+     * Generate a new CubeTexture.
+     * @static
+     * @param {string[]|PIXI.resources.Resource[]} resources - Collection of 6 URLs or resources
+     * @param {object} [options] - Optional options passed to the resources being loaded.
+     *        See {@PIXI.resources.autoDetectResource autoDetectResource} for more info
+     *        on the options available to each resource.
+     */
+    static from(resources, options)
     {
-        return new CubeTexture(new CubeResource(urls));
+        return new CubeTexture(new CubeResource(resources, options));
     }
 }
