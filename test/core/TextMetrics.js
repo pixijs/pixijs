14e93e471c396291c71b95a9faf64cf19a2a54cb
Brendon Blackwell <brendonblackwell@gmail.com>
2017-12-21 12:31:21 +0100

    Added some more tests to catch a few more cases

--- test/core/TextMetrics.js
+++ test/core/TextMetrics.js
@@ -2,61 +2,179 @@
 
 describe('PIXI.TextMetrics', function ()
 {
-    it('width should not be greater than wordWrapWidth with breakWords disabled', function ()
-    {
-        const width = 200;
+    const longText = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+    const breakingWordText = `Pixi.js - The HTML5 Creation Engine. \nCreate beautiful digital content with the supercalifragilisticexpialidociously fastest, most flexible 2D WebGL renderer.`;
+    const fillText = '. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ';
 
-        const style = new PIXI.TextStyle({
-            align: 'center',
-            breakWords: false,
-            dropShadow: true,
-            dropShadowColor: 0x000000,
-            dropShadowDistance: 3,
-            fill: 0xFF66CC,
-            fontFamily: 'Arial',
-            fontSize: 20,
-            fontStyle: 'italic',
-            fontVariant: 'normal',
-            fontWeight: 900,
-            wordWrap: true,
-            wordWrapWidth: width,
-            letterSpacing: 4,
+    describe('wordWrap without breakWords', function ()
+    {
+        it('width should not be greater than wordWrapWidth with longText', function ()
+        {
+            const width = 200;
+
+            const style = new PIXI.TextStyle({
+                breakWords: false,
+                fontFamily: 'Arial',
+                fontSize: 20,
+                fontStyle: 'italic',
+                fontVariant: 'normal',
+                fontWeight: 900,
+                wordWrap: true,
+                wordWrapWidth: width,
+                letterSpacing: 4,
+            });
+
+            const metrics = PIXI.TextMetrics.measureText(longText, style);
+
+            expect(metrics.width).to.be.below(width);
+
+            metrics.lines.forEach((line) => {
+                expect(line[0]).to.not.equal(' ', 'should not have space at the start');
+                expect(line[line-1]).to.not.equal(' ', 'should not have space at the end');
+            })
         });
 
-        const text = `Pixi.js - The HTML5 Creation Engine.
-        Create beautiful digital content with the fastest, most flexible 2D WebGL renderer.`;
+        it('width should be greater than wordWrapWidth with breakingWordText', function ()
+        {
+            const width = 200;
 
-        const metrics = PIXI.TextMetrics.measureText(text, style);
+            const style = new PIXI.TextStyle({
+                breakWords: false,
+                fontFamily: 'Arial',
+                fontSize: 20,
+                fontStyle: 'italic',
+                fontVariant: 'normal',
+                fontWeight: 900,
+                wordWrap: true,
+                wordWrapWidth: width,
+                letterSpacing: 4,
+            });
 
-        expect(metrics.width).to.be.below(width);
+            const metrics = PIXI.TextMetrics.measureText(breakingWordText, style);
+
+            expect(metrics.width).to.be.above(width);
+
+            metrics.lines.forEach((line) => {
+                expect(line[0]).to.not.equal(' ', 'should not have space at the start');
+                expect(line[line-1]).to.not.equal(' ', 'should not have space at the end');
+            })
+        });
+
+        it('width should be within a character width from wordWrapWidth with fillText', function ()
+        {
+            const charWidth = 4; // it should fill the line to at lease width -4
+            const width = 200;
+
+            const style = new PIXI.TextStyle({
+                breakWords: false,
+                fontFamily: 'Arial',
+                fontSize: 20,
+                fontStyle: 'italic',
+                fontVariant: 'normal',
+                fontWeight: 900,
+                wordWrap: true,
+                wordWrapWidth: width,
+                letterSpacing: 4,
+            });
+
+            const metrics = PIXI.TextMetrics.measureText(fillText, style);
+
+            expect(metrics.width).to.be.below(width);
+            expect(metrics.width+charWidth).to.be.above(width);
+
+            metrics.lines.forEach((line) => {
+                expect(line[0]).to.not.equal(' ', 'should not have space at the start');
+                expect(line[line-1]).to.not.equal(' ', 'should not have space at the end');
+            })
+        });
+
     });
 
-    it('width should not be greater than wordWrapWidth with breakWords enabled', function ()
+    describe('wordWrap with breakWords', function ()
     {
-        const width = 200;
+        it('width should not be greater than wordWrapWidth with longText', function ()
+        {
+            const width = 200;
 
-        const style = new PIXI.TextStyle({
-            align: 'center',
-            breakWords: true,
-            dropShadow: true,
-            dropShadowColor: 0x000000,
-            dropShadowDistance: 3,
-            fill: 0xFF66CC,
-            fontFamily: 'Arial',
-            fontSize: 20,
-            fontStyle: 'italic',
-            fontVariant: 'normal',
-            fontWeight: 900,
-            wordWrap: true,
-            wordWrapWidth: width,
-            letterSpacing: 4,
+            const style = new PIXI.TextStyle({
+                breakWords: true,
+                fontFamily: 'Arial',
+                fontSize: 20,
+                fontStyle: 'italic',
+                fontVariant: 'normal',
+                fontWeight: 900,
+                wordWrap: true,
+                wordWrapWidth: width,
+                letterSpacing: 4,
+            });
+
+            const metrics = PIXI.TextMetrics.measureText(longText, style);
+
+            expect(metrics.width).to.be.below(width);
+
+            metrics.lines.forEach((line) => {
+                expect(line[0]).to.not.equal(' ', 'should not have space at the start');
+                expect(line[line-1]).to.not.equal(' ', 'should not have space at the end');
+            })
         });
 
-        const text = `Pixi.js - The HTML5 Creation Engine.
-        Create beautiful digital content with the fastest, most flexible 2D WebGL renderer.`;
+        it('width should not be greater than wordWrapWidth with breakingWordAtStartText', function ()
+        {
+            const width = 200;
 
-        const metrics = PIXI.TextMetrics.measureText(text, style);
+            const style = new PIXI.TextStyle({
+                breakWords: true,
+                fontFamily: 'Arial',
+                fontSize: 20,
+                fontStyle: 'italic',
+                fontVariant: 'normal',
+                fontWeight: 900,
+                wordWrap: true,
+                wordWrapWidth: width,
+                letterSpacing: 4,
+            });
 
-        expect(metrics.width).to.be.below(width);
+            const metrics = PIXI.TextMetrics.measureText(breakingWordText, style);
+
+            // console.log(metrics)
+
+
+            expect(metrics.width).to.be.below(width);
+
+            metrics.lines.forEach((line) => {
+                expect(line[0]).to.not.equal(' ', 'should not have space at the start');
+                expect(line[line-1]).to.not.equal(' ', 'should not have space at the end');
+            })
+        });
+
+        it('width should be within a character width from wordWrapWidth with fillText', function ()
+        {
+            const charWidth = 4; // it should fill the line to at lease width -4
+            const width = 200;
+
+            const style = new PIXI.TextStyle({
+                breakWords: true,
+                fontFamily: 'Arial',
+                fontSize: 20,
+                fontStyle: 'italic',
+                fontVariant: 'normal',
+                fontWeight: 900,
+                wordWrap: true,
+                wordWrapWidth: width,
+                letterSpacing: 4,
+            });
+
+            const metrics = PIXI.TextMetrics.measureText(fillText, style);
+
+            expect(metrics.width).to.be.below(width);
+            expect(metrics.width+charWidth).to.be.above(width);
+
+            metrics.lines.forEach((line) => {
+                expect(line[0]).to.not.equal(' ', 'should not have space at the start');
+                expect(line[line-1]).to.not.equal(' ', 'should not have space at the end');
+            })
+
+        });
+
     });
 });
