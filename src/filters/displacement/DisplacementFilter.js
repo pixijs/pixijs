ea6db0427aea1a4e152e24457b4bb88e2b918799
finscn <finscn@gmail.com>
2018-01-04 11:01:24 +0800

    Update DisplacementFilter.js

--- src/filters/displacement/DisplacementFilter.js
+++ src/filters/displacement/DisplacementFilter.js
@@ -56,11 +56,12 @@ export default class DisplacementFilter extends core.Filter
      */
     apply(filterManager, input, output)
     {
-        const ratio =  (1 / output.destinationFrame.width) * (output.sourceFrame.width / input.sourceFrame.width);
+        this.uniforms.dimensions[0] = input.sourceFrame.width;
+        this.uniforms.dimensions[1] = input.sourceFrame.height;
 
         this.uniforms.filterMatrix = filterManager.calculateSpriteMatrix(this.maskMatrix, this.maskSprite);
-        this.uniforms.scale.x = this.scale.x * ratio;
-        this.uniforms.scale.y = this.scale.y * ratio;
+        this.uniforms.scale.x = this.scale.x;
+        this.uniforms.scale.y = this.scale.y;
 
          // draw the filter...
         filterManager.applyFilter(this, input, output);
