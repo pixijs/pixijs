8f535bb23400b366305ae3de42c1013f054855ac
cec <tsvetan.stanislavov@cayetanogaming.com>
2018-01-26 16:38:17 +0200

    Fixed _maxWidth used instead of _letterSpacing.

--- src/extras/BitmapText.js
+++ src/extras/BitmapText.js
@@ -113,7 +113,7 @@ export default class BitmapText extends core.Container
         /**
          * Letter spacing. This is useful for setting the space between characters.
          * @member {number}
-         * @public
+         * @private
          */
         this._letterSpacing = 0;
 
@@ -156,7 +156,6 @@ export default class BitmapText extends core.Container
         let lastSpaceWidth = 0;
         let spacesRemoved = 0;
         let maxLineHeight = 0;
-        let currentChar;
 
         for (let i = 0; i < this.text.length; i++)
         {
@@ -213,7 +212,7 @@ export default class BitmapText extends core.Container
                 texture: charData.texture,
                 line,
                 charCode,
-                position: new core.Point(pos.x + charData.xOffset + this._letterSpacing / 2, pos.y + charData.yOffset),
+                position: new core.Point(pos.x + charData.xOffset + (this._letterSpacing / 2), pos.y + charData.yOffset),
             });
             pos.x += charData.xAdvance + this._letterSpacing;
             lastLineWidth = pos.x;
@@ -492,14 +491,19 @@ export default class BitmapText extends core.Container
         return this._textWidth;
     }
 
+    /**
+     * Additional space between characters.
+     *
+     * @member {number}
+     */
     get letterSpacing()
     {
         return this._letterSpacing;
     }
 
-    set letterSpacing(value)
+    set letterSpacing(value) // eslint-disable-line require-jsdoc
     {
-        if (this._maxWidth !== value)
+        if (this._letterSpacing !== value)
         {
             this._letterSpacing = value;
             this.dirty = true;
