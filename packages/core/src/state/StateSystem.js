2a19bb829c5ec25ce420d027ed522a29e82ce99b
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2018-03-02 17:24:14 +0300

    Re-adds missing blendFuncSeparate from StateSystem (#4728)

--- packages/core/src/state/StateSystem.js
+++ packages/core/src/state/StateSystem.js
@@ -188,7 +188,17 @@ export default class StateSystem extends System
         }
 
         this.blendMode = value;
-        this.gl.blendFunc(this.blendModes[value][0], this.blendModes[value][1]);
+
+        const mode = this.blendModes[value];
+
+        if (mode.length === 2)
+        {
+            this.gl.blendFunc(mode[0], mode[1]);
+        }
+        else
+        {
+            this.gl.blendFuncSeparate(mode[0], mode[1], mode[2], mode[3]);
+        }
     }
 
     /**
