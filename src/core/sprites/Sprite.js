25dd30f07bb3804fa47f737fed4f3b71b6eccaf8
Matt Karl <matt@mattkarl.com>
2018-05-25 16:50:33 -0400

    Fixes listener not getting removed

--- src/core/sprites/Sprite.js
+++ src/core/sprites/Sprite.js
@@ -418,6 +418,8 @@ export default class Sprite extends Container
     {
         super.destroy(options);
 
+        this._texture.off('update', this._onTextureUpdate, this);
+
         this._anchor = null;
 
         const destroyTexture = typeof options === 'boolean' ? options : options && options.texture;
