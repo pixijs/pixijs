f8b5f5eb2893da6e316b94be5c2b696fbf9bbf38
Matt Karl <matt@mattkarl.com>
2018-05-25 16:11:43 -0400

    Reverts doc changes

--- packages/app/src/Application.js
+++ packages/app/src/Application.js
@@ -23,32 +23,32 @@ import { Renderer } from '@pixi/core';
 export default class Application
 {
     // eslint-disable-next-line valid-jsdoc
-    /**
-     * @param {object} [options] - The optional renderer parameters
-     * @param {boolean} [options.autoStart=true] - automatically starts the rendering after the construction.
-     *     Note that setting this parameter to false does NOT stop the shared ticker even if you set
+        /**
+     * @param {object} [options] - The optional renderer parameters.
+     * @param {boolean} [options.autoStart=true] - Automatically starts the rendering after the construction.
+     *     **Note**: Setting this parameter to false does NOT stop the shared ticker even if you set
      *     options.sharedTicker to true in case that it is already started. Stop it by your own.
-     * @param {number} [options.width=800] - the width of the renderers view
-     * @param {number} [options.height=600] - the height of the renderers view
-     * @param {HTMLCanvasElement} [options.view] - the canvas to use as a view, optional
-     * @param {boolean} [options.transparent=false] - If the render view is transparent, default false
+     * @param {number} [options.width=800] - The width of the renderers view.
+     * @param {number} [options.height=600] - The height of the renderers view.
+     * @param {HTMLCanvasElement} [options.view] - The canvas to use as a view, optional.
+     * @param {boolean} [options.transparent=false] - If the render view is transparent.
      * @param {boolean} [options.autoDensity=false] - Resizes renderer view in CSS pixels to allow for
-     *   resolutions other than 1
-     * @param {boolean} [options.antialias=false] - sets antialias (only applicable in chrome at the moment)
-     * @param {boolean} [options.preserveDrawingBuffer=false] - enables drawing buffer preservation, enable this if you
-     *  need to call toDataUrl on the webgl context
-     * @param {number} [options.resolution=1] - The resolution / device pixel ratio of the renderer, retina would be 2
-     * @param {boolean} [options.forceCanvas=false] - prevents selection of WebGL renderer, even if such is present
+     *   resolutions other than 1.
+     * @param {boolean} [options.antialias=false] - Sets antialias (only applicable in Chrome at the moment).
+     * @param {boolean} [options.preserveDrawingBuffer=false] - Enables drawing buffer preservation, enable this if you
+     *  need to call toDataUrl on the WebGL context.
+     * @param {number} [options.resolution=1] - The resolution / device pixel ratio of the renderer, retina would be 2.
+     * @param {boolean} [options.forceCanvas=false] - Prevents selection of WebGL renderer, even if such is present.
      * @param {number} [options.backgroundColor=0x000000] - The background color of the rendered area
      *  (shown if not transparent).
      * @param {boolean} [options.clearBeforeRender=true] - This sets if the renderer will clear the canvas or
      *   not before the new render pass.
      * @param {boolean} [options.roundPixels=false] - If true PixiJS will Math.floor() x/y values when rendering,
      *  stopping pixel interpolation.
-     * @param {boolean} [options.forceFXAA=false] - forces FXAA antialiasing to be used over native.
-     *  FXAA is faster, but may not always look as great **webgl only**
+     * @param {boolean} [options.forceFXAA=false] - Forces FXAA antialiasing to be used over native.
+     *  FXAA is faster, but may not always look as great. **(WebGL only)**.
      * @param {string} [options.powerPreference] - Parameter passed to webgl context, set to "high-performance"
-     *  for devices with dual graphics card **webgl only**
+     *  for devices with dual graphics card. **(WebGL only)**.
      * @param {boolean} [options.sharedTicker=false] - `true` to use PIXI.Ticker.shared, `false` to create new ticker.
      * @param {boolean} [options.sharedLoader=false] - `true` to use PIXI.Loaders.shared, `false` to create new Loader.
      * @param {Window|HTMLElement} [options.resizeTo] - Element to automatically resize stage to.
@@ -72,7 +72,7 @@ export default class Application
         }, options);
 
         /**
-         * WebGL renderer if available, otherwise CanvasRenderer
+         * WebGL renderer if available, otherwise CanvasRenderer.
          * @member {PIXI.Renderer|PIXI.CanvasRenderer}
          */
         this.renderer = this.createRenderer(options);
@@ -130,7 +130,7 @@ export default class Application
     }
 
     /**
-     * Reference to the renderer's screen rectangle. Its safe to use as filterArea or hitArea for whole screen
+     * Reference to the renderer's screen rectangle. Its safe to use as `filterArea` or `hitArea` for the whole screen.
      * @member {PIXI.Rectangle}
      * @readonly
      */
@@ -142,6 +142,14 @@ export default class Application
     /**
      * Destroy and don't use after this.
      * @param {Boolean} [removeView=false] Automatically remove canvas from DOM.
+     * @param {object|boolean} [stageOptions] - Options parameter. A boolean will act as if all options    
+     *  have been set to that value    
+     * @param {boolean} [stageOptions.children=false] - if set to true, all the children will have their destroy   
+     *  method called as well. 'stageOptions' will be passed on to those calls.    
+     * @param {boolean} [stageOptions.texture=false] - Only used for child Sprites if stageOptions.children is set 
+     *  to true. Should it destroy the texture of the child sprite 
+     * @param {boolean} [stageOptions.baseTexture=false] - Only used for child Sprites if stageOptions.children is set 
+     *  to true. Should it destroy the base texture of the child sprite    
      */
     destroy(removeView)
     {
