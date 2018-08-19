9387d786d8ff9dd256a3306b63b759f087e76166
Will <will@mbpro.local>
2018-08-19 21:37:54 +0900

    add base case for VideoBaseTexture

--- test/core/VideoBaseTexture.js
+++ test/core/VideoBaseTexture.js
@@ -1,12 +1,15 @@
 'use strict';
 
-const URL = '/test.mp4?123...';
+const URL1 = 'https://example.org/video.webm';
+const URL2 = '/test.mp4?123...';
 
 function cleanCache()
 {
-    delete PIXI.utils.BaseTextureCache[URL];
+    delete PIXI.utils.BaseTextureCache[URL1];
+    delete PIXI.utils.BaseTextureCache[URL2];
 
-    delete PIXI.utils.TextureCache[URL];
+    delete PIXI.utils.TextureCache[URL1];
+    delete PIXI.utils.TextureCache[URL2];
 }
 
 describe('PIXI.VideoBaseTexture', function ()
@@ -15,7 +18,16 @@ describe('PIXI.VideoBaseTexture', function ()
     {
         cleanCache();
 
-        const videoBaseTexture = new PIXI.VideoBaseTexture.fromUrl(URL);
+        const videoBaseTexture = new PIXI.VideoBaseTexture.fromUrl(URL1);
+
+        expect(videoBaseTexture.source.firstChild.type).to.be.equals('video/webm');
+    });
+
+    it('should get video extension without being thrown by query string', function ()
+    {
+        cleanCache();
+
+        const videoBaseTexture = new PIXI.VideoBaseTexture.fromUrl(URL2);
 
         expect(videoBaseTexture.source.firstChild.type).to.be.equals('video/mp4');
     });
