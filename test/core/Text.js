59c342397fde8326f534dd6d2116b106d17120bf
John Sjöström <johnSjo@users.noreply.github.com>
2018-08-21 17:34:26 +0200

    Fixes trimming a Text object with an empty string (#5045)

--- test/core/Text.js
+++ test/core/Text.js
@@ -109,6 +109,13 @@ describe('PIXI.Text', function ()
             expect(text.text).to.equal(' ');
         });
 
+        it('should trim an empty string', function ()
+        {
+            const text = new PIXI.Text('', { trim: true });
+
+            expect(text.text).to.equal(' ');
+        });
+
         it('should prevent setting \'\'', function ()
         {
             const text = new PIXI.Text('');
