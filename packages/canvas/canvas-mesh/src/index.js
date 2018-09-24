3eba381022b9ed8dcbff1f6fffaa419d070aefc6
Matt Karl <matt@mattkarl.com>
2018-09-24 15:58:48 -0700

    Fixes SimpleMesh mixins

--- packages/canvas/canvas-mesh/src/index.js
+++ packages/canvas/canvas-mesh/src/index.js
@@ -1,6 +1,6 @@
 export { default as CanvasMeshRenderer } from './CanvasMeshRenderer';
 
 import './settings';
-// import './Mesh2d';
-// import './NineSlicePlane';
-// import './Mesh';
+import './SimpleMesh';
+import './NineSlicePlane';
+import './Mesh';
