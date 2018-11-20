d74fce25da6230d8210484407c6c6e1c059edbb0
finscn <finscn@gmail.com>
2018-11-20 14:32:23 +0800

    Update BaseTexture.js

--- src/core/textures/BaseTexture.js
+++ src/core/textures/BaseTexture.js
@@ -730,7 +730,7 @@ export default class BaseTexture extends EventEmitter
             baseTexture = new BaseTexture(canvas, scaleMode);
             BaseTexture.addToCache(baseTexture, canvas._pixiId);
         }
-        else if (canvas.width !== baseTexture.realWidth || canvas.height !== baseTexture.realHeight)
+        else if (baseTexture.realWidth !== canvas.width || baseTexture.realHeight !== canvas.height)
         {
             baseTexture.update();
         }
