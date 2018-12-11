d3bb05217ae19ca93ba2e09536a16407bd42afa1
Matt Karl <matt@mattkarl.com>
2018-12-10 16:37:13 -0800

    Lint fix

--- packages/canvas/canvas-mesh/src/CanvasMeshRenderer.js
+++ packages/canvas/canvas-mesh/src/CanvasMeshRenderer.js
@@ -122,7 +122,7 @@ export default class CanvasMeshRenderer
     {
         const context = this.renderer.context;
         const vertices = mesh.geometry.buffers[0].data;
-        const {uvs, texture} = mesh;
+        const { uvs, texture } = mesh;
 
         if (!texture.valid)
         {
