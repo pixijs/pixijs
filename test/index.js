ca84d292e6f77998cd7c39ac278f9e8955373b79
Matt Karl <matt@mattkarl.com>
2019-01-05 12:47:03 -0800

    Updates build tool dependencies
    * Latest Rollup
    * Latest Lerna
    * Replace uglify-es with terser

--- test/index.js
+++ test/index.js
@@ -1,14 +1,39 @@
-const PackageUtilities = require('lerna/lib/PackageUtilities');
-const Repository = require('lerna/lib/Repository');
 const path = require('path');
+const batchPackages = require('@lerna/batch-packages');
+const filterPackages = require('@lerna/filter-packages');
+const { getPackages } = require('@lerna/project');
 
-// Standard Lerna plumbing getting packages
-const repo = new Repository(path.dirname(__dirname));
-const packages = PackageUtilities.getPackages(repo);
-
-// Look for tests in the packages
-packages.filter((pkg) => !!pkg.scripts.test).forEach((pkg) =>
+/**
+ * Get a list of the non-private sorted packages with Lerna v3
+ * @see https://github.com/lerna/lerna/issues/1848
+ * @return {Promise<Package[]>} List of packages
+ */
+async function getSortedPackages()
 {
-    // eslint-disable-next-line global-require
-    require(`${pkg.location}/test`);
+    // Standard Lerna plumbing getting packages
+    const packages = await getPackages(path.dirname(__dirname));
+    const filtered = filterPackages(packages);
+
+    return batchPackages(filtered)
+        .reduce((arr, batch) => arr.concat(batch), [])
+        .filter((pkg) => !!pkg.scripts.test);
+}
+
+/**
+ * Important: use Mocha as the entry point, inline async/await
+ * functions will be ignored by floss as the root level
+ * and all tests will be skipped.
+ */
+// eslint-disable-next-line func-names
+describe('PIXI', function ()
+{
+    // eslint-disable-next-line func-names
+    it('bootstrap all tests', async function ()
+    {
+        (await getSortedPackages()).forEach((pkg) =>
+        {
+            // eslint-disable-next-line global-require
+            require(`${pkg.location}/test`);
+        });
+    });
 });
