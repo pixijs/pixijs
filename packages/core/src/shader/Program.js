074f520254e9c2bc4b6a7f37008dcf89ad20560c
Mat Groves <mat@goodboydigital.com>
2019-01-08 21:48:02 +0000

    added a warning for auto detection of stride
    better naming of shader

--- packages/core/src/shader/Program.js
+++ packages/core/src/shader/Program.js
@@ -12,6 +12,8 @@ import { settings } from '@pixi/settings';
 
 let UID = 0;
 
+const nameCache = {};
+
 /**
  * @class
  * @memberof PIXI
@@ -43,8 +45,20 @@ class Program
 
         name = name.replace(/\s+/g, '-');
 
-        this.vertexSrc = `#define SHADER_NAME ${name}-${this.id}\n${this.vertexSrc}`;
-        this.fragmentSrc = `#define SHADER_NAME ${name}-${this.id}\n${this.fragmentSrc}`;
+        if (nameCache[name])
+        {
+            nameCache[name]++;
+            name += `-${nameCache[name]}`;
+        }
+        else
+        {
+            nameCache[name] = 1;
+        }
+
+        this.vertexSrc = `#define SHADER_NAME ${name}\n${this.vertexSrc}`;
+        this.fragmentSrc = `#define SHADER_NAME ${name}\n${this.fragmentSrc}`;
+
+        // console.log('settings: ', settings);
 
         this.vertexSrc = setPrecision(this.vertexSrc, settings.PRECISION_VERTEX);
         this.fragmentSrc = setPrecision(this.fragmentSrc, settings.PRECISION_FRAGMENT);
