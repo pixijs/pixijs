7ead4b99240a829cf4fac5441e2e7cdd3d00b26a
Matt Karl <matt@mattkarl.com>
2019-01-08 22:29:20 -0800

    Removes GL-only test gate

--- packages/mesh-extras/test/Plane.js
+++ packages/mesh-extras/test/Plane.js
@@ -6,15 +6,10 @@ const { RenderTexture, Texture } = require('@pixi/core');
 
 skipHello();
 
-function withGL(fn)
-{
-    return !process.env.DISABLE_WEBGL ? fn : undefined;
-}
-
 // TODO: fix with webglrenderer
 describe('PIXI.SimplePlane', function ()
 {
-    it('should create a plane from an external image', withGL(function (done)
+    it('should create a plane from an external image', function (done)
     {
         const loader = new Loader();
 
@@ -27,34 +22,34 @@ describe('PIXI.SimplePlane', function ()
                 expect(plane.geometry.segHeight).to.equal(100);
                 done();
             });
-    }));
+    });
 
-    it('should create a new empty textured SimplePlane', withGL(function ()
+    it('should create a new empty textured SimplePlane', function ()
     {
         const plane = new SimplePlane(Texture.EMPTY, 100, 100);
 
         expect(plane.geometry.segWidth).to.equal(100);
         expect(plane.geometry.segHeight).to.equal(100);
-    }));
+    });
 
     describe('containsPoint', function ()
     {
-        it('should return true when point inside', withGL(function ()
+        it('should return true when point inside', function ()
         {
             const point = new Point(10, 10);
             const texture = new RenderTexture.create(20, 30);
             const plane = new SimplePlane(texture, 100, 100);
 
             expect(plane.containsPoint(point)).to.be.true;
-        }));
+        });
 
-        it('should return false when point outside', withGL(function ()
+        it('should return false when point outside', function ()
         {
             const point = new Point(100, 100);
             const texture = new RenderTexture.create(20, 30);
             const plane = new SimplePlane(texture, 100, 100);
 
             expect(plane.containsPoint(point)).to.be.false;
-        }));
+        });
     });
 });
