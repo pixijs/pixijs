c5aeb5c2682e837c27d350b072a26462592ca246
Viktor Khokhryakov <viktor.khokhryakov@gmail.com>
2019-01-11 18:40:03 +0400

    Remove accessibility manager hook div from dom on destruction

--- packages/accessibility/src/AccessibilityManager.js
+++ packages/accessibility/src/AccessibilityManager.js
@@ -41,6 +41,11 @@ export default class AccessibilityManager
      */
     constructor(renderer)
     {
+        /**
+         * @type {?HTMLElement}
+         * @private
+         */
+        this._hookDiv = null;
         if ((Device.tablet || Device.phone) && !navigator.isCocoonJS)
         {
             this.createTouchHook();
@@ -157,10 +162,25 @@ export default class AccessibilityManager
         {
             this.isMobileAccessibility = true;
             this.activate();
-            document.body.removeChild(hookDiv);
+            this.destroyTouchHook();
         });
 
         document.body.appendChild(hookDiv);
+        this._hookDiv = hookDiv;
+    }
+
+    /**
+     * Destroys the touch hooks.
+     *
+     */
+    destroyTouchHook()
+    {
+        if (!this._hookDiv)
+        {
+            return;
+        }
+        document.body.removeChild(this._hookDiv);
+        this._hookDiv = null;
     }
 
     /**
@@ -529,6 +549,7 @@ export default class AccessibilityManager
      */
     destroy()
     {
+        this.destroyTouchHook();
         this.div = null;
 
         for (let i = 0; i < this.children.length; i++)
--- packages/accessibility/test/AccessibilityManager.js
+++ packages/accessibility/test/AccessibilityManager.js
@@ -1,5 +1,6 @@
 const { AccessibilityManager } = require('../');
 const { CanvasRenderer } = require('@pixi/canvas-renderer');
+const Device = require('ismobilejs');
 
 describe('PIXI.accessibility.AccessibilityManager', function ()
 {
@@ -24,5 +25,20 @@ describe('PIXI.accessibility.AccessibilityManager', function ()
 
         expect(renderer.plugins.accessibility).to.be.instanceof(AccessibilityManager);
         renderer.destroy();
+    });
+
+    it('should remove touch hook when destroyed', function ()
+    {
+        const phone = Device.phone;
+
+        Device.phone = true;
+        const manager = new AccessibilityManager();
+        const hookDiv = manager._hookDiv;
+
+        expect(hookDiv).to.be.instanceof(HTMLElement);
+        expect(document.body.contains(hookDiv)).to.be.true;
+        manager.destroy();
+        expect(document.body.contains(hookDiv)).to.be.false;
+        Device.phone = phone;
     });
 });
