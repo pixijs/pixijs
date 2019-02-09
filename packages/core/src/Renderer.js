9590e2d33b77c39aab4b29f1018a66fbc239d9a5
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2019-02-09 18:03:53 +0300

    defaults in render method

--- packages/core/src/Renderer.js
+++ packages/core/src/Renderer.js
@@ -296,9 +296,9 @@ export default class Renderer extends AbstractRenderer
      *
      * @param {PIXI.DisplayObject} displayObject - The object to be rendered.
      * @param {PIXI.RenderTexture} [renderTexture] - The render texture to render to.
-     * @param {boolean} [clear] - Should the canvas be cleared before the new render.
+     * @param {boolean} [clear=true] - Should the canvas be cleared before the new render.
      * @param {PIXI.Matrix} [transform] - A transform to apply to the render texture before rendering.
-     * @param {boolean} [skipUpdateTransform] - Should we skip the update transform pass?
+     * @param {boolean} [skipUpdateTransform=false] - Should we skip the update transform pass?
      */
     render(displayObject, renderTexture, clear, transform, skipUpdateTransform)
     {
