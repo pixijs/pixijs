39be656b6ab0284bfe0a6f80c92e306d14638d3f
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2019-02-12 02:43:42 +0300

    remove dependence on `null !== undefined`

--- packages/core/src/renderTexture/RenderTextureSystem.js
+++ packages/core/src/renderTexture/RenderTextureSystem.js
@@ -63,7 +63,7 @@ export default class RenderTextureSystem extends System
      * @param {PIXI.Rectangle} sourceFrame
      * @param {PIXI.Rectangle} destinationFrame
      */
-    bind(renderTexture, sourceFrame, destinationFrame)
+    bind(renderTexture = null, sourceFrame, destinationFrame)
     {
         // TODO - do we want this??
         if (this.current === renderTexture) return;
