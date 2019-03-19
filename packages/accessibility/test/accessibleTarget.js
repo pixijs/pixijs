4ce14e774d9c1eb7a6c5a075af338fc0eb56c840
Matt Karl <matt@mattkarl.com>
2019-03-19 14:17:51 -0700

    Mixin utils are no longer needed (#5529)

--- packages/accessibility/test/accessibleTarget.js
+++ packages/accessibility/test/accessibleTarget.js
@@ -1,21 +1,11 @@
-const { accessibleTarget } = require('../');
 const { DisplayObject } = require('@pixi/display');
-const { mixins } = require('@pixi/utils');
+
+require('../');
 
 describe('PIXI.accessibility.accessibleTarget', function ()
 {
     it('should have target public properties', function ()
     {
-        expect(accessibleTarget.accessible).to.be.a.boolean;
-        expect(accessibleTarget.accessible).to.be.false;
-        expect(accessibleTarget.accessibleTitle).to.be.null;
-        expect(accessibleTarget.accessibleHint).to.be.null;
-        expect(accessibleTarget.tabIndex).to.equal(0);
-    });
-
-    it('should add properties after mixin', function ()
-    {
-        mixins.performMixins();
         const obj = new DisplayObject();
 
         expect(obj.accessible).to.be.a.boolean;
