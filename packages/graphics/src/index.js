671b20429bd027484b32ff65c0d745b49d307d54
Avery Fay <avery@shadypixel.com>
2019-03-26 19:13:41 -0700

    Add note about memory leaks to Graphics; make GraphicsGeometry useable by endusers

--- packages/graphics/src/index.js
+++ packages/graphics/src/index.js
@@ -1,4 +1,6 @@
 export * from './const';
+export { default as FillStyle } from './styles/FillStyle';
 export { default as Graphics } from './Graphics';
 export { default as GraphicsData } from './GraphicsData';
 export { default as GraphicsGeometry } from './GraphicsGeometry';
+export { default as LineStyle } from './styles/LineStyle';
