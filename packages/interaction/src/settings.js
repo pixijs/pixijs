552f743e3040e5a5e13d3958b110b977edb1cfeb
Dave Moore <dave.moore@inseinc.com>
2019-04-24 12:27:11 +0100

    Defaulted to true, to keep v4 behaviour, which is better for surface tablets which don't report ontouchstart in window

--- packages/interaction/src/settings.js
+++ packages/interaction/src/settings.js
@@ -4,8 +4,9 @@ import { settings } from '@pixi/settings';
  * Should PixiJS use the PointerEvent API instead of MouseEvent API when generating interaction events.
  * This setting effects 'mouse' events only, not 'touch' events, which always use the TouchEvent API.
  * Advantages to this are using the modern api with the extra properties it comes with, such as 'pressure'.
- * Disadvantages are 'quirks' to how a PointerEvent works, such a down and up events not being fired if other
- * buttons are already in a down state.
+ * It also allows 'touch' to work on Surface laptops that don't otherwise report as supporting touch.
+ * Disadvantages are 'quirks' to how a PointerEvent works for mice, such a down and up events not being
+ * fired if other buttons are already in a down state.
  * Whether this is enabled or disabled, you can still listen to normalised events such as 'pointerdown',
  * 'pointerup' - it's just that if it is disabled you will not be getting true PointerEvent behaviour.
  *
@@ -18,6 +19,6 @@ import { settings } from '@pixi/settings';
  * @type {boolean}
  * @default false
  */
-settings.USE_POINTER_EVENT_API = false;
+settings.USE_POINTER_EVENT_API = true;
 
 export { settings };
