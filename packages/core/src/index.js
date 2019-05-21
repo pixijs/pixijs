200e2e319a81ba1f0cf04ee733575e73babeb684
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2019-05-21 20:34:44 +0300

    derp shaderGen

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -22,7 +22,7 @@ export { default as BaseRenderTexture } from './renderTexture/BaseRenderTexture'
 export { default as TextureUvs } from './textures/TextureUvs';
 export { default as State } from './state/State';
 export { default as ObjectRenderer } from './batch/ObjectRenderer';
-export { default as BatchShaderGen } from './batch/BatchRenderer';
+export { default as BatchShaderGen } from './batch/BatchShaderGen';
 export { default as BatchRenderer } from './batch/BatchRenderer';
 export { default as BatchGeometry } from './batch/BatchGeometry';
 export { default as BatchDrawCall } from './batch/BatchDrawCall';
