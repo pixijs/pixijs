c563b92e4d4e9229b591fc73e416730855adf798
mofnire <masashi.kojo@toshiba.co.jp>
2019-06-25 16:03:06 +0900

    Fix the JSDoc comments as per the review

--- packages/interaction/src/index.js
+++ packages/interaction/src/index.js
@@ -10,4 +10,3 @@ export { default as InteractionManager } from './InteractionManager';
 export { default as interactiveTarget } from './interactiveTarget';
 export { default as InteractionTrackingData } from './InteractionTrackingData';
 export { default as InteractionEvent } from './InteractionEvent';
-import './IHitArea';
