a31d56793946e08d1b8182be4f87b2e45888af6a
Daniel <23189912+danyisill@users.noreply.github.com>
2019-07-04 18:46:05 +0300

    Typo fix (#5878)

--- packages/utils/src/browser/hello.js
+++ packages/utils/src/browser/hello.js
@@ -15,7 +15,7 @@ export function skipHello()
 /**
  * Logs out the version and renderer information for this running instance of PIXI.
  * If you don't want to see this message you can run `PIXI.utils.skipHello()` before
- * creating your renderer. Keep in mind that doing that will forever makes you a jerk face.
+ * creating your renderer. Keep in mind that doing that will forever make you a jerk face.
  *
  * @static
  * @function sayHello
