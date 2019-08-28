143cfe3380d471ee8ef43b437ca7c1967f066bc1
Scythian <scythianfuego@gmail.com>
2019-08-28 22:50:20 +0100

    remove export

--- packages/mesh-extras/src/index.js
+++ packages/mesh-extras/src/index.js
@@ -1,7 +1,6 @@
 export { default as PlaneGeometry } from './geometry/PlaneGeometry';
 export { default as RopeGeometry } from './geometry/RopeGeometry';
 export { default as SimpleRope } from './SimpleRope';
-export { default as TilingRope } from './TilingRope';
 export { default as SimplePlane } from './SimplePlane';
 export { default as SimpleMesh } from './SimpleMesh';
 export { default as NineSlicePlane } from './NineSlicePlane';
