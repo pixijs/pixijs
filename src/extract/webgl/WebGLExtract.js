3bf4e47b22d19923406fbc1dc226e2849e3c2ced
Martin Bokša <martin.boksa@gmail.com>
2019-09-21 03:05:08 +0200

    Correction of height and width in extracted canvas (#5842)

--- src/extract/webgl/WebGLExtract.js
+++ src/extract/webgl/WebGLExtract.js
@@ -106,8 +106,8 @@ export default class WebGLExtract
             frame.height = textureBuffer.size.height;
         }
 
-        const width = frame.width * resolution;
-        const height = frame.height * resolution;
+        const width = Math.floor((frame.width * resolution) + 1e-4);
+        const height = Math.floor((frame.height * resolution) + 1e-4);
 
         const canvasBuffer = new core.CanvasRenderTarget(width, height, 1);
 
