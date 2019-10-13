3661ecc04f6620c0688f33a4169589ef65c327bb
Matt Karl <matt@mattkarl.com>
2019-10-13 15:29:51 -0700

    Convert Runner to TypeScript

--- packages/runner/src/Runner.ts
+++ packages/runner/src/Runner.ts
@@ -5,8 +5,10 @@
  *
  * like a signal..
  * ```
+ * import { Runner } from '@pixi/runner';
+ *
  * const myObject = {
- *     loaded: new PIXI.Runner('loaded')
+ *     loaded: new Runner('loaded')
  * }
  *
  * const listener = {
@@ -22,8 +24,10 @@
  *
  * Or for handling calling the same function on many items
  * ```
+ * import { Runner } from '@pixi/runner';
+ *
  * const myGame = {
- *     update: new PIXI.Runner('update')
+ *     update: new Runner('update')
  * }
  *
  * const gameObject = {
@@ -41,10 +45,14 @@
  */
 export class Runner
 {
+    public items: any[];
+    private _name: string;
+    private _aliasCount: number;
+
     /**
      *  @param {string} name the function name that will be executed on the listeners added to this Runner.
      */
-    constructor(name)
+    constructor(name: string)
     {
         this.items = [];
         this._name = name;
@@ -53,9 +61,10 @@ export class Runner
 
     /**
      * Dispatch/Broadcast Runner to all listeners added to the queue.
-     * @param {...any} params - optional parameters to pass to each listener
+     * @param {...any} [params] - optional parameters to pass to each listener
+     * @return {PIXI.Runner}
      */
-    emit(a0, a1, a2, a3, a4, a5, a6, a7)
+    public emit(a0?: any, a1?: any, a2?: any, a3?: any, a4?: any, a5?: any, a6?: any, a7?: any): this
     {
         if (arguments.length > 8)
         {
@@ -79,7 +88,7 @@ export class Runner
         return this;
     }
 
-    ensureNonAliasedItems()
+    private ensureNonAliasedItems(): void
     {
         if (this._aliasCount > 0 && this.items.length > 1)
         {
@@ -98,14 +107,17 @@ export class Runner
      * Eg A listener passed to this Runner will require a 'complete' function.
      *
      * ```
-     * const complete = new PIXI.Runner('complete');
+     * import { Runner } from '@pixi/runner';
+     *
+     * const complete = new Runner('complete');
      * ```
      *
      * The scope used will be the object itself.
      *
      * @param {any} item - The object that will be listening.
+     * @return {PIXI.Runner}
      */
-    add(item)
+    public add(item: any): this
     {
         if (item[this._name])
         {
@@ -120,8 +132,9 @@ export class Runner
     /**
      * Remove a single listener from the dispatch queue.
      * @param {any} item - The listenr that you would like to remove.
+     * @return {PIXI.Runner}
      */
-    remove(item)
+    public remove(item: any): this
     {
         const index = this.items.indexOf(item);
 
@@ -138,15 +151,16 @@ export class Runner
      * Check to see if the listener is already in the Runner
      * @param {any} item - The listener that you would like to check.
      */
-    contains(item)
+    public contains(item: any): boolean
     {
         return this.items.indexOf(item) !== -1;
     }
 
     /**
      * Remove all listeners from the Runner
+     * @return {PIXI.Runner}
      */
-    removeAll()
+    public removeAll(): this
     {
         this.ensureNonAliasedItems();
         this.items.length = 0;
@@ -157,11 +171,10 @@ export class Runner
     /**
      * Remove all references, don't use after this.
      */
-    destroy()
+    public destroy(): void
     {
         this.removeAll();
-        this.items = null;
-        this._name = null;
+        this.items.length = 0;
     }
 
     /**
@@ -170,7 +183,7 @@ export class Runner
      * @member {boolean}
      * @readonly
      */
-    get empty()
+    public get empty(): boolean
     {
         return this.items.length === 0;
     }
@@ -181,24 +194,25 @@ export class Runner
      * @member {string}
      * @readonly
      */
-    get name()
+    public get name(): string
     {
         return this._name;
     }
 }
 
-/**
- * Alias for `emit`
- * @memberof PIXI.Runner#
- * @method dispatch
- * @see PIXI.Runner#emit
- */
-Runner.prototype.dispatch = Runner.prototype.emit;
-
-/**
- * Alias for `emit`
- * @memberof PIXI.Runner#
- * @method run
- * @see PIXI.Runner#emit
- */
-Runner.prototype.run = Runner.prototype.emit;
+Object.defineProperties(Runner.prototype, {
+    /**
+     * Alias for `emit`
+     * @memberof PIXI.Runner#
+     * @method dispatch
+     * @see PIXI.Runner#emit
+     */
+    dispatch: { value: Runner.prototype.emit },
+    /**
+     * Alias for `emit`
+     * @memberof PIXI.Runner#
+     * @method run
+     * @see PIXI.Runner#emit
+     */
+    run: { value: Runner.prototype.emit },
+});
