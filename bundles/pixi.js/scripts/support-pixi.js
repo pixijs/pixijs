61a4b2bbd6385a289e687b7c127aabe75cfc7264
Matt Karl <matt@mattkarl.com>
2019-11-06 14:11:03 -0800

    Remove post-install message, replaces with funding field (#6207)

--- bundles/pixi.js/scripts/support-pixi.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-
-const green = '\u001b[32m';
-const white = '\u001b[22m\u001b[39m';
-const boldCyan = '\u001b[96m\u001b[1m';
-const reset = '\u001b[0m';
-
-console.log(`${green}Have some ❤️  for PixiJS? You can support the project via OpenCollective:`);
-console.log(`${white}> ${boldCyan}https://opencollective.com/pixijs/contribute\n${reset}`);
