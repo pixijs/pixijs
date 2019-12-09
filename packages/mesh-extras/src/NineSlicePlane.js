d4dbee233a3a574ea5c001a2840cf715dc5c38a9
Heorhiy Kharvat <Heorhiy.Kharvat@Gamesys.co.uk>
2019-12-09 15:51:34 +0000

    Corners supposed to be proportionally scaled

--- packages/mesh-extras/src/NineSlicePlane.js
+++ packages/mesh-extras/src/NineSlicePlane.js
@@ -126,8 +126,7 @@ export class NineSlicePlane extends SimplePlane
     {
         const vertices = this.vertices;
 
-        const h = this._topHeight + this._bottomHeight;
-        const scale = this._height > h ? 1.0 : this._height / h;
+        const scale = this._getMinScale();
 
         vertices[9] = vertices[11] = vertices[13] = vertices[15] = this._topHeight * scale;
         vertices[17] = vertices[19] = vertices[21] = vertices[23] = this._height - (this._bottomHeight * scale);
@@ -142,12 +141,24 @@ export class NineSlicePlane extends SimplePlane
     {
         const vertices = this.vertices;
 
-        const w = this._leftWidth + this._rightWidth;
-        const scale = this._width > w ? 1.0 : this._width / w;
+        const scale = this._getMinScale();
 
         vertices[2] = vertices[10] = vertices[18] = vertices[26] = this._leftWidth * scale;
         vertices[4] = vertices[12] = vertices[20] = vertices[28] = this._width - (this._rightWidth * scale);
         vertices[6] = vertices[14] = vertices[22] = vertices[30] = this._width;
+    }
+
+    _getMinScale()
+    {
+        const w = this._leftWidth + this._rightWidth;
+        const scale_w = this._width > w ? 1.0 : this._width / w;
+        
+        const h = this._topHeight + this._bottomHeight;
+        const scale_h = this._height > h ? 1.0 : this._height / h;
+        
+        const scale = Math.min(scale_w, scale_h);
+        
+        return scale;
     }
 
     /**
