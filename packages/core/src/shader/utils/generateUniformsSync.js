f25320d8da41c5b170470372c9be171cea212aab
Mat Groves <mat@goodboydigital.com>
2020-01-30 19:53:26 +0000

    refactor generateUniformsSync to use an exposed set of parsers
    refactored generateUniformSync to run on a parsers list
    expose the parsing functions used to generate custom uniform upload code
    optimise function generation by joins strings all at the end

--- packages/core/src/shader/utils/generateUniformsSync.js
+++ packages/core/src/shader/utils/generateUniformsSync.js
@@ -78,13 +78,143 @@ const GLSL_TO_ARRAY_SETTERS = {
     sampler2DArray: 'gl.uniform1iv(location, v)',
 };
 
+// Parsers, each one of these will take a look at the type of shader property and uniform.
+// if they pass the test function then the glsl function is called that returns a the shader upload code for that uniform.
+// Shader upload code is automagically generated with these parsers.
+// If no parser is valid then the default upload functions are used.
+// exposing Parsers means that custom upload logic can be added to pixi's shaders.
+// A good example would be a pixi rectangle can be directly set on a uniform.
+// If the shader sees it it knows how to upload the rectangle structure as a vec4
+// format is as follows:
+//
+// {
+//     test: (data, uniform) => {} <--- test is this glsl should be used for this uniform
+//     glsl: (name, uniform) => {} <--- returns the string of the piece of code that uploads the uniform
+// }
+export const PARSERS = [
+
+    // a float cache layer
+    {
+        test: (data) =>
+            data.type === 'float' && data.size === 1,
+        glsl: (name) =>
+            `
+            if(uv.${name} !== ud.${name}.value)
+            {
+                ud.${name}.value = uv.${name}
+                gl.uniform1f(ud.${name}.location, uv.${name})
+            }
+            `,
+    },
+    // handling samplers
+    {
+        test: (data) =>
+            // eslint-disable-next-line max-len
+            (data.type === 'sampler2D' || data.type === 'samplerCube' || data.type === 'sampler2DArray') && data.size === 1 && !data.isArray,
+        glsl: (name) => `t = syncData.textureCount++;
+
+            renderer.texture.bind(uv.${name}, t);
+            
+            if(ud.${name}.value !== t)
+            {
+                ud.${name}.value = t;
+                gl.uniform1i(ud.${name}.location, t);\n; // eslint-disable-line max-len
+            }`,
+    },
+    // uploading pixi matrix object to mat3
+    {
+        test: (data, uniform) =>
+            data.type === 'mat3' && data.size === 1 && uniform.a !== undefined,
+        glsl: (name) =>
+
+            // TODO and some smart caching dirty ids here!
+            `
+            gl.uniformMatrix3fv(ud.${name}.location, false, uv.${name}.toArray(true));
+            `
+        ,
+
+    },
+    // uploading a pixi point as a vec2 with caching layer
+    {
+        test: (data, uniform) =>
+            data.type === 'vec2' && data.size === 1 && uniform.x !== undefined,
+        glsl: (name) =>
+            `
+                cv = ud.${name}.value;
+                v = uv.${name};
+
+                if(cv[0] !== v.x || cv[1] !== v.y)
+                {
+                    cv[0] = v.x;
+                    cv[1] = v.y;
+                    gl.uniform2f(ud.${name}.location, v.x, v.y);
+                }`,
+    },
+    // caching layer for a vec2
+    {
+        test: (data) =>
+            data.type === 'vec2' && data.size === 1,
+        glsl: (name) =>
+            `
+                cv = ud.${name}.value;
+                v = uv.${name};
+
+                if(cv[0] !== v[0] || cv[1] !== v[1])
+                {
+                    cv[0] = v[0];
+                    cv[1] = v[1];
+                    gl.uniform2f(ud.${name}.location, v[0], v[1]);
+                }
+            `,
+    },
+    // upload a pixi rectangle as a vec4 with caching layer
+    {
+        test: (data, uniform) =>
+            data.type === 'vec4' && data.size === 1 && uniform.width !== undefined,
+
+        glsl: (name) =>
+            `
+                cv = ud.${name}.value;
+                v = uv.${name};
+
+                if(cv[0] !== v.x || cv[1] !== v.y || cv[2] !== v.width || cv[3] !== v.height)
+                {
+                    cv[0] = v.x;
+                    cv[1] = v.y;
+                    cv[2] = v.width;
+                    cv[3] = v.height;
+                    gl.uniform4f(ud.${name}.location, v.x, v.y, v.width, v.height)
+                }`,
+    },
+    // a caching layer for vec4 uploading
+    {
+        test: (data) =>
+            data.type === 'vec4' && data.size === 1,
+        glsl: (name) =>
+            `
+                cv = ud.${name}.value;
+                v = uv.${name};
+
+                if(cv[0] !== v[0] || cv[1] !== v[1] || cv[2] !== v[2] || cv[3] !== v[3])
+                {
+                    cv[0] = v[0];
+                    cv[1] = v[1];
+                    cv[2] = v[2];
+                    cv[3] = v[3];
+
+                    gl.uniform4f(ud.${name}.location, v[0], v[1], v[2], v[3])
+                }`,
+    },
+];
+
 export function generateUniformsSync(group, uniformData)
 {
-    let func = `var v = null;
-    var cv = null
-    var t = 0;
-    var gl = renderer.gl
-    `;
+    const funcFragments = [`
+        var v = null;
+        var cv = null
+        var t = 0;
+        var gl = renderer.gl
+    `];
 
     for (const i in group.uniforms)
     {
@@ -94,135 +224,39 @@ export function generateUniformsSync(group, uniformData)
         {
             if (group.uniforms[i].group)
             {
-                func += `
+                funcFragments.push(`
                     renderer.shader.syncUniformGroup(uv.${i}, syncData);
-                `;
+                `);
             }
 
             continue;
         }
 
-        // TODO && uniformData[i].value !== 0 <-- do we still need this?
-        if (data.type === 'float' && data.size === 1)
-        {
-            func += `
-            if(uv.${i} !== ud.${i}.value)
-            {
-                ud.${i}.value = uv.${i}
-                gl.uniform1f(ud.${i}.location, uv.${i})
-            }\n`;
-        }
-        /* eslint-disable max-len */
-        else if ((data.type === 'sampler2D' || data.type === 'samplerCube' || data.type === 'sampler2DArray') && data.size === 1 && !data.isArray)
-        /* eslint-disable max-len */
-        {
-            func += `
+        const uniform = group.uniforms[i];
 
-            t = syncData.textureCount++;
+        let parsed = false;
 
-            renderer.texture.bind(uv.${i}, t);
-            
-            if(ud.${i}.value !== t)
-            {
-                ud.${i}.value = t;
-                gl.uniform1i(ud.${i}.location, t);\n; // eslint-disable-line max-len
-            }\n`;
-        }
-        else if (data.type === 'mat3' && data.size === 1)
+        for (let j = 0; j < PARSERS.length; j++)
         {
-            if (group.uniforms[i].a !== undefined)
+            if (PARSERS[j].test(data, uniform))
             {
-                // TODO and some smart caching dirty ids here!
-                func += `
-                gl.uniformMatrix3fv(ud.${i}.location, false, uv.${i}.toArray(true));
-                \n`;
-            }
-            else
-            {
-                func += `
-                gl.uniformMatrix3fv(ud.${i}.location, false, uv.${i});
-                \n`;
+                funcFragments.push(PARSERS[j].glsl(i, uniform));
+                parsed = true;
+
+                break;
             }
         }
-        else if (data.type === 'vec2' && data.size === 1)
-        {
-            // TODO - do we need both here?
-            // maybe we can get away with only using points?
-            if (group.uniforms[i].x !== undefined)
-            {
-                func += `
-                cv = ud.${i}.value;
-                v = uv.${i};
 
-                if(cv[0] !== v.x || cv[1] !== v.y)
-                {
-                    cv[0] = v.x;
-                    cv[1] = v.y;
-                    gl.uniform2f(ud.${i}.location, v.x, v.y);
-                }\n`;
-            }
-            else
-            {
-                func += `
-                cv = ud.${i}.value;
-                v = uv.${i};
-
-                if(cv[0] !== v[0] || cv[1] !== v[1])
-                {
-                    cv[0] = v[0];
-                    cv[1] = v[1];
-                    gl.uniform2f(ud.${i}.location, v[0], v[1]);
-                }
-                \n`;
-            }
-        }
-        else if (data.type === 'vec4' && data.size === 1)
-        {
-            // TODO - do we need both here?
-            // maybe we can get away with only using points?
-            if (group.uniforms[i].width !== undefined)
-            {
-                func += `
-                cv = ud.${i}.value;
-                v = uv.${i};
-
-                if(cv[0] !== v.x || cv[1] !== v.y || cv[2] !== v.width || cv[3] !== v.height)
-                {
-                    cv[0] = v.x;
-                    cv[1] = v.y;
-                    cv[2] = v.width;
-                    cv[3] = v.height;
-                    gl.uniform4f(ud.${i}.location, v.x, v.y, v.width, v.height)
-                }\n`;
-            }
-            else
-            {
-                func += `
-                cv = ud.${i}.value;
-                v = uv.${i};
-
-                if(cv[0] !== v[0] || cv[1] !== v[1] || cv[2] !== v[2] || cv[3] !== v[3])
-                {
-                    cv[0] = v[0];
-                    cv[1] = v[1];
-                    cv[2] = v[2];
-                    cv[3] = v[3];
-
-                    gl.uniform4f(ud.${i}.location, v[0], v[1], v[2], v[3])
-                }
-                \n`;
-            }
-        }
-        else
+        if (!parsed)
         {
             const templateType = (data.size === 1) ? GLSL_TO_SINGLE_SETTERS_CACHED : GLSL_TO_ARRAY_SETTERS;
 
             const template =  templateType[data.type].replace('location', `ud.${i}.location`);
 
-            func += `
+            funcFragments.push(`
             cv = ud.${i}.value;
             v = uv.${i};
-            ${template};\n`;
+            ${template};`);
         }
     }
 
@@ -232,5 +266,5 @@ export function generateUniformsSync(group, uniformData)
      * no matter which group is being used
      *
      */
-    return new Function('ud', 'uv', 'renderer', 'syncData', func); // eslint-disable-line no-new-func
+    return new Function('ud', 'uv', 'renderer', 'syncData', funcFragments.join('\n')); // eslint-disable-line no-new-func
 }
