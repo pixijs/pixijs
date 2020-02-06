1528b4d2d1356f7075ab97254d8cea7386f2fd96
Exponenta <rondo.devil@gmail.com>
2020-02-06 15:48:56 +0300

    JSDoc

--- packages/loaders/src/LoaderResource.ts
+++ packages/loaders/src/LoaderResource.ts
@@ -2,13 +2,7 @@ import { Resource } from 'resource-loader';
 import { Texture } from '@pixi/core';
 import { Spritesheet } from '@pixi/spritesheet';
 
-/**
-* Reference to **{@link https://github.com/englercj/resource-loader
-* resource-loader}**'s Resource class.
-* @see http://englercj.github.io/resource-loader/Resource.html
-* @class LoaderResource
-* @memberof PIXI
-*/
+
 
 export interface IResourceMetadata extends Resource.IMetadata {
     imageMetadata?: any;
@@ -25,4 +19,11 @@ export interface ILoaderResource extends Resource
     metadata: IResourceMetadata;
 }
 
+/**
+* Reference to **{@link https://github.com/englercj/resource-loader
+* resource-loader}**'s Resource class.
+* @see http://englercj.github.io/resource-loader/Resource.html
+* @class LoaderResource
+* @memberof PIXI
+*/
 export const LoaderResource: ILoaderResource = Resource as any;
