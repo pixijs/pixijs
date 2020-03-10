b18c58ac156fd8f32b6240efa52bba07d839ecd2
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2020-03-10 17:29:23 +0300

    unit test

--- packages/mixin-cache-as-bitmap/test/index.js
+++ packages/mixin-cache-as-bitmap/test/index.js
@@ -1,4 +1,5 @@
-const { DisplayObject } = require('@pixi/display');
+const { DisplayObject, Container } = require('@pixi/display');
+const { Renderer, Filter } = require('@pixi/core');
 
 require('../');
 
@@ -18,5 +19,44 @@ describe('PIXI.DisplayObject#cacheAsBitmap', function ()
         const obj = new DisplayObject();
 
         obj.cacheAsBitmap = true;
+    });
+
+    it('should respect filters', function ()
+    {
+        const par = new Container();
+        const obj = new Container();
+        let renderer = null;
+
+        par.filters = [new Filter()];
+        par.addChild(obj);
+
+        obj.position.set(5, 15);
+        obj.updateTransform();
+        obj.cacheAsBitmap = true;
+        obj._calculateBounds = function ()
+        {
+            this._bounds.clear();
+            this._bounds.addFrame(this.transform, 0, 0, 10, 11);
+        };
+
+        try
+        {
+            renderer = new Renderer();
+            renderer.filter.push(par, par.filters);
+            obj.render(renderer);
+
+            const frame = renderer.renderTexture.sourceFrame;
+
+            expect(obj._cacheData.sprite.width).to.equal(10);
+            expect(obj._cacheData.sprite.height).to.equal(11);
+            expect(frame.x).to.equal(5);
+            expect(frame.y).to.equal(15);
+            expect(frame.width).to.equal(10);
+            expect(frame.height).to.equal(11);
+        }
+        finally
+        {
+            renderer.destroy();
+        }
     });
 });
