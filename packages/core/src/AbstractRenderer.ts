edf6465d4ec922e7c74f803b658d0d10c4736a71
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2020-03-17 17:35:16 +0300

    cleaning up-2

--- packages/core/src/AbstractRenderer.ts
+++ packages/core/src/AbstractRenderer.ts
@@ -308,7 +308,7 @@ export abstract class AbstractRenderer extends EventEmitter
     generateTexture(displayObject: DisplayObject,
         scaleMode?: SCALE_MODES, resolution?: number, region?: Rectangle): RenderTexture
     {
-        region = region || (displayObject as Container).getLocalBounds(undefined, true);
+        region = region || (displayObject as Container).getLocalBounds(null, true);
 
         // minimum texture size is 1x1, 0x0 will throw an error
         if (region.width === 0) region.width = 1;
