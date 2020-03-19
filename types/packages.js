7d9623bd2af3e8402ac4e166d8d2fc3a89177e7d
盛庆鸿 <admin@sqhong.com>
2020-03-20 06:40:26 +0800

    Import @microsoft/api-extractor

---
+++ types/packages.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-console */
+const path = require('path');
+const fs = require('fs-extra');
+const { getPackages } = require('@lerna/project');
+const { Extractor, ExtractorConfig, CompilerState } = require('@microsoft/api-extractor');
+const chalk = require('chalk');
+const glob = require('glob');
+
+main();
+
+async function main()
+{
+    const baseDir = path.join(__dirname, '..');
+    let packages = await getPackages(baseDir);
+
+    packages = packages.filter((pkg) => !pkg.get('standalone') && !pkg.private);
+    packages = packages.filter((pkg) => pkg.get('types'));
+
+    /**
+     * Represents the TypeScript compiler state.
+     * This allows an optimization where multiple invocations can reuse the same compiler analysis.
+     */
+    let compilerState;
+
+    // Share compiler state require load all entry points first.
+    const entryPoints = packages.map((pkg) =>
+    {
+        const relative = path.relative(baseDir, pkg.location);
+
+        return path.join(baseDir, 'dist', relative, 'src/index.d.ts');
+    });
+
+    // Patch for api-extractor not followed the declares of *.vert and *.frag in global.d.ts
+    const strings = glob.sync('packages/**/*.{vert,frag}', { cwd: baseDir });
+
+    await Promise.all(strings.map((file) =>
+    {
+        const target = path.join(baseDir, 'dist', `${file}.d.ts`);
+
+        return fs.outputFile(target, `declare const content:string; export default content;`, 'utf8');
+    }));
+
+    entryPoints.push(path.join(baseDir, 'global.d.ts'));
+    
+    for (const pkg of packages)
+    {
+        const extractorConfigPath = path.join(pkg.location, 'api-extractor.json');
+
+        const extractorConfig = ExtractorConfig.loadFileAndPrepare(extractorConfigPath);
+
+        if (!compilerState)
+        {
+            compilerState = CompilerState.create(extractorConfig, {
+                additionalEntryPoints: entryPoints,
+            });
+        }
+
+        const result = Extractor.invoke(extractorConfig, {
+            localBuild: true,
+            showVerboseMessages: true,
+            compilerState,
+        });
+
+        if (result.succeeded)
+        {
+            console.log(
+                chalk.bold(chalk.green(`API Extractor completed successfully.`))
+            );
+        }
+        else
+        {
+            console.error(
+                `API Extractor completed with ${result.errorCount} errors`
+              + ` and ${result.warningCount} warnings`
+            );
+            process.exitCode = 1;
+        }
+    }
+}
