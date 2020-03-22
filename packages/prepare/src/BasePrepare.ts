b31dd0029a04da54625f6432ac67e5e21703e554
Mikhail Gordiyenko <motan@meta.ua>
2020-03-22 09:47:27 +0200

    fixes according to suggestions

--- packages/prepare/src/BasePrepare.ts
+++ packages/prepare/src/BasePrepare.ts
@@ -1,10 +1,12 @@
-import { Texture, BaseTexture, AbstractRenderer, Renderer } from '@pixi/core';
+import { Texture, BaseTexture } from '@pixi/core';
 import { Ticker, UPDATE_PRIORITY } from '@pixi/ticker';
 import { settings } from '@pixi/settings';
 import { Container, DisplayObject } from '@pixi/display';
 import { Text, TextStyle, TextMetrics } from '@pixi/text';
 import { CountLimiter } from './CountLimiter';
 import { CanvasPrepare } from 'pixi.js-legacy';
+
+import type { AbstractRenderer, Renderer } from '@pixi/core';
 
 interface IArrowFunction {
     (): void;
@@ -22,321 +24,6 @@ interface IDisplayObjectExtended extends DisplayObject {
     _texture?: Texture;
     style?: TextStyle;
 }
-/**
- * The prepare manager provides functionality to upload content to the GPU.
- *
- * BasePrepare handles basic queuing functionality and is extended by
- * {@link PIXI.Prepare} and {@link PIXI.CanvasPrepare}
- * to provide preparation capabilities specific to their respective renderers.
- *
- * @example
- * // Create a sprite
- * const sprite = PIXI.Sprite.from('something.png');
- *
- * // Load object into GPU
- * app.renderer.plugins.prepare.upload(sprite, () => {
- *
- *     //Texture(s) has been uploaded to GPU
- *     app.stage.addChild(sprite);
- *
- * })
- *
- * @abstract
- * @class
- * @memberof PIXI
- */
-export class BasePrepare
-{
-    private limiter: CountLimiter;
-    public renderer: AbstractRenderer;
-    public uploadHookHelper: any;
-    public queue: Array<any>;
-    public addHooks: Array<any>;
-    public uploadHooks: Array<any>;
-    public completes: Array<any>;
-    public ticking: boolean;
-    private delayedTick: IArrowFunction;
-    /**
-     * @param {PIXI.AbstractRenderer} renderer - A reference to the current renderer
-     */
-    constructor(renderer: AbstractRenderer)
-    {
-        /**
-         * The limiter to be used to control how quickly items are prepared.
-         * @type {PIXI.CountLimiter|PIXI.TimeLimiter}
-         */
-        this.limiter = new CountLimiter(settings.UPLOADS_PER_FRAME);
-
-        /**
-         * Reference to the renderer.
-         * @type {PIXI.AbstractRenderer}
-         * @protected
-         */
-        this.renderer = renderer;
-
-        /**
-         * The only real difference between CanvasPrepare and Prepare is what they pass
-         * to upload hooks. That different parameter is stored here.
-         * @type {object}
-         * @protected
-         */
-        this.uploadHookHelper = null;
-
-        /**
-         * Collection of items to uploads at once.
-         * @type {Array<*>}
-         * @private
-         */
-        this.queue = [];
-
-        /**
-         * Collection of additional hooks for finding assets.
-         * @type {Array<Function>}
-         * @private
-         */
-        this.addHooks = [];
-
-        /**
-         * Collection of additional hooks for processing assets.
-         * @type {Array<Function>}
-         * @private
-         */
-        this.uploadHooks = [];
-
-        /**
-         * Callback to call after completed.
-         * @type {Array<Function>}
-         * @private
-         */
-        this.completes = [];
-
-        /**
-         * If prepare is ticking (running).
-         * @type {boolean}
-         * @private
-         */
-        this.ticking = false;
-
-        /**
-         * 'bound' call for prepareItems().
-         * @type {Function}
-         * @private
-         */
-        this.delayedTick = (): void =>
-        {
-            // unlikely, but in case we were destroyed between tick() and delayedTick()
-            if (!this.queue)
-            {
-                return;
-            }
-            this.prepareItems();
-        };
-
-        // hooks to find the correct texture
-        this.registerFindHook(findText);
-        this.registerFindHook(findTextStyle);
-        this.registerFindHook(findMultipleBaseTextures);
-        this.registerFindHook(findBaseTexture);
-        this.registerFindHook(findTexture);
-
-        // upload hooks
-        this.registerUploadHook(drawText);
-        this.registerUploadHook(calculateTextStyle);
-    }
-
-    /**
-     * Upload all the textures and graphics to the GPU.
-     *
-     * @param {Function|PIXI.DisplayObject|PIXI.Container|PIXI.BaseTexture|PIXI.Texture|PIXI.Graphics|PIXI.Text} item -
-     *        Either the container or display object to search for items to upload, the items to upload themselves,
-     *        or the callback function, if items have been added using `prepare.add`.
-     * @param {Function} [done] - Optional callback when all queued uploads have completed
-     */
-    upload(item: IDisplayObjectExtended | ICallback, done: ICallback): void
-    {
-        if (typeof item === 'function')
-        {
-            done = item;
-            item = null;
-        }
-
-        // If a display object, search for items
-        // that we could upload
-        if (item)
-        {
-            this.add(item);
-        }
-
-        // Get the items for upload from the display
-        if (this.queue.length)
-        {
-            if (done)
-            {
-                this.completes.push(done);
-            }
-
-            if (!this.ticking)
-            {
-                this.ticking = true;
-                Ticker.system.addOnce(this.tick, this, UPDATE_PRIORITY.UTILITY);
-            }
-        }
-        else if (done)
-        {
-            done();
-        }
-    }
-
-    /**
-     * Handle tick update
-     *
-     * @private
-     */
-    tick(): void
-    {
-        setTimeout(this.delayedTick, 0);
-    }
-
-    /**
-     * Actually prepare items. This is handled outside of the tick because it will take a while
-     * and we do NOT want to block the current animation frame from rendering.
-     *
-     * @private
-     */
-    prepareItems(): void
-    {
-        this.limiter.beginFrame();
-        // Upload the graphics
-        while (this.queue.length && this.limiter.allowedToUpload())
-        {
-            const item = this.queue[0];
-            let uploaded = false;
-
-            if (item && !item._destroyed)
-            {
-                for (let i = 0, len = this.uploadHooks.length; i < len; i++)
-                {
-                    if (this.uploadHooks[i](this.uploadHookHelper, item))
-                    {
-                        this.queue.shift();
-                        uploaded = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!uploaded)
-            {
-                this.queue.shift();
-            }
-        }
-
-        // We're finished
-        if (!this.queue.length)
-        {
-            this.ticking = false;
-
-            const completes = this.completes.slice(0);
-
-            this.completes.length = 0;
-
-            for (let i = 0, len = completes.length; i < len; i++)
-            {
-                completes[i]();
-            }
-        }
-        else
-        {
-            // if we are not finished, on the next rAF do this again
-            Ticker.system.addOnce(this.tick, this, UPDATE_PRIORITY.UTILITY);
-        }
-    }
-
-    /**
-     * Adds hooks for finding items.
-     *
-     * @param {Function} addHook - Function call that takes two parameters: `item:*, queue:Array`
-     *          function must return `true` if it was able to add item to the queue.
-     * @return {this} Instance of plugin for chaining.
-     */
-    registerFindHook(addHook: IFindHook): BasePrepare
-    {
-        if (addHook)
-        {
-            this.addHooks.push(addHook);
-        }
-
-        return this;
-    }
-
-    /**
-     * Adds hooks for uploading items.
-     *
-     * @param {Function} uploadHook - Function call that takes two parameters: `prepare:CanvasPrepare, item:*` and
-     *          function must return `true` if it was able to handle upload of item.
-     * @return {this} Instance of plugin for chaining.
-     */
-    registerUploadHook(uploadHook: IUploadHook): BasePrepare
-    {
-        if (uploadHook)
-        {
-            this.uploadHooks.push(uploadHook);
-        }
-
-        return this;
-    }
-
-    /**
-     * Manually add an item to the uploading queue.
-     *
-     * @param {PIXI.DisplayObject|PIXI.Container|PIXI.BaseTexture|PIXI.Texture|PIXI.Graphics|PIXI.Text|*} item - Object to
-     *        add to the queue
-     * @return {this} Instance of plugin for chaining.
-     */
-    add(item: IDisplayObjectExtended | IUploadHook | IFindHook): BasePrepare
-    {
-        // Add additional hooks for finding elements on special
-        // types of objects that
-        for (let i = 0, len = this.addHooks.length; i < len; i++)
-        {
-            if (this.addHooks[i](item, this.queue))
-            {
-                break;
-            }
-        }
-
-        // Get children recursively
-        if (item instanceof Container)
-        {
-            for (let i = item.children.length - 1; i >= 0; i--)
-            {
-                this.add(item.children[i]);
-            }
-        }
-
-        return this;
-    }
-
-    /**
-     * Destroys the plugin, don't use after this.
-     *
-     */
-    destroy(): void
-    {
-        if (this.ticking)
-        {
-            Ticker.system.remove(this.tick, this);
-        }
-        this.ticking = false;
-        this.addHooks = null;
-        this.uploadHooks = null;
-        this.renderer = null;
-        this.completes = null;
-        this.queue = null;
-        this.limiter = null;
-        this.uploadHookHelper = null;
-    }
-}
 
 /**
  * Built-in hook to find multiple textures from objects like AnimatedSprites.
@@ -347,7 +34,7 @@ export class BasePrepare
  * @return {boolean} if a PIXI.Texture object was found.
  */
 
-function findMultipleBaseTextures(item: IDisplayObjectExtended, queue: Array<any>): any
+function findMultipleBaseTextures(item: IDisplayObjectExtended, queue: Array<any>): boolean
 {
     let result = false;
 
@@ -522,4 +209,320 @@ function findTextStyle(item: TextStyle, queue: Array<any>): boolean
     }
 
     return false;
+}
+
+/**
+ * The prepare manager provides functionality to upload content to the GPU.
+ *
+ * BasePrepare handles basic queuing functionality and is extended by
+ * {@link PIXI.Prepare} and {@link PIXI.CanvasPrepare}
+ * to provide preparation capabilities specific to their respective renderers.
+ *
+ * @example
+ * // Create a sprite
+ * const sprite = PIXI.Sprite.from('something.png');
+ *
+ * // Load object into GPU
+ * app.renderer.plugins.prepare.upload(sprite, () => {
+ *
+ *     //Texture(s) has been uploaded to GPU
+ *     app.stage.addChild(sprite);
+ *
+ * })
+ *
+ * @abstract
+ * @class
+ * @memberof PIXI
+ */
+export class BasePrepare
+{
+    private limiter: CountLimiter;
+    public renderer: AbstractRenderer;
+    public uploadHookHelper: any;
+    public queue: Array<any>;
+    public addHooks: Array<any>;
+    public uploadHooks: Array<any>;
+    public completes: Array<any>;
+    public ticking: boolean;
+    private delayedTick: IArrowFunction;
+    /**
+     * @param {PIXI.AbstractRenderer} renderer - A reference to the current renderer
+     */
+    constructor(renderer: AbstractRenderer)
+    {
+        /**
+         * The limiter to be used to control how quickly items are prepared.
+         * @type {PIXI.CountLimiter|PIXI.TimeLimiter}
+         */
+        this.limiter = new CountLimiter(settings.UPLOADS_PER_FRAME);
+
+        /**
+         * Reference to the renderer.
+         * @type {PIXI.AbstractRenderer}
+         * @protected
+         */
+        this.renderer = renderer;
+
+        /**
+         * The only real difference between CanvasPrepare and Prepare is what they pass
+         * to upload hooks. That different parameter is stored here.
+         * @type {object}
+         * @protected
+         */
+        this.uploadHookHelper = null;
+
+        /**
+         * Collection of items to uploads at once.
+         * @type {Array<*>}
+         * @private
+         */
+        this.queue = [];
+
+        /**
+         * Collection of additional hooks for finding assets.
+         * @type {Array<Function>}
+         * @private
+         */
+        this.addHooks = [];
+
+        /**
+         * Collection of additional hooks for processing assets.
+         * @type {Array<Function>}
+         * @private
+         */
+        this.uploadHooks = [];
+
+        /**
+         * Callback to call after completed.
+         * @type {Array<Function>}
+         * @private
+         */
+        this.completes = [];
+
+        /**
+         * If prepare is ticking (running).
+         * @type {boolean}
+         * @private
+         */
+        this.ticking = false;
+
+        /**
+         * 'bound' call for prepareItems().
+         * @type {Function}
+         * @private
+         */
+        this.delayedTick = (): void =>
+        {
+            // unlikely, but in case we were destroyed between tick() and delayedTick()
+            if (!this.queue)
+            {
+                return;
+            }
+            this.prepareItems();
+        };
+
+        // hooks to find the correct texture
+        this.registerFindHook(findText);
+        this.registerFindHook(findTextStyle);
+        this.registerFindHook(findMultipleBaseTextures);
+        this.registerFindHook(findBaseTexture);
+        this.registerFindHook(findTexture);
+
+        // upload hooks
+        this.registerUploadHook(drawText);
+        this.registerUploadHook(calculateTextStyle);
+    }
+
+    /**
+     * Upload all the textures and graphics to the GPU.
+     *
+     * @param {Function|PIXI.DisplayObject|PIXI.Container|PIXI.BaseTexture|PIXI.Texture|PIXI.Graphics|PIXI.Text} item -
+     *        Either the container or display object to search for items to upload, the items to upload themselves,
+     *        or the callback function, if items have been added using `prepare.add`.
+     * @param {Function} [done] - Optional callback when all queued uploads have completed
+     */
+    upload(item: IDisplayObjectExtended | IUploadHook | IFindHook, done: any): void
+    {
+        if (typeof item === 'function')
+        {
+            done = item;
+            item = null;
+        }
+
+        // If a display object, search for items
+        // that we could upload
+        if (item)
+        {
+            this.add(item);
+        }
+
+        // Get the items for upload from the display
+        if (this.queue.length)
+        {
+            if (done)
+            {
+                this.completes.push(done);
+            }
+
+            if (!this.ticking)
+            {
+                this.ticking = true;
+                Ticker.system.addOnce(this.tick, this, UPDATE_PRIORITY.UTILITY);
+            }
+        }
+        else if (done)
+        {
+            done();
+        }
+    }
+
+    /**
+     * Handle tick update
+     *
+     * @private
+     */
+    tick(): void
+    {
+        setTimeout(this.delayedTick, 0);
+    }
+
+    /**
+     * Actually prepare items. This is handled outside of the tick because it will take a while
+     * and we do NOT want to block the current animation frame from rendering.
+     *
+     * @private
+     */
+    prepareItems(): void
+    {
+        this.limiter.beginFrame();
+        // Upload the graphics
+        while (this.queue.length && this.limiter.allowedToUpload())
+        {
+            const item = this.queue[0];
+            let uploaded = false;
+
+            if (item && !item._destroyed)
+            {
+                for (let i = 0, len = this.uploadHooks.length; i < len; i++)
+                {
+                    if (this.uploadHooks[i](this.uploadHookHelper, item))
+                    {
+                        this.queue.shift();
+                        uploaded = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!uploaded)
+            {
+                this.queue.shift();
+            }
+        }
+
+        // We're finished
+        if (!this.queue.length)
+        {
+            this.ticking = false;
+
+            const completes = this.completes.slice(0);
+
+            this.completes.length = 0;
+
+            for (let i = 0, len = completes.length; i < len; i++)
+            {
+                completes[i]();
+            }
+        }
+        else
+        {
+            // if we are not finished, on the next rAF do this again
+            Ticker.system.addOnce(this.tick, this, UPDATE_PRIORITY.UTILITY);
+        }
+    }
+
+    /**
+     * Adds hooks for finding items.
+     *
+     * @param {Function} addHook - Function call that takes two parameters: `item:*, queue:Array`
+     *          function must return `true` if it was able to add item to the queue.
+     * @return {this} Instance of plugin for chaining.
+     */
+    registerFindHook(addHook: IFindHook): this
+    {
+        if (addHook)
+        {
+            this.addHooks.push(addHook);
+        }
+
+        return this;
+    }
+
+    /**
+     * Adds hooks for uploading items.
+     *
+     * @param {Function} uploadHook - Function call that takes two parameters: `prepare:CanvasPrepare, item:*` and
+     *          function must return `true` if it was able to handle upload of item.
+     * @return {this} Instance of plugin for chaining.
+     */
+    registerUploadHook(uploadHook: IUploadHook): this
+    {
+        if (uploadHook)
+        {
+            this.uploadHooks.push(uploadHook);
+        }
+
+        return this;
+    }
+
+    /**
+     * Manually add an item to the uploading queue.
+     *
+     * @param {PIXI.DisplayObject|PIXI.Container|PIXI.BaseTexture|PIXI.Texture|PIXI.Graphics|PIXI.Text|*} item - Object to
+     *        add to the queue
+     * @return {this} Instance of plugin for chaining.
+     */
+    add(item: IDisplayObjectExtended | IUploadHook | IFindHook): this
+    {
+        // Add additional hooks for finding elements on special
+        // types of objects that
+        for (let i = 0, len = this.addHooks.length; i < len; i++)
+        {
+            if (this.addHooks[i](item, this.queue))
+            {
+                break;
+            }
+        }
+
+        // Get children recursively
+        if (item instanceof Container)
+        {
+            for (let i = item.children.length - 1; i >= 0; i--)
+            {
+                this.add(item.children[i]);
+            }
+        }
+
+        return this;
+    }
+
+    /**
+     * Destroys the plugin, don't use after this.
+     *
+     */
+    destroy(): void
+    {
+        if (this.ticking)
+        {
+            Ticker.system.remove(this.tick, this);
+        }
+        this.ticking = false;
+        this.addHooks = null;
+        this.uploadHooks = null;
+        this.renderer = null;
+        this.completes = null;
+        this.queue = null;
+        this.limiter = null;
+        this.uploadHookHelper = null;
+    }
 }
