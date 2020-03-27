f8bd51827d68be230ff614d64a31907243c50ab5
Shukant Pal <shukantpal@outlook.com>
2020-03-27 14:06:51 -0400

    Fixed miter limit (pdist calculation)

--- packages/graphics/src/utils/buildLine.ts
+++ packages/graphics/src/utils/buildLine.ts
@@ -367,7 +367,7 @@ function buildNonNativeLine(graphicsData: GraphicsData, graphicsGeometry: Graphi
         const py = ((dy1 * c1) - (dy0 * c2)) / cross;
         const pdist = ((px - x1) * (px - x1)) + ((py - y1) * (py - y1));
 
-        if (style.join === LINE_JOIN.BEVEL || pdist > style.miterLimit)
+        if (style.join === LINE_JOIN.BEVEL || pdist > style.miterLimit * style.miterLimit)
         {
             if (clockwise) /* rotating at inner angle */
             {
