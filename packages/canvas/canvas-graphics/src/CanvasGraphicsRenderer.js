bb08df2faeb22b2ccc5c2452356eecda7e595362
Shukant Pal <shukantpal@outlook.com>
2020-04-02 13:49:26 -0400

    Remove updateGraphicsTint in lieu of shared geometries, calculate fillTint/lineTint at render cycle.

--- packages/canvas/canvas-graphics/src/CanvasGraphicsRenderer.js
+++ packages/canvas/canvas-graphics/src/CanvasGraphicsRenderer.js
@@ -75,20 +75,16 @@ export class CanvasGraphicsRenderer
         const transform = graphics.transform.worldTransform;
 
         renderer.setContextTransform(transform);
-
-        // update tint if graphics was dirty
-        if (graphics.canvasTintDirty !== graphics.geometry.dirty
-            || graphics._prevTint !== graphics.tint)
-        {
-            this.updateGraphicsTint(graphics);
-        }
-
         renderer.setBlendMode(graphics.blendMode);
 
         const graphicsData = graphics.geometry.graphicsData;
 
         let contextFillStyle;
         let contextStrokeStyle;
+
+        const tintR = ((graphics.tint >> 16) & 0xFF) / 255;
+        const tintG = ((graphics.tint >> 8) & 0xFF) / 255;
+        const tintB = (graphics.tint & 0xFF) / 255;
 
         for (let i = 0; i < graphicsData.length; i++)
         {
@@ -97,13 +93,27 @@ export class CanvasGraphicsRenderer
             const fillStyle = data.fillStyle;
             const lineStyle = data.lineStyle;
 
+            const fillColor = data.fillStyle.color | 0;
+            const lineColor = data.lineStyle.color | 0;
+
+            const fillTint = (
+                (((fillColor >> 16) & 0xFF) / 255 * tintR * 255 << 16)
+                + (((fillColor >> 8) & 0xFF) / 255 * tintG * 255 << 8)
+                + (((fillColor & 0xFF) / 255) * tintB * 255)
+            );
+            const lineTint = (
+                (((lineColor >> 16) & 0xFF) / 255 * tintR * 255 << 16)
+                + (((lineColor >> 8) & 0xFF) / 255 * tintG * 255 << 8)
+                + (((lineColor & 0xFF) / 255) * tintB * 255)
+            );
+
             if (fillStyle.visible)
             {
-                contextFillStyle = this._calcCanvasStyle(fillStyle, data._fillTint);
+                contextFillStyle = this._calcCanvasStyle(fillStyle, fillTint);
             }
             if (lineStyle.visible)
             {
-                contextStrokeStyle = this._calcCanvasStyle(lineStyle, data._lineTint);
+                contextStrokeStyle = this._calcCanvasStyle(lineStyle, lineTint);
             }
 
             context.lineWidth = lineStyle.width;
@@ -317,44 +327,6 @@ export class CanvasGraphicsRenderer
                     context.stroke();
                 }
             }
-        }
-    }
-
-    /**
-     * Updates the tint of a graphics object
-     *
-     * @protected
-     * @param {PIXI.Graphics} graphics - the graphics that will have its tint updated
-     */
-    updateGraphicsTint(graphics)
-    {
-        graphics._prevTint = graphics.tint;
-        graphics.canvasTintDirty = graphics.geometry.dirty;
-
-        const tintR = ((graphics.tint >> 16) & 0xFF) / 255;
-        const tintG = ((graphics.tint >> 8) & 0xFF) / 255;
-        const tintB = (graphics.tint & 0xFF) / 255;
-        const graphicsData = graphics.geometry.graphicsData;
-
-        for (let i = 0; i < graphicsData.length; ++i)
-        {
-            const data = graphicsData[i];
-
-            const fillColor = data.fillStyle.color | 0;
-            const lineColor = data.lineStyle.color | 0;
-
-            // super inline, cos optimization :)
-            data._fillTint = (
-                (((fillColor >> 16) & 0xFF) / 255 * tintR * 255 << 16)
-                + (((fillColor >> 8) & 0xFF) / 255 * tintG * 255 << 8)
-                + (((fillColor & 0xFF) / 255) * tintB * 255)
-            );
-
-            data._lineTint = (
-                (((lineColor >> 16) & 0xFF) / 255 * tintR * 255 << 16)
-                + (((lineColor >> 8) & 0xFF) / 255 * tintG * 255 << 8)
-                + (((lineColor & 0xFF) / 255) * tintB * 255)
-            );
         }
     }
 
