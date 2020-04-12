5827a05de6dc1336af9c49d82c8ed049e0a58a1b
Yevhenii Huselietov <d46k16@gmail.com>
2020-04-12 14:06:52 +0300

    [RFC] Github Actions

--- packages/settings/src/utils/isMobile.ts
+++ packages/settings/src/utils/isMobile.ts
@@ -3,6 +3,6 @@
 // designed for Node-only environments
 import isMobileCall from 'ismobilejs';
 
-const isMobile = isMobileCall();
+const isMobile = isMobileCall(window && window.navigator);
 
 export { isMobile };
