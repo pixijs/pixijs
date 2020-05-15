238d3453a6e39423975575858bc0723077e011ef
Matt Karl <matt@mattkarl.com>
2020-05-15 09:10:43 -0700

    Add optional @pixi/graphics-extras package (#6613)

---
+++ packages/graphics-extras/global.d.ts
@@ -0,0 +1,8 @@
+declare namespace GlobalMixins
+{
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface Graphics extends Partial<import('@pixi/graphics-extras').IGraphicsExtras>
+    {
+
+    }
+}
