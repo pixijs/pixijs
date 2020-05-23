6a7f53fd5620f43661af1d7f5ee781190459d93d
Matt Karl <matt@mattkarl.com>
2020-05-23 07:24:49 -0700

    Updated BitmapFont.from signature

--- packages/text-bitmap/test/BitmapFont.js
+++ packages/text-bitmap/test/BitmapFont.js
@@ -14,7 +14,7 @@ describe('BitmapFont', function ()
 
         it('should throw for missing name', function ()
         {
-            expect(() => BitmapFont.from({ chars: 'CHARSET' })).to.throw;
+            expect(() => BitmapFont.from()).to.throw;
         });
 
         it('should register the font if a name is provided', function ()
@@ -22,32 +22,34 @@ describe('BitmapFont', function ()
             this.timeout(8000);
             this.slow(4000);
 
-            const font = BitmapFont.from({ name: 'MockFont', chars: 'CHARSET' });
+            expect(BitmapFont.available.foo).to.be.undefined;
 
-            expect(BitmapFont.available.MockFont).to.equal(font);
+            const font = BitmapFont.from('foo');
+
+            expect(BitmapFont.available.foo).to.equal(font);
         });
 
         it('should draw all characters in a provided range', function ()
         {
-            const font = BitmapFont.from({ name: 'foo', chars: [['a', 'z']] });
+            const font = BitmapFont.from('foo', {}, { chars: [['a', 'z']] });
 
             expect(Object.keys(font.chars).length).to.equal(26);
         });
 
         it('should throw an error when an invalid range is given', function ()
         {
-            expect(() => BitmapFont.from({ name: 'foo', chars: [['l', 'i', 'm']] })).to.throw;
+            expect(() => BitmapFont.from('foo', {}, { chars: [['l', 'i', 'm']] })).to.throw;
         });
 
         it('should throw an error when an invalid start/end of range', function ()
         {
-            expect(() => BitmapFont.from({ name: 'foo', chars: [['z', 'a']] })).to.throw;
+            expect(() => BitmapFont.from('foo', {}, { chars: [['z', 'a']] })).to.throw;
         });
 
         it('should render resolution with proportional size', function ()
         {
-            const fontRes1 = BitmapFont.from({ name: 'foo', chars: 'a' });
-            const fontRes2 = BitmapFont.from({ name: 'bar', chars: 'a', resolution: 2 });
+            const fontRes1 = BitmapFont.from('foo', {}, { chars: 'a' });
+            const fontRes2 = BitmapFont.from('bar', {}, { chars: 'a', resolution: 2 });
 
             // BitmapFont.from IDs are charCodes
             const id = 'a'.charCodeAt(0);
@@ -60,17 +62,17 @@ describe('BitmapFont', function ()
         {
             const id = 'a'.charCodeAt(0);
 
-            BitmapFont.from({ name: 'foo', chars: 'a' });
+            BitmapFont.from('foo', {}, { chars: 'a' });
             expect(Object.keys(BitmapFont.available.foo.chars).length).to.equal(1);
 
-            BitmapFont.from({ name: 'foo', chars: 'bc' });
+            BitmapFont.from('foo', {}, { chars: 'bc' });
             expect(Object.keys(BitmapFont.available.foo.chars).length).to.equal(2);
             expect(BitmapFont.available.foo.chars[id]).to.be.undefined;
         });
 
         it('should throw an error when no characters are passed', function ()
         {
-            expect(() => BitmapFont.from({ name: 'foo', chars: [] })).to.throw;
+            expect(() => BitmapFont.from('foo', {}, { chars: [] })).to.throw;
         });
     });
 });
