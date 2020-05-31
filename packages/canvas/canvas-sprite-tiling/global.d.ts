1790db7a6c4051a7c9d95c60106264b4b54acec1
Matt Karl <mattkarl@netflix.com>
2020-05-31 16:38:51 -0700

    Typings handled by Sprite

--- packages/canvas/canvas-sprite-tiling/global.d.ts
+++ packages/canvas/canvas-sprite-tiling/global.d.ts
@@ -1,7 +1,5 @@
 declare namespace GlobalMixins {
     interface TilingSprite {
-        _renderCanvas: (renderer: import('@pixi/canvas-renderer').CanvasRenderer) => void;
-        _tintedCanvas: HTMLCanvasElement;
         _canvasPattern: CanvasPattern;
     }
 }
