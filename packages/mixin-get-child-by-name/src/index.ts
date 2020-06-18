01581acc0415a89034b0ef85504467172f8ec5e3
Sukant Pal <SukantK2002@outlook.com>
2020-06-18 15:35:53 -0400

    Add option for recursive search in @pixi/mixin-get-child-by-name (#6713)

--- packages/mixin-get-child-by-name/src/index.ts
+++ packages/mixin-get-child-by-name/src/index.ts
@@ -11,18 +11,41 @@ DisplayObject.prototype.name = null;
 /**
  * Returns the display object in the container.
  *
+ * Recursive searches are done in a preorder traversal.
+ *
  * @method getChildByName
  * @memberof PIXI.Container#
  * @param {string} name - Instance name.
+ * @param {boolean}[deep=false] - Whether to search recursively
  * @return {PIXI.DisplayObject} The child with the specified name.
  */
-Container.prototype.getChildByName = function getChildByName(name: string): DisplayObject
+Container.prototype.getChildByName = function getChildByName(name: string, deep?: boolean): DisplayObject
 {
-    for (let i = 0; i < this.children.length; i++)
+    for (let i = 0, j = this.children.length; i < j; i++)
     {
         if (this.children[i].name === name)
         {
             return this.children[i];
+        }
+    }
+
+    if (deep)
+    {
+        for (let i = 0, j = this.children.length; i < j; i++)
+        {
+            const child = (this.children[i] as Container);
+
+            if (!child.getChildByName)
+            {
+                continue;
+            }
+
+            const target = (this.children[i] as Container).getChildByName(name, true);
+
+            if (target)
+            {
+                return target;
+            }
         }
     }
 
