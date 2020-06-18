140ca83f6cff21e4a3a70e134ad322a1d217e755
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2020-06-18 17:31:06 +0300

    Add RenderTexture support for CubeResource (#6435)

--- packages/core/src/textures/resources/CubeResource.ts
+++ packages/core/src/textures/resources/CubeResource.ts
@@ -1,4 +1,4 @@
-import { ArrayResource } from './ArrayResource';
+import { AbstractMultiResource } from './AbstractMultiResource';
 import { TARGETS } from '@pixi/constants';
 
 import type { Resource } from './Resource';
@@ -14,6 +14,7 @@ import type { GLTexture } from '../GLTexture';
 export interface ICubeResourceOptions extends ISize
 {
     autoLoad?: boolean;
+    linkBaseTexture?: boolean;
 }
 
 /**
@@ -27,25 +28,42 @@ export interface ICubeResourceOptions extends ISize
  * @param {object} [options] - ImageResource options
  * @param {number} [options.width] - Width of resource
  * @param {number} [options.height] - Height of resource
+ * @param {number} [options.autoLoad=true] - Whether to auto-load resources
+ * @param {number} [options.linkBaseTexture=true] - In case BaseTextures are supplied,
+ *   whether to copy them or use
  */
-export class CubeResource extends ArrayResource
+export class CubeResource extends AbstractMultiResource
 {
     items: ArrayFixed<BaseTexture, 6>;
 
-    constructor(source: ArrayFixed<string|Resource, 6>, options?: ICubeResourceOptions)
+    linkBaseTexture: boolean;
+
+    constructor(source?: ArrayFixed<string|Resource, 6>, options?: ICubeResourceOptions)
     {
-        const { width, height, autoLoad } = options || {};
+        const { width, height, autoLoad, linkBaseTexture } = options || {};
 
-        super(source, { width, height });
-
-        if (this.length !== CubeResource.SIDES)
+        if (source && source.length !== CubeResource.SIDES)
         {
-            throw new Error(`Invalid length. Got ${this.length}, expected 6`);
+            throw new Error(`Invalid length. Got ${source.length}, expected 6`);
         }
+
+        super(6, { width, height });
 
         for (let i = 0; i < CubeResource.SIDES; i++)
         {
             this.items[i].target = TARGETS.TEXTURE_CUBE_MAP_POSITIVE_X + i;
+        }
+
+        /**
+         * In case BaseTextures are supplied, whether to use same resource or bind baseTexture itself
+         * @member {boolean}
+         * @protected
+         */
+        this.linkBaseTexture = linkBaseTexture !== false;
+
+        if (source)
+        {
+            this.initFromArray(source, options);
         }
 
         if (autoLoad !== false)
@@ -67,6 +85,51 @@ export class CubeResource extends ArrayResource
         baseTexture.target = TARGETS.TEXTURE_CUBE_MAP;
     }
 
+    addBaseTextureAt(baseTexture: BaseTexture, index: number, linkBaseTexture?: boolean): this
+    {
+        if (linkBaseTexture === undefined)
+        {
+            linkBaseTexture = this.linkBaseTexture;
+        }
+
+        if (!this.items[index])
+        {
+            throw new Error(`Index ${index} is out of bounds`);
+        }
+
+        if (!this.linkBaseTexture
+            || baseTexture.parentTextureArray
+            || Object.keys(baseTexture._glTextures).length > 0)
+        {
+            // copy mode
+            if (baseTexture.resource)
+            {
+                this.addResourceAt(baseTexture.resource, index);
+            }
+            else
+            {
+                throw new Error(`CubeResource does not support copying of renderTexture.`);
+            }
+        }
+        else
+        {
+            // link mode, the difficult one!
+            baseTexture.target = TARGETS.TEXTURE_CUBE_MAP_POSITIVE_X + index;
+            baseTexture.parentTextureArray = this.baseTexture;
+
+            this.items[index] = baseTexture;
+        }
+
+        if (baseTexture.valid && !this.valid)
+        {
+            this.resize(baseTexture.realWidth, baseTexture.realHeight);
+        }
+
+        this.items[index] = baseTexture;
+
+        return this;
+    }
+
     /**
      * Upload the resource
      *
@@ -82,14 +145,24 @@ export class CubeResource extends ArrayResource
 
             if (dirty[i] < side.dirtyId)
             {
-                dirty[i] = side.dirtyId;
-                if (side.valid)
+                if (side.valid && side.resource)
                 {
                     side.resource.upload(renderer, side, glTexture);
+                    dirty[i] = side.dirtyId;
                 }
-                else
+                else if (dirty[i] < -1)
                 {
-                    // TODO: upload zero buffer
+                    // either item is not valid yet, either its a renderTexture
+                    // allocate the memory
+                    renderer.gl.texImage2D(side.target, 0,
+                        glTexture.internalFormat,
+                        _baseTexture.realWidth,
+                        _baseTexture.realHeight,
+                        0,
+                        _baseTexture.format,
+                        glTexture.type,
+                        null);
+                    dirty[i] = -1;
                 }
             }
         }
