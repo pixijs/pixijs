2df48f8143ac86a24843e67115437403ac78eed7
Shukant Pal <shukantpal@outlook.com>
2020-08-07 18:23:59 -0400

    WebGLExtensions interface

--- packages/core/src/context/ContextSystem.ts
+++ packages/core/src/context/ContextSystem.ts
@@ -4,15 +4,7 @@ import { settings } from '../settings';
 
 import type { IRenderingContext } from '../IRenderingContext';
 import type { Renderer } from '../Renderer';
-
-/* eslint-disable camelcase */
-import type {
-    WEBGL_compressed_texture_pvrtc,
-    WEBGL_compressed_texture_etc,
-    WEBGL_compressed_texture_etc1,
-    WEBGL_compressed_texture_atc
-} from './WebGLExtensions';
-/* eslint-enable camelcase */
+import type { WebGLExtensions } from './WebGLExtensions';
 
 let CONTEXT_UID_COUNTER = 0;
 
@@ -34,30 +26,8 @@ export class ContextSystem extends System
 
     protected CONTEXT_UID: number;
     protected gl: IRenderingContext;
-    /* eslint-disable camelcase */
-    extensions:
-    {
-        drawBuffers?: WEBGL_draw_buffers;
-        depthTexture?: OES_texture_float;
-        loseContext?: WEBGL_lose_context;
-        vertexArrayObject?: OES_vertex_array_object;
-        anisotropicFiltering?: EXT_texture_filter_anisotropic;
-        uint32ElementIndex?: OES_element_index_uint;
-        floatTexture?: OES_texture_float;
-        floatTextureLinear?: OES_texture_float_linear;
-        textureHalfFloat?: OES_texture_half_float;
-        textureHalfFloatLinear?: OES_texture_half_float_linear;
-        colorBufferFloat?: WEBGL_color_buffer_float;
 
-        s3tc?: WEBGL_compressed_texture_s3tc;
-        s3tc_sRGB?: WEBGL_compressed_texture_s3tc_srgb;
-        etc?: WEBGL_compressed_texture_etc;
-        etc1?: WEBGL_compressed_texture_etc1;
-        pvrtc?: WEBGL_compressed_texture_pvrtc;
-        atc?: WEBGL_compressed_texture_atc;
-        astc?: WEBGL_compressed_texture_astc;
-    };
-    /* eslint-enable camelcase */
+    public extensions: WebGLExtensions;
 
     /**
      * @param {PIXI.Renderer} renderer - The renderer this System works for.
--- packages/core/src/context/WebGLExtensions.ts
+++ packages/core/src/context/WebGLExtensions.ts
@@ -29,4 +29,26 @@ export interface WEBGL_compressed_texture_atc
     COMPRESSED_RGBA_ATC_EXPLICIT_ALPHA_WEBGL: number;
     COMPRESSED_RGBA_ATC_INTERPOLATED_ALPHA_WEBGL: number;
 }
+
+export interface WebGLExtensions {
+    drawBuffers?: WEBGL_draw_buffers;
+    depthTexture?: OES_texture_float;
+    loseContext?: WEBGL_lose_context;
+    vertexArrayObject?: OES_vertex_array_object;
+    anisotropicFiltering?: EXT_texture_filter_anisotropic;
+    uint32ElementIndex?: OES_element_index_uint;
+    floatTexture?: OES_texture_float;
+    floatTextureLinear?: OES_texture_float_linear;
+    textureHalfFloat?: OES_texture_half_float;
+    textureHalfFloatLinear?: OES_texture_half_float_linear;
+    colorBufferFloat?: WEBGL_color_buffer_float;
+
+    s3tc?: WEBGL_compressed_texture_s3tc;
+    s3tc_sRGB?: WEBGL_compressed_texture_s3tc_srgb;
+    etc?: WEBGL_compressed_texture_etc;
+    etc1?: WEBGL_compressed_texture_etc1;
+    pvrtc?: WEBGL_compressed_texture_pvrtc;
+    atc?: WEBGL_compressed_texture_atc;
+    astc?: WEBGL_compressed_texture_astc;
+}
 /* eslint-enable camelcase */
