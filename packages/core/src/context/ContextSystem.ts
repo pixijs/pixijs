4938e3f0d8f3a47a39d6a542f42b8a86699c1670
Shukant Pal <shukantpal@outlook.com>
2020-08-07 18:18:32 -0400

    Move WebGL extension types out of ContextSystem.ts

--- packages/core/src/context/ContextSystem.ts
+++ packages/core/src/context/ContextSystem.ts
@@ -5,40 +5,16 @@ import { settings } from '../settings';
 import type { IRenderingContext } from '../IRenderingContext';
 import type { Renderer } from '../Renderer';
 
-let CONTEXT_UID_COUNTER = 0;
-
 /* eslint-disable camelcase */
-interface WEBGL_compressed_texture_pvrtc
-{
-    COMPRESSED_RGB_PVRTC_4BPPV1_IMG: number;
-    COMPRESSED_RGBA_PVRTC_4BPPV1_IMG: number;
-    COMPRESSED_RGB_PVRTC_2BPPV1_IMG: number;
-    COMPRESSED_RGBA_PVRTC_2BPPV1_IMG: number;
-}
-interface WEBGL_compressed_texture_etc
-{
-    COMPRESSED_R11_EAC: number;
-    COMPRESSED_SIGNED_R11_EAC: number;
-    COMPRESSED_RG11_EAC: number;
-    COMPRESSED_SIGNED_RG11_EAC: number;
-    COMPRESSED_RGB8_ETC2: number;
-    COMPRESSED_RGBA8_ETC2_EAC: number;
-    COMPRESSED_SRGB8_ETC2: number;
-    COMPRESSED_SRGB8_ALPHA8_ETC2_EAC: number;
-    COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2: number;
-    COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2: number;
-}
-interface WEBGL_compressed_texture_etc1
-{
-    COMPRESSED_RGB_ETC1_WEBGL: number;
-}
-interface WEBGL_compressed_texture_atc
-{
-    COMPRESSED_RGB_ATC_WEBGL: number;
-    COMPRESSED_RGBA_ATC_EXPLICIT_ALPHA_WEBGL: number;
-    COMPRESSED_RGBA_ATC_INTERPOLATED_ALPHA_WEBGL: number;
-}
+import type {
+    WEBGL_compressed_texture_pvrtc,
+    WEBGL_compressed_texture_etc,
+    WEBGL_compressed_texture_etc1,
+    WEBGL_compressed_texture_atc
+} from './WebGLExtensions';
 /* eslint-enable camelcase */
+
+let CONTEXT_UID_COUNTER = 0;
 
 export interface ISupportDict {
     uint32Indices: boolean;
---
+++ packages/core/src/context/WebGLExtensions.ts
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+export interface WEBGL_compressed_texture_pvrtc
+{
+    COMPRESSED_RGB_PVRTC_4BPPV1_IMG: number;
+    COMPRESSED_RGBA_PVRTC_4BPPV1_IMG: number;
+    COMPRESSED_RGB_PVRTC_2BPPV1_IMG: number;
+    COMPRESSED_RGBA_PVRTC_2BPPV1_IMG: number;
+}
+export interface WEBGL_compressed_texture_etc
+{
+    COMPRESSED_R11_EAC: number;
+    COMPRESSED_SIGNED_R11_EAC: number;
+    COMPRESSED_RG11_EAC: number;
+    COMPRESSED_SIGNED_RG11_EAC: number;
+    COMPRESSED_RGB8_ETC2: number;
+    COMPRESSED_RGBA8_ETC2_EAC: number;
+    COMPRESSED_SRGB8_ETC2: number;
+    COMPRESSED_SRGB8_ALPHA8_ETC2_EAC: number;
+    COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2: number;
+    COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2: number;
+}
+export interface WEBGL_compressed_texture_etc1
+{
+    COMPRESSED_RGB_ETC1_WEBGL: number;
+}
+export interface WEBGL_compressed_texture_atc
+{
+    COMPRESSED_RGB_ATC_WEBGL: number;
+    COMPRESSED_RGBA_ATC_EXPLICIT_ALPHA_WEBGL: number;
+    COMPRESSED_RGBA_ATC_INTERPOLATED_ALPHA_WEBGL: number;
+}
+/* eslint-enable camelcase */
