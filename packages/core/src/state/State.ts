f94df18f5b0f7513ce37d37f80e3e6114668dcd8
Jens Malmborg <jnsmalm@gmail.com>
2020-08-18 19:04:27 +0200

    Add support for depth mask (#6753)

--- packages/core/src/state/State.ts
+++ packages/core/src/state/State.ts
@@ -7,6 +7,7 @@ const OFFSET = 1;
 const CULLING = 2;
 const DEPTH_TEST = 3;
 const WINDING = 4;
+const DEPTH_MASK = 5;
 
 /**
  * This is a WebGL state, and is is passed The WebGL StateManager.
@@ -31,6 +32,7 @@ export class State
         this.polygonOffset = 0;
 
         this.blend = true;
+        this.depthMask = true;
         //  this.depthTest = true;
     }
 
@@ -106,6 +108,25 @@ export class State
         if (!!(this.data & (1 << DEPTH_TEST)) !== value)
         {
             this.data ^= (1 << DEPTH_TEST);
+        }
+    }
+
+    /**
+     * Enables or disables writing to the depth buffer.
+     *
+     * @member {boolean}
+     * @default true
+     */
+    get depthMask(): boolean
+    {
+        return !!(this.data & (1 << DEPTH_MASK));
+    }
+
+    set depthMask(value: boolean)
+    {
+        if (!!(this.data & (1 << DEPTH_MASK)) !== value)
+        {
+            this.data ^= (1 << DEPTH_MASK);
         }
     }
 
