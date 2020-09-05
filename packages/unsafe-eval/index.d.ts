6a06f7e9ccdf3676234628c09d1500e5fb93ea24
Shukant Pal <shukantpal@outlook.com>
2020-09-05 13:42:02 -0400

    Remove index.d.ts

--- packages/unsafe-eval/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ShaderSystem } from '@pixi/core';
-
-export declare function install(PIXI: PIXICore): void;
-
-declare interface PIXICore {
-    ShaderSystem: typeof ShaderSystem;
-}
-
-export { }
