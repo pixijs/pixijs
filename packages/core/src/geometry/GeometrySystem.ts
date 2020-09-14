225faf054db3322ef63c467f697405a77fce0d9f
James Chen <dumganhar@gmail.com>
2020-09-15 05:38:05 +0800

    Fix VAO & instance array check in WebGL2 (#6877)

--- packages/core/src/geometry/GeometrySystem.ts
+++ packages/core/src/geometry/GeometrySystem.ts
@@ -93,7 +93,7 @@ export class GeometrySystem extends System
         this.CONTEXT_UID = this.renderer.CONTEXT_UID;
 
         // webgl2
-        if (!gl.createVertexArray)
+        if (context.webGLVersion !== 2)
         {
             // webgl 1!
             let nativeVaoExtension = this.renderer.context.extensions.vertexArrayObject;
@@ -128,7 +128,7 @@ export class GeometrySystem extends System
             }
         }
 
-        if (!gl.vertexAttribDivisor)
+        if (context.webGLVersion !== 2)
         {
             const instanceExt = gl.getExtension('ANGLE_instanced_arrays');
 
