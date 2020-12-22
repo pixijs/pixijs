2ed1ff97986138308cd0fb73402d86bd5d43c09c
Mat Groves <mat@goodboydigital.com>
2020-12-22 16:36:42 +0000

    docs

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -434,7 +434,21 @@ export enum MSAA_QUALITY {
     HIGH = 8
 }
 
+/**
+ * Constants for various buffer types in Pixi
+ *
+ * @see PIXI.BUFFER_TYPE
+ *
+ * @name BUFFER_TYPE
+ * @memberof PIXI
+ * @static
+ * @enum {number}
+ * @property {number} ELEMENT_ARRAY_BUFFER - buffer type for using as an index buffer
+ * @property {number} ARRAY_BUFFER - buffer type for using attribute data
+ * @property {number} UNIFORM_BUFFER - the buffer type is for uniform buffer objects
+ */
 export enum BUFFER_TYPE {
+
     ELEMENT_ARRAY_BUFFER = 34963,
     ARRAY_BUFFER = 34962,
     // NOT YET SUPPORTED
