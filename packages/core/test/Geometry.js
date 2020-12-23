3f048344f63a436bad40b0d0542a34bd8dc692b9
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2020-12-23 21:24:16 +0300

    Geometry with different shaders leaked buffers (#7093)
    Co-authored-by: Matt Karl <matt@mattkarl.com>

--- packages/core/test/Geometry.js
+++ packages/core/test/Geometry.js
@@ -11,6 +11,16 @@ void main() {
     vUvs = aUvs;
     gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
 }`;
+const vert2 = `
+attribute vec2 aVertexPosition;
+uniform mat3 translationMatrix;
+uniform mat3 projectionMatrix;
+varying vec2 vUvs;
+
+void main() {
+    vUvs = aVertexPosition;
+    gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+}`;
 const frag = `
 varying vec2 vUvs;
 uniform sampler2D uSampler;
@@ -48,6 +58,44 @@ describe('PIXI.Geometry', function ()
             expect(Object.keys(indices._glBuffers).length).to.equal(1);
             geometry2.destroy();
             expect(Object.keys(indices._glBuffers).length).to.equal(0);
+        }
+        finally
+        {
+            renderer.destroy();
+        }
+    });
+
+    it('should dispose buffer if geometry is used by two shaders', function ()
+    {
+        const renderer = new Renderer(1, 1);
+
+        try
+        {
+            const indices = new Buffer([0, 1, 2, 0, 2, 3], true, true);
+            const geometry = new Geometry();
+            const prog = new Program(vert, frag);
+            const prog2 = new Program(vert2, frag);
+            const shader = new Shader(prog, { uSampler: Texture.WHITE });
+            const shader2 = new Shader(prog2, { uSampler: Texture.WHITE });
+
+            geometry.addAttribute('aVertexPosition', [-100, -100, 100, -100, 100, 100, -100, 100], 2)
+                .addAttribute('aUvs', [0, 0, 1, 0, 1, 1, 0, 1], 2)
+                .addIndex(indices);
+
+            renderer.geometry.bind(geometry, shader);
+            renderer.geometry.bind(geometry, shader2);
+
+            // 2 signatures and 2 by shader-ids
+            expect(Object.keys(geometry.glVertexArrayObjects).length).to.equal(1);
+            expect(Object.keys(geometry.glVertexArrayObjects[renderer.CONTEXT_UID]).length).to.equal(4);
+            expect(Object.keys(renderer.geometry.managedGeometries).length).to.equal(1);
+            expect(Object.keys(indices._glBuffers).length).to.equal(1);
+            expect(indices._glBuffers[renderer.CONTEXT_UID].refCount).to.equal(1);
+            geometry.dispose();
+            expect(Object.keys(geometry.glVertexArrayObjects).length).to.equal(0);
+            expect(Object.keys(renderer.geometry.managedGeometries).length).to.equal(0);
+            expect(Object.keys(indices._glBuffers).length).to.equal(0);
+            geometry.destroy();
         }
         finally
         {
