a542760e5ac619b06ea8ba9aa5f17c98b42a8946
Matt Karl <mattkarl@netflix.com>
2021-01-05 16:36:41 -0800

    Add unit-testing support for specific packages

--- test/index.js
+++ test/index.js
@@ -13,8 +13,24 @@ global.chai.use(sinonChai);
 
 // Synchronously generate the list of testable packages
 const script = path.join(__dirname, './packages.js');
-const packages = execFileSync('node', [script]).toString();
+const { availableSuites, locations } = JSON.parse(execFileSync('node', [script]).toString());
 
-// Require the test
+// Filter any packages from the commandline options
+const onlyPackages = global.options.args
+    .filter((arg) => arg.startsWith('--package='))
+    .map((arg) => arg.replace('--package=', ''));
+
+// Filter the tests in case we have options to narrow scope
+const enabledSuites = !onlyPackages.length ? availableSuites
+    : availableSuites.filter((pkg) => onlyPackages.some((p) => pkg.startsWith(locations[p])));
+
+if (!enabledSuites.length)
+{
+    const invalidNames = onlyPackages.filter((pkg) => !locations[pkg]);
+
+    // eslint-disable-next-line no-console
+    console.log(`WARNING: Invalid package name${invalidNames.length > 1 ? 's' : ''}:`, `"${invalidNames.join('", "')}"`);
+}
+
 // eslint-disable-next-line global-require
-JSON.parse(packages).forEach((pkg) => require(pkg));
+enabledSuites.forEach((pkg) => require(pkg));
