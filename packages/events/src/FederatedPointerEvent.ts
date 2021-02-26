8ab4e5299e7cae0df4c339e91984a04e22c04135
Shukant Pal <shukantpal@outlook.com>
2021-02-25 20:06:22 -0500

    More docs

--- packages/events/src/FederatedPointerEvent.ts
+++ packages/events/src/FederatedPointerEvent.ts
@@ -1,7 +1,7 @@
 import { FederatedMouseEvent } from './FederatedMouseEvent';
 
 /**
- * A {@link FederatedEvent} for pointer events.
+ * A {@link PIXI.FederatedEvent} for pointer events.
  *
  * @memberof PIXI
  */
@@ -36,11 +36,49 @@ export class FederatedPointerEvent extends FederatedMouseEvent implements Pointe
      * @see https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/isPrimary
      */
     public isPrimary = false;
+
+    /**
+     * The type of pointer that triggered the event.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/pointerType
+     */
     public pointerType: string;
+
+    /**
+     * Pressure applied by the pointing device during the event.
+     *s
+     * A Touch's force property will be represented by this value.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/pressure
+     */
     public pressure: number;
+
+    /**
+     * Barrel pressure on a stylus pointer.
+     *
+     * @see https://w3c.github.io/pointerevents/#pointerevent-interface
+     */
     public tangentialPressure: number;
+
+    /**
+     * The angle, in degrees, between the pointer device and the screen.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/tiltX
+     */
     public tiltX: number;
+
+    /**
+     * The angle, in degrees, between the pointer device and the screen.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/tiltY
+     */
     public tiltY: number;
+
+    /**
+     * Twist of a stylus pointer.
+     *
+     * @see https://w3c.github.io/pointerevents/#pointerevent-interface
+     */
     public twist: number;
 
     /**
