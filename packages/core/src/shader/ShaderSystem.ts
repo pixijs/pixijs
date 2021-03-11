9dd2bc49dc956042f075a7b5af17f5985e91b510
Mat Groves <mat@goodboydigital.com>
2021-03-11 21:33:34 +0000

    - fixes the the shader error i introduced - pretty error printing

--- packages/core/src/shader/ShaderSystem.ts
+++ packages/core/src/shader/ShaderSystem.ts
@@ -1,5 +1,5 @@
 import { GLProgram } from './GLProgram';
-import { generateUniformsSync, unsafeEvalSupported, defaultValue, compileProgram } from './utils';
+import { generateUniformsSync, unsafeEvalSupported, defaultValue, logProgramError, compileShader } from './utils';
 
 import type { ISystem } from '../ISystem';
 import type { IGLUniformData } from './GLProgram';
@@ -231,10 +231,39 @@ export class ShaderSystem implements ISystem
 
         const program = shader.program;
 
-        const shaderProgram = compileProgram(gl, program.vertexSrc, program.fragmentSrc);
+        const glVertShader = compileShader(gl, gl.VERTEX_SHADER, program.vertexSrc);
+        const glFragShader = compileShader(gl, gl.FRAGMENT_SHADER, program.fragmentSrc);
 
-        program.attributeData = getAttributeData(shaderProgram, gl);
-        program.uniformData = getUniformData(shaderProgram, gl);
+        const webGLProgram = gl.createProgram();
+
+        gl.attachShader(webGLProgram, glVertShader);
+        gl.attachShader(webGLProgram, glFragShader);
+
+        gl.linkProgram(webGLProgram);
+
+        if (!gl.getProgramParameter(webGLProgram, gl.LINK_STATUS))
+        {
+            logProgramError(gl, webGLProgram, glVertShader, glFragShader);
+        }
+
+        program.attributeData = getAttributeData(webGLProgram, gl);
+        program.uniformData = getUniformData(webGLProgram, gl);
+
+        const keys = Object.keys(program.attributeData);
+
+        keys.sort((a, b) => (a > b) ? 1 : -1); // eslint-disable-line no-confusing-arrow
+
+        for (let i = 0; i < keys.length; i++)
+        {
+            program.attributeData[keys[i]].location = i;
+
+            gl.bindAttribLocation(webGLProgram, i, keys[i]);
+        }
+
+        gl.linkProgram(webGLProgram);
+
+        gl.deleteShader(glVertShader);
+        gl.deleteShader(glFragShader);
 
         const uniformData: {[key: string]: IGLUniformData} = {};
 
@@ -243,12 +272,12 @@ export class ShaderSystem implements ISystem
             const data = program.uniformData[i];
 
             uniformData[i] = {
-                location: gl.getUniformLocation(shaderProgram, i),
+                location: gl.getUniformLocation(webGLProgram, i),
                 value: defaultValue(data.type, data.size),
             };
         }
 
-        const glProgram = new GLProgram(shaderProgram, uniformData);
+        const glProgram = new GLProgram(webGLProgram, uniformData);
 
         program.glPrograms[this.renderer.CONTEXT_UID] = glProgram;
 
