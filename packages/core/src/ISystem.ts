1e24df0811a71badf7daffb3d1309dbaad66454e
Matt Karl <mattkarl@netflix.com>
2021-03-30 11:01:46 -0700

    Fix deprecation for System class

--- packages/core/src/ISystem.ts
+++ packages/core/src/ISystem.ts
@@ -1,7 +1,9 @@
 import type { Renderer } from './Renderer';
+import { deprecation } from '@pixi/utils';
 
 /**
  * Interface for systems used by the {@link PIXI.Renderer}.
+ * @memberof PIXI
  */
 export interface ISystem
 {
@@ -19,4 +21,33 @@ export interface ISystem
 export interface ISystemConstructor<R = Renderer>
 {
     new (renderer: R): ISystem;
+}
+
+/**
+ * Use the ISystem interface instead.
+ * @deprecated since 6.1.0
+ * @memberof PIXI
+ */
+export class System implements ISystem
+{
+    /** Reference to the main renderer */
+    public renderer: Renderer;
+
+    /**
+     * @param renderer - Reference to Renderer
+     */
+    constructor(renderer: Renderer)
+    {
+        // #if _DEBUG
+        deprecation('6.1.0', 'System class is deprecated, implemement ISystem interface instead.');
+        // #endif
+
+        this.renderer = renderer;
+    }
+
+    /** Destroy and don't use after this. */
+    destroy(): void
+    {
+        this.renderer = null;
+    }
 }
