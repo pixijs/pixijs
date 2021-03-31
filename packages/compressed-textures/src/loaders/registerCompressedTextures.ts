2e58d746f71fee136e188fbee947f6f3452850d0
Matt Karl <matt@mattkarl.com>
2021-03-31 14:51:08 -0700

    Fix errors in basis and compressed-textures packages (#7354)

--- packages/compressed-textures/src/loaders/registerCompressedTextures.ts
+++ packages/compressed-textures/src/loaders/registerCompressedTextures.ts
@@ -1,7 +1,14 @@
 import { MIPMAP_MODES, ALPHA_MODES } from '@pixi/constants';
 import { BaseTexture, Texture } from '@pixi/core';
 
+import type { ILoaderResource, IResourceMetadata } from '@pixi/loaders';
 import type { CompressedTextureResource } from '../resources/CompressedTextureResource';
+
+/**
+ * Result when calling registerCompressedTextures.
+ * @ignore
+ */
+type CompressedTexturesResult = Pick<ILoaderResource, 'textures' | 'texture'>;
 
 /**
  * Creates base-textures and textures for each compressed-texture resource and adds them into the global
@@ -12,28 +19,45 @@ import type { CompressedTextureResource } from '../resources/CompressedTextureRe
  * @param resources - the resources backing texture data
  * @ignore
  */
-export function registerCompressedTextures(url: string, resources: CompressedTextureResource[]): void
+export function registerCompressedTextures(url: string,
+    resources: CompressedTextureResource[],
+    metadata: IResourceMetadata): CompressedTexturesResult
 {
+    const result: CompressedTexturesResult = {
+        textures: {},
+        texture: null,
+    };
+
     if (!resources)
     {
-        return;
+        return result;
     }
 
-    resources.forEach((resource, i) =>
+    const textures = resources.map((resource) =>
+        (
+            new Texture(new BaseTexture(resource, Object.assign({
+                mipmap: MIPMAP_MODES.OFF,
+                alphaMode: ALPHA_MODES.NO_PREMULTIPLIED_ALPHA
+            }, metadata)))
+        ));
+
+    textures.forEach((texture, i) =>
     {
-        const baseTexture = new BaseTexture(resource, {
-            mipmap: MIPMAP_MODES.OFF,
-            alphaMode: ALPHA_MODES.NO_PREMULTIPLIED_ALPHA
-        });
+        const { baseTexture } = texture;
         const cacheID = `${url}-${i + 1}`;
 
         BaseTexture.addToCache(baseTexture, cacheID);
-        Texture.addToCache(new Texture(baseTexture), cacheID);
+        Texture.addToCache(texture, cacheID);
 
         if (i === 0)
         {
             BaseTexture.addToCache(baseTexture, url);
-            Texture.addToCache(new Texture(baseTexture), url);
+            Texture.addToCache(texture, url);
+            result.texture = texture;
         }
+
+        result.textures[cacheID] = texture;
     });
+
+    return result;
 }
