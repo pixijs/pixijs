0c16e6b2fb7069f4a8587cd6782bfae4bfc4cd9b
dev7355608 <dev7355608@gmail.com>
2021-05-01 16:51:23 +0200

    Add tests

--- packages/core/test/FramebufferSystem.js
+++ packages/core/test/FramebufferSystem.js
@@ -1,5 +1,6 @@
 const { Renderer, Framebuffer } = require('../');
 const { Rectangle } = require('../../math');
+const { MSAA_QUALITY } = require('@pixi/constants');
 
 describe('PIXI.FramebufferSystem', function ()
 {
@@ -64,5 +65,304 @@ describe('PIXI.FramebufferSystem', function ()
         this.renderer.framebuffer.bind(framebuffer, frame, 0);
 
         expect(Array.from(gl.getParameter(gl.VIEWPORT))).to.deep.equal([2, 2, 2, 2]);
+    });
+
+    it('should create an incomplete framebuffer if it has no attachments', function ()
+    {
+        this.renderer.framebuffer.contextChange();
+
+        const { gl, CONTEXT_UID } = this.renderer;
+
+        const framebuffer = new Framebuffer(4, 4);
+
+        framebuffer.depth = false;
+        framebuffer.stencil = false;
+        framebuffer.multisample = MSAA_QUALITY.NONE;
+
+        this.renderer.framebuffer.bind(framebuffer);
+
+        expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT);
+    });
+
+    it('should create a complete framebuffer with a color texture attachment', function ()
+    {
+        this.renderer.framebuffer.contextChange();
+
+        const { gl, CONTEXT_UID } = this.renderer;
+
+        const framebuffer = new Framebuffer(4, 4);
+
+        framebuffer.depth = false;
+        framebuffer.stencil = false;
+        framebuffer.multisample = MSAA_QUALITY.NONE;
+        framebuffer.addColorTexture(0);
+
+        this.renderer.framebuffer.bind(framebuffer);
+
+        const fbo = framebuffer.glFramebuffers[CONTEXT_UID];
+
+        expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+        expect(fbo.multisample).to.equal(0);
+        expect(fbo.msaaBuffer).to.be.null;
+        expect(fbo.stencil).to.be.null;
+    });
+
+    it('should create a complete framebuffer with depth/stencil attachment', function ()
+    {
+        this.renderer.framebuffer.contextChange();
+
+        if (this.renderer.context.webGLVersion === 1
+            || this.renderer.framebuffer.msaaSamples === null
+            || this.renderer.framebuffer.msaaSamples.every((x) => x <= 1))
+        {
+            this.skip();
+        }
+
+        const { gl, CONTEXT_UID } = this.renderer;
+
+        const framebuffer = new Framebuffer(4, 4);
+
+        framebuffer.depth = true;
+        framebuffer.stencil = true;
+        framebuffer.multisample = MSAA_QUALITY.NONE;
+
+        this.renderer.framebuffer.bind(framebuffer);
+
+        const fbo = framebuffer.glFramebuffers[CONTEXT_UID];
+
+        expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+        expect(fbo.multisample).to.equal(0);
+        expect(fbo.msaaBuffer).to.be.null;
+        expect(fbo.stencil).to.be.not.null;
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, fbo.stencil);
+
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_SAMPLES)).to.equal(fbo.multisample);
+    });
+
+    it('should create a complete multisampled framebuffer', function ()
+    {
+        this.renderer.framebuffer.contextChange();
+
+        if (this.renderer.context.webGLVersion === 1
+            || this.renderer.framebuffer.msaaSamples === null
+            || this.renderer.framebuffer.msaaSamples.every((x) => x <= 1))
+        {
+            this.skip();
+        }
+        const { gl, CONTEXT_UID } = this.renderer;
+
+        const framebuffer = new Framebuffer(4, 4);
+
+        framebuffer.depth = false;
+        framebuffer.stencil = false;
+        framebuffer.multisample = MSAA_QUALITY.HIGH;
+
+        this.renderer.framebuffer.bind(framebuffer);
+
+        const fbo = framebuffer.glFramebuffers[CONTEXT_UID];
+
+        expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+        expect(fbo.multisample).to.be.greaterThan(1);
+        expect(fbo.msaaBuffer).to.be.not.null;
+        expect(fbo.stencil).to.be.null;
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, fbo.msaaBuffer);
+
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_SAMPLES)).to.equal(fbo.multisample);
+    });
+
+    it('should create a complete multisampled framebuffer with depth/stencil attachment', function ()
+    {
+        this.renderer.framebuffer.contextChange();
+
+        if (this.renderer.context.webGLVersion === 1
+            || this.renderer.framebuffer.msaaSamples === null
+            || this.renderer.framebuffer.msaaSamples.every((x) => x <= 1))
+        {
+            this.skip();
+        }
+
+        const { gl, CONTEXT_UID } = this.renderer;
+
+        const framebuffer = new Framebuffer(4, 4);
+
+        framebuffer.depth = true;
+        framebuffer.stencil = true;
+        framebuffer.multisample = MSAA_QUALITY.HIGH;
+
+        this.renderer.framebuffer.bind(framebuffer);
+
+        const fbo = framebuffer.glFramebuffers[CONTEXT_UID];
+
+        expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+        expect(fbo.multisample).to.be.greaterThan(1);
+        expect(fbo.msaaBuffer).to.be.not.null;
+        expect(fbo.stencil).to.be.not.null;
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, fbo.msaaBuffer);
+
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_SAMPLES)).to.equal(fbo.multisample);
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, fbo.stencil);
+
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_SAMPLES)).to.equal(fbo.multisample);
+    });
+
+    it('should not create an incomplete framebuffer if it has multiple color attachments and multisampling is requested',
+        function ()
+        {
+            this.renderer.framebuffer.contextChange();
+
+            const { gl, CONTEXT_UID } = this.renderer;
+
+            const framebuffer = new Framebuffer(4, 4);
+
+            framebuffer.depth = false;
+            framebuffer.stencil = false;
+            framebuffer.multisample = MSAA_QUALITY.HIGH;
+            framebuffer.addColorTexture(0);
+            framebuffer.addColorTexture(1);
+
+            this.renderer.framebuffer.bind(framebuffer);
+
+            const fbo = framebuffer.glFramebuffers[CONTEXT_UID];
+
+            expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+            expect(fbo.msaaBuffer).to.be.null;
+            expect(fbo.stencil).to.be.null;
+        });
+
+    it('should not create an incomplete framebuffer if it has depth texture and multisampling is requested',
+        function ()
+        {
+            this.renderer.framebuffer.contextChange();
+
+            if (!this.renderer.framebuffer.writeDepthTexture)
+            {
+                this.skip();
+            }
+
+            const { gl, CONTEXT_UID } = this.renderer;
+
+            const framebuffer = new Framebuffer(4, 4);
+
+            framebuffer.depth = false;
+            framebuffer.stencil = false;
+            framebuffer.multisample = MSAA_QUALITY.HIGH;
+            framebuffer.addDepthTexture();
+
+            this.renderer.framebuffer.bind(framebuffer);
+
+            const fbo = framebuffer.glFramebuffers[CONTEXT_UID];
+
+            expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+            expect(fbo.msaaBuffer).to.be.null;
+            expect(fbo.stencil).to.be.null;
+        });
+
+    it('should succeed to resize framebuffer', function ()
+    {
+        this.renderer.framebuffer.contextChange();
+
+        const { gl, CONTEXT_UID } = this.renderer;
+
+        const framebuffer = new Framebuffer(4, 8);
+
+        framebuffer.depth = true;
+        framebuffer.stencil = true;
+        framebuffer.multisample = MSAA_QUALITY.NONE;
+
+        this.renderer.framebuffer.bind(framebuffer);
+
+        const fbo = framebuffer.glFramebuffers[CONTEXT_UID];
+
+        expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+        expect(fbo.multisample).to.equal(0);
+        expect(fbo.msaaBuffer).to.be.null;
+        expect(fbo.stencil).to.be.not.null;
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, fbo.stencil);
+
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_SAMPLES)).to.equal(fbo.multisample);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_WIDTH)).to.equal(4);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_HEIGHT)).to.equal(8);
+
+        framebuffer.resize(16, 32);
+
+        this.renderer.framebuffer.bind(framebuffer);
+
+        expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+        expect(fbo.multisample).to.equal(0);
+        expect(fbo.msaaBuffer).to.be.null;
+        expect(fbo.stencil).to.be.not.null;
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, fbo.stencil);
+
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_SAMPLES)).to.equal(fbo.multisample);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_WIDTH)).to.equal(16);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_HEIGHT)).to.equal(32);
+    });
+
+    it('should succeed to resize multisampled framebuffer', function ()
+    {
+        this.renderer.framebuffer.contextChange();
+
+        if (this.renderer.context.webGLVersion === 1
+            || this.renderer.framebuffer.msaaSamples === null
+            || this.renderer.framebuffer.msaaSamples.every((x) => x <= 1))
+        {
+            this.skip();
+        }
+
+        const { gl, CONTEXT_UID } = this.renderer;
+
+        const framebuffer = new Framebuffer(4, 8);
+
+        framebuffer.depth = true;
+        framebuffer.stencil = true;
+        framebuffer.multisample = MSAA_QUALITY.HIGH;
+
+        this.renderer.framebuffer.bind(framebuffer);
+
+        const fbo = framebuffer.glFramebuffers[CONTEXT_UID];
+
+        expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+        expect(fbo.multisample).to.be.greaterThan(1);
+        expect(fbo.msaaBuffer).to.be.not.null;
+        expect(fbo.stencil).to.be.not.null;
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, fbo.msaaBuffer);
+
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_SAMPLES)).to.equal(fbo.multisample);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_WIDTH)).to.equal(4);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_HEIGHT)).to.equal(8);
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, fbo.stencil);
+
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_SAMPLES)).to.equal(fbo.multisample);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_WIDTH)).to.equal(4);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_HEIGHT)).to.equal(8);
+
+        framebuffer.resize(16, 32);
+
+        this.renderer.framebuffer.bind(framebuffer);
+
+        expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+        expect(fbo.multisample).to.be.greaterThan(1);
+        expect(fbo.msaaBuffer).to.be.not.null;
+        expect(fbo.stencil).to.be.not.null;
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, fbo.msaaBuffer);
+
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_SAMPLES)).to.equal(fbo.multisample);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_WIDTH)).to.equal(16);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_HEIGHT)).to.equal(32);
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, fbo.stencil);
+
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_SAMPLES)).to.equal(fbo.multisample);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_WIDTH)).to.equal(16);
+        expect(gl.getRenderbufferParameter(gl.RENDERBUFFER, gl.RENDERBUFFER_HEIGHT)).to.equal(32);
     });
 });
