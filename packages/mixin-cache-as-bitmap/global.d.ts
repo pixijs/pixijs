cfbd1eebd2d693a2ae99f8bc81b281e8dd23ba25
Matt Karl <matt@mattkarl.com>
2021-05-04 13:44:52 -0700

    Fix GlobalMixins to be non-optional to avoid issues with strictNullChecks (#7458)

--- packages/mixin-cache-as-bitmap/global.d.ts
+++ packages/mixin-cache-as-bitmap/global.d.ts
@@ -1,18 +1,18 @@
 declare namespace GlobalMixins
 {
     interface DisplayObject {
-        cacheAsBitmap?: boolean;
-        cacheAsBitmapResolution?: number;
-        _cacheAsBitmapResolution?: number;
-        _cacheAsBitmap?: boolean;
-        _cacheData?: import('@pixi/mixin-cache-as-bitmap').CacheData;
-        _renderCached?: (renderer: import('@pixi/core').Renderer) => void;
-        _initCachedDisplayObject?: (renderer: import('@pixi/core').Renderer) => void;
-        _calculateCachedBounds?: () => void;
-        _getCachedLocalBounds?: () => import('@pixi/math').Rectangle;
-        _renderCachedCanvas?: (renderer: import('@pixi/canvas-renderer').CanvasRenderer) => void;
-        _initCachedDisplayObjectCanvas?: (renderer: import('@pixi/canvas-renderer').CanvasRenderer) => void;
-        _destroyCachedDisplayObject?: () => void;
-        _cacheAsBitmapDestroy?: (options?: import('@pixi/display').IDestroyOptions|boolean) => void;
+        cacheAsBitmap: boolean;
+        cacheAsBitmapResolution: number;
+        _cacheAsBitmapResolution: number;
+        _cacheAsBitmap: boolean;
+        _cacheData: import('@pixi/mixin-cache-as-bitmap').CacheData;
+        _renderCached(renderer: import('@pixi/core').Renderer): void;
+        _initCachedDisplayObject(renderer: import('@pixi/core').Renderer): void;
+        _calculateCachedBounds(): void;
+        _getCachedLocalBounds(): import('@pixi/math').Rectangle;
+        _renderCachedCanvas(renderer: import('@pixi/canvas-renderer').CanvasRenderer): void;
+        _initCachedDisplayObjectCanvas(renderer: import('@pixi/canvas-renderer').CanvasRenderer): void;
+        _destroyCachedDisplayObject(): void;
+        _cacheAsBitmapDestroy(options?: import('@pixi/display').IDestroyOptions|boolean): void;
     }
 }
