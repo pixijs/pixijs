70bf3cca825beba2811c73f06249b7c43b80caba
Milton <miltoncandelero@gmail.com>
2021-06-10 18:20:59 -0300

    linting

--- packages/filters/filter-color-matrix/src/ColorMatrixFilter.ts
+++ packages/filters/filter-color-matrix/src/ColorMatrixFilter.ts
@@ -156,9 +156,9 @@ export class ColorMatrixFilter extends Filter
         const b = color & 0xff;
 
         const matrix: ColorMatrix = [
-            r/255, 0, 0, 0, 0,
-            0, g/255, 0, 0, 0,
-            0, 0, b/255, 0, 0,
+            r / 255, 0, 0, 0, 0,
+            0, g / 255, 0, 0, 0,
+            0, 0, b / 255, 0, 0,
             0, 0, 0, 1, 0,
         ];
 
