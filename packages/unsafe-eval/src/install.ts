2d9cea7114608d8de898f0bc3195be6502c6af96
Matt Karl <mattkarl@netflix.com>
2021-06-21 11:53:52 -0700

    Fix extra new Function call in unsafe-eval

--- packages/unsafe-eval/src/install.ts
+++ packages/unsafe-eval/src/install.ts
@@ -6,52 +6,33 @@ interface PIXICore {
     ShaderSystem: typeof ShaderSystem;
 }
 
-export function install(PIXI: PIXICore): void
+export function install({ ShaderSystem }: PIXICore): void
 {
-    if (!(PIXI?.ShaderSystem))
+    if (!ShaderSystem)
     {
         throw new Error('Unable to patch ShaderSystem, class not found.');
     }
 
-    const { ShaderSystem } = PIXI;
-    let proceed = false;
-
-    // Do a quick check to see if the patch is needed
-    // want to make sure we only apply if necessary!
-    try
-    {
-        ShaderSystem.prototype.systemCheck.call(null);
-        proceed = false;
-    }
-    catch (err)
-    {
-        proceed = true;
-    }
-
-    // Only apply if needed
-    if (proceed)
-    {
-        Object.assign(ShaderSystem.prototype,
+    Object.assign(ShaderSystem.prototype,
+        {
+            systemCheck()
             {
-                systemCheck()
-                {
-                // do nothing, don't throw error
-                },
-                syncUniforms(group: UniformGroup, glProgram: Program)
-                {
-                    const { shader, renderer } = (this as any);
+            // do nothing, don't throw error
+            },
+            syncUniforms(group: UniformGroup, glProgram: Program)
+            {
+                const { shader, renderer } = (this as any);
 
-                    /* eslint-disable max-len */
-                    syncUniforms(
-                        group,
-                        shader.program.uniformData,
-                        glProgram.uniformData,
-                        group.uniforms,
-                        renderer
-                    );
                 /* eslint-disable max-len */
-                },
-            }
-        );
-    }
+                syncUniforms(
+                    group,
+                    shader.program.uniformData,
+                    glProgram.uniformData,
+                    group.uniforms,
+                    renderer
+                );
+            /* eslint-disable max-len */
+            },
+        }
+    );
 }
