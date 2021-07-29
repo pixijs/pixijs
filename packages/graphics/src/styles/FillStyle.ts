c874bebfb7185efe44315ba3cbd50e36d13e94c2
Doehyun Baek <doehyunbaek@gmail.com>
2021-07-30 00:48:11 +0900

    Update graphics documentation (#7641)

--- packages/graphics/src/styles/FillStyle.ts
+++ packages/graphics/src/styles/FillStyle.ts
@@ -12,17 +12,11 @@ export class FillStyle
     /**
      * The hex color value used when coloring the Graphics object.
      *
-     * @member {number}
      * @default 0xFFFFFF
      */
     public color = 0xFFFFFF;
 
-    /**
-     * The alpha value used when filling the Graphics object.
-     *
-     * @member {number}
-     * @default 1
-     */
+    /** The alpha value used when filling the Graphics object. */
     public alpha = 1.0;
 
     /**
@@ -41,12 +35,7 @@ export class FillStyle
      */
     public matrix: Matrix = null;
 
-    /**
-     * If the current fill is visible.
-     *
-     * @member {boolean}
-     * @default false
-     */
+    /** If the current fill is visible. */
     public visible = false;
 
     constructor()
