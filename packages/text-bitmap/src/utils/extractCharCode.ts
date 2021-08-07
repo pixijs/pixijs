bc332f9ac32e457ba0dd30065f82314b701452b1
Ilya <41614937+s-r-x@users.noreply.github.com>
2021-08-07 07:14:08 +0500

    Update packages/text-bitmap/src/utils/extractCharCode.ts
    Co-authored-by: Matt Karl <matt@mattkarl.com>

--- packages/text-bitmap/src/utils/extractCharCode.ts
+++ packages/text-bitmap/src/utils/extractCharCode.ts
@@ -1,3 +1,7 @@
+/**
+ * Ponyfill for IE because it doesn't support `codePointAt`
+ * @private
+ */
 export function extractCharCode(str: string): number
 {
     return str.codePointAt ? str.codePointAt(0) : str.charCodeAt(0);
