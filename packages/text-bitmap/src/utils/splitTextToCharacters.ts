7db462f2ec31be8b0152ea25687ace357bc09b0f
Ilya <41614937+s-r-x@users.noreply.github.com>
2021-08-07 07:14:16 +0500

    Update packages/text-bitmap/src/utils/splitTextToCharacters.ts
    Co-authored-by: Matt Karl <matt@mattkarl.com>

--- packages/text-bitmap/src/utils/splitTextToCharacters.ts
+++ packages/text-bitmap/src/utils/splitTextToCharacters.ts
@@ -1,3 +1,7 @@
+/**
+ * Ponyfill for IE because it doesn't support `Array.from`
+ * @private
+ */
 export function splitTextToCharacters(text: string): string[]
 {
     return Array.from ? Array.from(text) : text.split('');
