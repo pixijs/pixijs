ec4366c5f1436b33e831014fb42a32dcd8f8bdb1
dev7355608 <dev7355608@gmail.com>
2021-08-24 01:52:07 +0200

    Improve test

--- packages/mesh/test/Mesh.tests.ts
+++ packages/mesh/test/Mesh.tests.ts
@@ -9,8 +9,8 @@ describe('Mesh', function ()
 {
     it('should dispose geometry correctly', function ()
     {
-        const geometry1 = new MeshGeometry();
-        const geometry2 = new MeshGeometry();
+        const geometry1 = new MeshGeometry([0, 0]);
+        const geometry2 = new MeshGeometry([1, 1]);
 
         const dispose1 = sinon.spy(geometry1, 'dispose');
         const dispose2 = sinon.spy(geometry2, 'dispose');
@@ -23,14 +23,24 @@ describe('Mesh', function ()
         const mesh = new Mesh(geometry1, new MeshMaterial());
 
         expect(mesh.geometry).to.equal(geometry1);
+        expect(mesh.vertexDirty).to.equal(-1);
         expect(geometry1.refCount).to.equal(1);
         expect(geometry2.refCount).to.equal(0);
         expect(dispose1.called).to.be.false;
         expect(dispose2.called).to.be.false;
 
+        mesh.calculateVertices();
+
+        expect(mesh.vertexDirty).to.equal(mesh.verticesBuffer._updateID);
+        expect(mesh.vertexData[0]).to.equal(0);
+        expect(mesh.vertexData[1]).to.equal(0);
+
         mesh.geometry = geometry1;
 
         expect(mesh.geometry).to.equal(geometry1);
+        expect(mesh.vertexDirty).to.equal(mesh.verticesBuffer._updateID);
+        expect(mesh.vertexData[0]).to.equal(0);
+        expect(mesh.vertexData[1]).to.equal(0);
         expect(geometry1.refCount).to.equal(1);
         expect(geometry2.refCount).to.equal(0);
         expect(dispose1.called).to.be.false;
@@ -39,14 +49,22 @@ describe('Mesh', function ()
         mesh.geometry = geometry2;
 
         expect(mesh.geometry).to.equal(geometry2);
+        expect(mesh.vertexDirty).to.equal(-1);
         expect(geometry1.refCount).to.equal(0);
         expect(geometry2.refCount).to.equal(1);
         expect(dispose1.called).to.be.true;
         expect(dispose2.called).to.be.false;
 
+        mesh.calculateVertices();
+
+        expect(mesh.vertexDirty).to.equal(mesh.verticesBuffer._updateID);
+        expect(mesh.vertexData[0]).to.equal(1);
+        expect(mesh.vertexData[1]).to.equal(1);
+
         mesh.destroy();
 
         expect(mesh.geometry).to.equal(null);
+        expect(mesh.vertexDirty).to.equal(-1);
         expect(geometry1.refCount).to.equal(0);
         expect(geometry2.refCount).to.equal(0);
         expect(dispose1.called).to.be.true;
