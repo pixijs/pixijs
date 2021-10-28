68c7054a226cd1eaf3918ba3e14ff41d4d46d9a3
Matt Karl <mattkarl@netflix.com>
2021-10-28 19:22:52 -0400

    Simplify the build process

--- scripts/fixPeerVersions.ts
+++ scripts/fixPeerVersions.ts
@@ -3,22 +3,29 @@ import path from 'path';
 import fs from 'fs';
 import util from 'util';
 
-/**
- * Simplified interface for a package.json
- */
+/** Basic package.json structure */
+interface PackageConfig
+{
+    version: string;
+    name: string;
+    peerDependencies?: Record<string, string>;
+    devDependencies?: Record<string, string>;
+    dependencies?: Record<string, string>;
+}
+
+/** Simplified interface for a package.json */
 interface Package
 {
     name: string;
     dir: string;
-    config: {
-        version: string;
-        peerDependencies?: Record<string, string>;
-    }
+    config: PackageConfig;
 }
 
 // Create utils for writing JSON file
 const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
 const writeJson = (file: string, data: unknown) => writeFile(file, `${JSON.stringify(data, null, 2)}\n`);
+const readJson = (file: string) => readFile(file, 'utf8').then((data) => JSON.parse(data));
 
 /**
  * The goal of this script is to sync the peer versions to exact. Lerna does
@@ -26,34 +33,76 @@ const writeJson = (file: string, data: unknown) => writeFile(file, `${JSON.strin
  */
 async function main(): Promise<void>
 {
+    const cwd = process.cwd();
     const versions: Record<string, string> = {};
-    const packagesWithPeers: Package[] = [];
+    const packages: Package[] = [];
+    const pathToPackages: Record<string, { pkg: Package, entry: PackageConfig }> = {};
+    const lockFile = path.join(cwd, 'package-lock.json');
+    const lock = await readJson(lockFile);
 
-    await workspacesRun({ cwd: process.cwd() }, async (pkg) =>
+    // Get all the packages and create a map of versions
+    await workspacesRun({ cwd }, async (pkg) =>
     {
         versions[pkg.name] = pkg.config.version;
 
-        if (pkg.config.peerDependencies)
+        const relativePath = path.relative(cwd, pkg.dir);
+
+        if (relativePath in lock.packages)
         {
-            packagesWithPeers.push(pkg);
+            pathToPackages[relativePath] = { pkg, entry: lock.packages[relativePath] };
         }
+        packages.push(pkg);
     });
 
-    for (const p of packagesWithPeers)
+    // Update the peer dependencies in packages
+    // because Lerna doesn't do this for us
+    for (const p of packages)
     {
-        const json = p.config;
-        const peers = json.peerDependencies;
+        const { peerDependencies: peers } = p.config;
 
-        for (const n in peers)
+        if (peers)
         {
-            if (n in versions)
+            for (const n in peers)
             {
-                peers[n] = versions[n];
+                if (n in versions)
+                {
+                    peers[n] = versions[n];
+                }
             }
-        }
 
-        await writeJson(path.join(p.dir, 'package.json'), json);
+            await writeJson(path.join(p.dir, 'package.json'), p.config);
+        }
     }
+
+    // `lerna version` doesn't update package-lock.json for npm 7
+    // so we disable Lerna auto-tagging/pushing and do it ourself
+    // See https://github.com/lerna/lerna/issues/2891
+    for (const relativePath in pathToPackages)
+    {
+        const { pkg, entry } = pathToPackages[relativePath];
+
+        entry.version = pkg.config.version;
+        entry.dependencies = pkg.config?.dependencies;
+        entry.devDependencies = pkg.config?.devDependencies;
+        entry.peerDependencies = pkg.config?.peerDependencies;
+    }
+
+    // Update the packages' requires field in lock file
+    Object.keys(lock.dependencies)
+        .filter((name) => !!versions[name])
+        .map((name) => lock.dependencies[name].requires)
+        .forEach((requires: Record<string, string>) =>
+        {
+            for (const n in requires)
+            {
+                if (n in versions)
+                {
+                    requires[n] = versions[n];
+                }
+            }
+        });
+
+    await writeJson(lockFile, lock);
 }
 
 main();
