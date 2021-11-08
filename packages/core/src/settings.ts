0b196f9347b8d2290aa3ff5de05409acf70e036c
dev7355608 <dev7355608@gmail.com>
2021-11-08 12:40:17 +0100

    Set PREFER_ENV default to WEBGL2 for all devices (#7899)

--- packages/core/src/settings.ts
+++ packages/core/src/settings.ts
@@ -1,6 +1,5 @@
 import { settings } from '@pixi/settings';
 import { ENV } from '@pixi/constants';
-import { isMobile } from '@pixi/utils';
 
 /**
  * The maximum support for using WebGL. If a device does not
@@ -9,16 +8,13 @@ import { isMobile } from '@pixi/utils';
  * explicitly remove feature support to target a more stable
  * baseline, prefer a lower environment.
  *
- * Due to {@link https://bugs.chromium.org/p/chromium/issues/detail?id=934823|bug in chromium}
- * we disable webgl2 by default for all non-apple mobile devices.
- *
  * @static
  * @name PREFER_ENV
  * @memberof PIXI.settings
  * @type {number}
  * @default PIXI.ENV.WEBGL2
  */
-settings.PREFER_ENV = isMobile.any ? ENV.WEBGL : ENV.WEBGL2;
+settings.PREFER_ENV = ENV.WEBGL2;
 
 /**
  * If set to `true`, *only* Textures and BaseTexture objects stored
