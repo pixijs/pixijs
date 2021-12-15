9c87826e7fc99e62479b820e7707fe7cdc1e66e6
Joshua Pelealu <hasianjoshua@gmail.com>
2021-12-15 13:35:39 +0000

    Change 'self' into 'globalThis'

--- packages/settings/src/utils/isMobile.ts
+++ packages/settings/src/utils/isMobile.ts
@@ -1,9 +1,9 @@
 // The ESM/CJS versions of ismobilejs only
 // exports the function for executing
 // designed for Node-only environments
-import isMobileCall from 'ismobilejs';
-import type { isMobileResult } from 'ismobilejs';
+import isMobileCall from "ismobilejs";
+import type { isMobileResult } from "ismobilejs";
 
-const isMobile: isMobileResult = isMobileCall(self.navigator);
+const isMobile: isMobileResult = isMobileCall(globalThis.navigator);
 
 export { isMobile };
