55d794061eefba8bd6999cd7260e1ccfa0931c8d
Shukant Pal <SukantK2002@outlook.com>
2022-02-07 12:40:24 -0500

    Reformat documentation for Graphics, GraphicsData, GraphicsGeometry, FillStyle, LineStyle (#8168)

--- packages/graphics/src/styles/LineStyle.ts
+++ packages/graphics/src/styles/LineStyle.ts
@@ -3,9 +3,8 @@ import { LINE_JOIN, LINE_CAP } from '../const';
 
 /**
  * Represents the line style for Graphics.
+ *
  * @memberof PIXI
- * @class
- * @extends PIXI.FillStyle
  */
 export class LineStyle extends FillStyle
 {
@@ -15,7 +14,7 @@ export class LineStyle extends FillStyle
     /** The alignment of any lines drawn (0.5 = middle, 1 = outer, 0 = inner). WebGL only. */
     public alignment = 0.5;
 
-    /** If true the lines will be draw using LINES instead of TRIANGLE_STRIP */
+    /** If true the lines will be draw using LINES instead of TRIANGLE_STRIP. */
     public native = false;
 
     /**
@@ -37,11 +36,7 @@ export class LineStyle extends FillStyle
     /** Miter limit. */
     public miterLimit = 10;
 
-    /**
-     * Clones the object
-     *
-     * @return {PIXI.LineStyle}
-     */
+    /** Clones the object. */
     public clone(): LineStyle
     {
         const obj = new LineStyle();
@@ -61,9 +56,7 @@ export class LineStyle extends FillStyle
         return obj;
     }
 
-    /**
-     * Reset the line style to default.
-     */
+    /** Reset the line style to default. */
     public reset(): void
     {
         super.reset();
