e5cfbfcaf4a5b960c2fdd6044080f612fc4b59b5
dev7355608 <dev7355608@gmail.com>
2022-02-15 18:25:44 +0100

    Fix rounding in getOptimalTexture

--- packages/core/test/RenderTexturePool.tests.ts
+++ packages/core/test/RenderTexturePool.tests.ts
@@ -29,4 +29,37 @@ describe('RenderTexturePool', function ()
 
         renderTexturePool.clear(true);
     });
+
+    it('should create screen-sized texture with noninteger resolution', function ()
+    {
+        const resolution = 1.1;
+        const viewWidth = 1419;
+        const viewHeight = 983;
+        const screenWidth = viewWidth / resolution;
+        const screenHeight = viewHeight / resolution;
+
+        expect(screenWidth * resolution).to.equal(1419.0000000000002);
+        expect(screenHeight * resolution).to.equal(982.9999999999999);
+
+        const renderTexturePool = new RenderTexturePool();
+
+        renderTexturePool.setScreenSize({ width: viewWidth, height: viewHeight });
+
+        expect(renderTexturePool.enableFullScreen).to.be.true;
+
+        const renderTexture = renderTexturePool.getOptimalTexture(screenWidth, screenHeight, resolution);
+        const baseRenderTexture = renderTexture.baseTexture;
+
+        expect(baseRenderTexture.width).to.equal(screenWidth);
+        expect(baseRenderTexture.height).to.equal(screenHeight);
+        expect(baseRenderTexture.realWidth).to.equal(viewWidth);
+        expect(baseRenderTexture.realHeight).to.equal(viewHeight);
+        expect(renderTexturePool.texturePool[RenderTexturePool.SCREEN_KEY]?.length ?? 0).to.equal(0);
+
+        renderTexturePool.returnTexture(renderTexture);
+
+        expect(renderTexturePool.texturePool[RenderTexturePool.SCREEN_KEY]?.length ?? 0).to.equal(1);
+
+        renderTexturePool.clear(true);
+    });
 });
