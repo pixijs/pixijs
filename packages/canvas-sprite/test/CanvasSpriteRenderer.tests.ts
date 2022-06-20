6a2cb93eb77813f20d1b3f6fdc01cbabcc65956f
Lukas Steinmetz <drspoik@users.noreply.github.com>
2022-06-20 11:47:36 +0200

    added test for canvas rendering of scaled base textures

--- packages/canvas-sprite/test/CanvasSpriteRenderer.tests.ts
+++ packages/canvas-sprite/test/CanvasSpriteRenderer.tests.ts
@@ -1,7 +1,10 @@
-import { Texture } from '@pixi/core';
+import { BaseTexture, CanvasResource, Texture } from '@pixi/core';
 import { Sprite } from '@pixi/sprite';
 import { CanvasRenderer } from '@pixi/canvas-renderer';
 import { expect } from 'chai';
+import { Container } from '@pixi/display';
+import { MIPMAP_MODES, SCALE_MODES } from '@pixi/constants';
+import { Rectangle } from '@pixi/math';
 
 describe('CanvasSpriteRenderer', () =>
 {
@@ -23,4 +26,56 @@ describe('CanvasSpriteRenderer', () =>
             renderer.destroy();
         }
     });
+
+    it('should scale the base texture correctly using a trimmed rect', () =>
+    {
+        //create 2x1 texture, left pixel red, right pixel green
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+
+        canvas.width = 2;
+        canvas.height = 1;
+
+        context.fillStyle = "#ff0000";
+        context.fillRect(0, 0, 1, 1);
+
+        context.fillStyle = "#00ff00";
+        context.fillRect(1, 0, 1, 1);
+
+        const baseTexture = new BaseTexture(new CanvasResource(canvas), {
+            mipmap: MIPMAP_MODES.OFF, 
+            scaleMode: SCALE_MODES.NEAREST
+        });
+
+        //set a scale value to orig and trim rectangle to scale the texture
+        const scale = 2;
+
+        const frame = new Rectangle(0, 0, baseTexture.width,         baseTexture.height);
+        const orig  = new Rectangle(0, 0, baseTexture.width * scale, baseTexture.height * scale);
+        const trim  = new Rectangle(0, 0, baseTexture.width * scale, baseTexture.height * scale);
+
+        let testSprite = new Sprite(new Texture(baseTexture, frame, orig, trim));
+
+        let stage = new Container();
+        stage.addChild(testSprite);
+
+        let renderer = new CanvasRenderer();
+        renderer.render(stage);
+
+        let ctx = renderer.view.getContext("2d");
+
+        let pixels = ctx.getImageData(0, 0, 2 * scale, 1 * scale).data;
+
+        expect(pixels[0]).to.equal(255);
+        expect(pixels[1]).to.equal(0);
+        expect(pixels[2]).to.equal(0);
+        expect(pixels[3]).to.equal(255);
+
+        expect(pixels[8]).to.equal(0);
+        expect(pixels[9]).to.equal(255);
+        expect(pixels[10]).to.equal(0);
+        expect(pixels[11]).to.equal(255);
+
+    });
+
 });
