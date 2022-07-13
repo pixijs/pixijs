493f02c9d5163cec519f220b5daa061c2f8a40cd
Zyie <24736175+Zyie@users.noreply.github.com>
2022-07-13 17:11:13 +0100

    add ktx test

--- packages/assets/test/resolver.tests.ts
+++ packages/assets/test/resolver.tests.ts
@@ -1,9 +1,45 @@
-import { resolveSpriteSheetUrl, resolveTextureUrl } from '@pixi/assets';
+import { resolveCompressedTextureUrl, resolveSpriteSheetUrl, resolveTextureUrl } from '@pixi/assets';
 import { Resolver } from '../src/resolver/Resolver';
 import { manifest } from './sampleManifest';
 
 describe('Resolver', () =>
 {
+    it('should resolve asset', () =>
+    {
+        const resolver = new Resolver();
+
+        resolver.addUrlParser(resolveCompressedTextureUrl);
+
+        resolver.prefer({
+            priority: ['format'],
+            params: {
+                format: ['s3tc', 's3tc_sRGB', 'png', 'webp'],
+                resolution: 1
+            }
+        });
+
+        resolver.add('test', [
+            {
+                resolution: 1,
+                format: 'png',
+                src: 'my-image.png',
+            },
+            {
+                resolution: 1,
+                format: 'webp',
+                src: 'my-image.webp',
+            },
+            {
+                resolution: 1,
+                format: 's3tc',
+                src: 'my-image.s3tc.ktx',
+            },
+        ]);
+
+        const asset = resolver.resolveUrl('test');
+
+        expect(asset).toEqual('my-image.s3tc.ktx');
+    });
     it('should resolve asset', () =>
     {
         const resolver = new Resolver();
