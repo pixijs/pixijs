c5ec8ae53be557fbef9f248853e65660c28c8dba
Zyie <24736175+Zyie@users.noreply.github.com>
2022-08-26 11:23:01 +0100

    remove more unused resources

--- packages/assets/test/loader.tests.ts
+++ packages/assets/test/loader.tests.ts
@@ -1,4 +1,3 @@
-import type { ImageResource } from '@pixi/core';
 import { Texture } from '@pixi/core';
 
 import type { LoaderParser } from '@pixi/assets';
@@ -226,24 +225,5 @@ describe('Loader', () =>
         });
 
         expect(foundFont).toBe(false);
-    });
-
-    it('should split fonts if page IDs are in chronological order', async () =>
-    {
-        const loader = new Loader();
-
-        loader['_parsers'].push(loadTextures, loadBitmapFont);
-
-        const font = await loader.load(`${serverPath}bitmap-font/split_font2.fnt`);
-
-        const charA = font.chars['A'.charCodeAt(0)];
-        const charC = font.chars['C'.charCodeAt(0)];
-        const charATexture = charA.texture as Texture<ImageResource>;
-        const charCTexture = charC.texture as Texture<ImageResource>;
-
-        expect(charA.page).toEqual(0);
-        expect(charC.page).toEqual(1);
-        expect(charATexture.baseTexture.resource.src).toEqual(`${serverPath}bitmap-font/split_font_ab.png`);
-        expect(charCTexture.baseTexture.resource.src).toEqual(`${serverPath}bitmap-font/split_font_cd.png`);
     });
 });
