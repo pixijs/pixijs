7423e45134d00bd8f639cacea5664bd6e2995fed
Matt Karl <matt@mattkarl.com>
2022-09-15 11:16:36 -0400

    Update index.ts

--- packages/assets/src/detections/index.ts
+++ packages/assets/src/detections/index.ts
@@ -14,14 +14,14 @@ export interface FormatDetectionParser
     /**
      * Add formats (file extensions) to the existing list of formats.
      * Return an new array with added formats, do not mutate the formats argument.
-     * @return {Promise<string[]>} - Promise that resolves to the new formats array.
+     * @returns {Promise<string[]>} - Promise that resolves to the new formats array.
      */
     add: (formats: string[]) => Promise<string[]>,
     /**
      * Remove formats (file extensions) from the list of supported formats.
      * This is used when uninstalling this DetectionParser.
      * Return an new array with filtered formats, do not mutate the formats argument.
-     * @return {Promise<string[]>} - Promise that resolves to the new formats array.
+     * @returns {Promise<string[]>} - Promise that resolves to the new formats array.
      */
     remove: (formats: string[]) => Promise<string[]>,
 }
