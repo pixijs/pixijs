77f6ffc71d8e6f0e373786756567fb70da7cad7b
Bryan Housel <bryan@7thposition.com>
2022-09-29 15:33:10 -0400

    Transfer the shiftKey property for federated events (#8674)

--- packages/events/src/EventSystem.ts
+++ packages/events/src/EventSystem.ts
@@ -663,6 +663,7 @@ export class EventSystem
         event.page.x = nativeEvent.pageX;
         event.page.y = nativeEvent.pageY;
         event.relatedTarget = null;
+        event.shiftKey = nativeEvent.shiftKey;
     }
 }
 
