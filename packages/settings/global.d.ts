4f16e60f3dd681999e37a4663af705ecb616da56
SuperSodaSea <bobby825@126.com>
2022-10-04 02:09:12 +0800

    Fix ICanvas in package/settings/global.d.ts

--- packages/settings/global.d.ts
+++ packages/settings/global.d.ts
@@ -1,7 +1,7 @@
 declare namespace GlobalMixins
 {
     // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface ICanvas extends GlobalTintable
+    interface ICanvas
     {
 
     }
