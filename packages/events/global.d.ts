0a95fa0ff2e3ff1c8dc8dbcf9f0a93925cb06a99
Matt Karl <matt@mattkarl.com>
2022-10-24 14:05:35 -0400

    Decouple events from display

--- packages/events/global.d.ts
+++ packages/events/global.d.ts
@@ -9,6 +9,12 @@ declare namespace GlobalMixins
 
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface DisplayObjectEvents extends FederatedEventEmitterTypes
+    {
+
+    }
+
     interface IRenderer
     {
         readonly events: import('@pixi/events').EventSystem;
