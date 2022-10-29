cd075fa935fa5b988f0874f39222c673840553dc
Matt Karl <matt@mattkarl.com>
2022-10-29 16:55:51 -0400

    Fix publishing for `@pixi/unsafe-eval` (#8795)

--- packages/unsafe-eval/src/bundle.ts
+++
@@ -1,8 +0,0 @@
-import { install } from './install';
-
-if (typeof (globalThis as any).PIXI === 'undefined')
-{
-    throw new Error('Global PIXI not found.');
-}
-
-install((globalThis as any).PIXI);
