8ec8abb0df837cb70f6fedbef3162bdc676b37bb
Adam Savard <savardfs@gmail.com>
2022-11-21 16:24:09 -0400

    Added documentation + examples

--- packages/events/src/FederatedEventTarget.ts
+++ packages/events/src/FederatedEventTarget.ts
@@ -1,5 +1,7 @@
 import { DisplayObject } from '@pixi/display';
 import { FederatedEvent } from './FederatedEvent';
+import type { FederatedPointerEvent } from './FederatedPointerEvent';
+import type { FederatedWheelEvent } from './FederatedWheelEvent';
 
 import type { utils } from '@pixi/core';
 import type { FederatedEventMap } from './FederatedEventMap';
@@ -83,6 +85,7 @@ export interface FederatedEventTarget extends utils.EventEmitter, EventTarget
 
 type AddListenerOptions = boolean | AddEventListenerOptions;
 type RemoveListenerOptions = boolean | EventListenerOptions;
+type EventHandler<T= FederatedPointerEvent> = (event: T) => void;
 
 export interface IFederatedDisplayObject
     extends Omit<FederatedEventTarget, 'parent' | 'children' | keyof utils.EventEmitter | 'cursor'>
@@ -107,9 +110,369 @@ export interface IFederatedDisplayObject
         listener: EventListenerOrEventListenerObject,
         options?: RemoveListenerOptions
     ): void;
+    onclick: EventHandler | null;
+    onmousedown: EventHandler | null;
+    onmouseenter: EventHandler | null;
+    onmouseleave: EventHandler | null;
+    onmousemove: EventHandler | null;
+    onmouseout: EventHandler | null;
+    onmouseover: EventHandler | null;
+    onmouseup: EventHandler | null;
+    onmouseupoutside: EventHandler | null;
+    onpointercancel: EventHandler | null;
+    onpointerdown: EventHandler | null;
+    onpointerenter: EventHandler | null;
+    onpointerleave: EventHandler | null;
+    onpointermove: EventHandler | null;
+    onpointerout: EventHandler | null;
+    onpointerover: EventHandler | null;
+    onpointertap: EventHandler | null;
+    onpointerup: EventHandler | null;
+    onpointerupoutside: EventHandler | null;
+    onrightclick: EventHandler | null;
+    onrightdown: EventHandler | null;
+    onrightup: EventHandler | null;
+    onrightupoutside: EventHandler | null;
+    ontap: EventHandler | null;
+    ontouchcancel: EventHandler | null;
+    ontouchend: EventHandler | null;
+    ontouchendoutside: EventHandler | null;
+    ontouchmove: EventHandler | null;
+    ontouchstart: EventHandler | null;
+    onwheel: EventHandler<FederatedWheelEvent> | null;
 }
 
 export const FederatedDisplayObject: IFederatedDisplayObject = {
+
+    /**
+     * Property-based event handler for the `click` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onclick = (event) => {
+     *  //some function here that happens on click
+     * }
+     */
+    onclick: null,
+
+    /**
+     * Property-based event handler for the `mousedown` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onmousedown = (event) => {
+     *  //some function here that happens on mousedown
+     * }
+     */
+    onmousedown: null,
+
+    /**
+     * Property-based event handler for the `mouseenter` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onmouseenter = (event) => {
+     *  //some function here that happens on mouseenter
+     * }
+     */
+    onmouseenter: null,
+
+    /**
+     * Property-based event handler for the `mouseleave` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onmouseleave = (event) => {
+     *  //some function here that happens on mouseleave
+     * }
+     */
+    onmouseleave: null,
+
+    /**
+     * Property-based event handler for the `mousemove` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onmousemove = (event) => {
+     *  //some function here that happens on mousemove
+     * }
+     */
+    onmousemove: null,
+
+    /**
+     * Property-based event handler for the `mouseout` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onmouseout = (event) => {
+     *  //some function here that happens on mouseout
+     * }
+     */
+    onmouseout: null,
+
+    /**
+     * Property-based event handler for the `mouseover` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onmouseover = (event) => {
+     *  //some function here that happens on mouseover
+     * }
+     */
+    onmouseover:  null,
+
+    /**
+     * Property-based event handler for the `mouseup` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onmouseup = (event) => {
+     *  //some function here that happens on mouseup
+     * }
+     */
+    onmouseup:  null,
+
+    /**
+     * Property-based event handler for the `mouseupoutside` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onmouseupoutside = (event) => {
+     *  //some function here that happens on mouseupoutside
+     * }
+     */
+    onmouseupoutside:  null,
+
+    /**
+     * Property-based event handler for the `pointercancel` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onpointercancel = (event) => {
+     *  //some function here that happens on pointercancel
+     * }
+     */
+    onpointercancel:  null,
+
+    /**
+     * Property-based event handler for the `pointerdown` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onpointerdown = (event) => {
+     *  //some function here that happens on pointerdown
+     * }
+     */
+    onpointerdown:  null,
+
+    /**
+     * Property-based event handler for the `pointerenter` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onpointerenter = (event) => {
+     *  //some function here that happens on pointerenter
+     * }
+     */
+    onpointerenter:  null,
+
+    /**
+     * Property-based event handler for the `pointerleave` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onpointerleave = (event) => {
+     *  //some function here that happens on pointerleave
+     * }
+     */
+    onpointerleave:  null,
+
+    /**
+     * Property-based event handler for the `pointermove` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onpointermove = (event) => {
+     *  //some function here that happens on pointermove
+     * }
+     */
+    onpointermove:  null,
+
+    /**
+     * Property-based event handler for the `pointerout` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onpointerout = (event) => {
+     *  //some function here that happens on pointerout
+     * }
+     */
+    onpointerout:  null,
+
+    /**
+     * Property-based event handler for the `pointerover` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onpointerover = (event) => {
+     *  //some function here that happens on pointerover
+     * }
+     */
+    onpointerover:  null,
+
+    /**
+     * Property-based event handler for the `pointertap` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onpointertap = (event) => {
+     *  //some function here that happens on pointertap
+     * }
+     */
+    onpointertap:  null,
+
+    /**
+     * Property-based event handler for the `pointerup` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onpointerup = (event) => {
+     *  //some function here that happens on pointerup
+     * }
+     */
+    onpointerup:  null,
+
+    /**
+     * Property-based event handler for the `pointerupoutside` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onpointerupoutside = (event) => {
+     *  //some function here that happens on pointerupoutside
+     * }
+     */
+    onpointerupoutside:  null,
+
+    /**
+     * Property-based event handler for the `rightclick` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onrightclick = (event) => {
+     *  //some function here that happens on rightclick
+     * }
+     */
+    onrightclick:  null,
+
+    /**
+     * Property-based event handler for the `rightdown` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onrightdown = (event) => {
+     *  //some function here that happens on rightdown
+     * }
+     */
+    onrightdown:  null,
+
+    /**
+     * Property-based event handler for the `rightup` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onrightup = (event) => {
+     *  //some function here that happens on rightup
+     * }
+     */
+    onrightup:  null,
+
+    /**
+     * Property-based event handler for the `rightupoutside` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onrightupoutside = (event) => {
+     *  //some function here that happens on rightupoutside
+     * }
+     */
+    onrightupoutside:  null,
+
+    /**
+     * Property-based event handler for the `tap` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.ontap = (event) => {
+     *  //some function here that happens on tap
+     * }
+     */
+    ontap:  null,
+
+    /**
+     * Property-based event handler for the `touchcancel` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.ontouchcancel = (event) => {
+     *  //some function here that happens on touchcancel
+     * }
+     */
+    ontouchcancel:  null,
+
+    /**
+     * Property-based event handler for the `touchend` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.ontouchend = (event) => {
+     *  //some function here that happens on touchend
+     * }
+     */
+    ontouchend:  null,
+
+    /**
+     * Property-based event handler for the `touchendoutside` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.ontouchendoutside = (event) => {
+     *  //some function here that happens on touchendoutside
+     * }
+     */
+    ontouchendoutside:  null,
+
+    /**
+     * Property-based event handler for the `touchmove` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.ontouchmove = (event) => {
+     *  //some function here that happens on touchmove
+     * }
+     */
+    ontouchmove:  null,
+
+    /**
+     * Property-based event handler for the `touchstart` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.ontouchstart = (event) => {
+     *  //some function here that happens on touchstart
+     * }
+     */
+    ontouchstart:  null,
+
+    /**
+     * Property-based event handler for the `wheel` event.
+     * @memberof PIXI.DisplayObject#
+     * @default null
+     * @example
+     * this.onwheel = (event) => {
+     *  //some function here that happens on wheel
+     * }
+     */
+    onwheel:  null,
     /**
      * Enable interaction events for the DisplayObject. Touch, pointer and mouse
      * events will not be emitted unless `interactive` is set to `true`.
