0937ce6988eeb5777580992ab4d0792e8c5d9616
viniciusbitt <vbittencourta@gmail.com>
2023-02-20 15:01:18 -0300

    Fixes lint

--- packages/events/src/EventBoundary.ts
+++ packages/events/src/EventBoundary.ts
@@ -868,8 +868,8 @@ export class EventBoundary
     }
 
     /**
-     * Maps the upstream `pointerup` event to downstream `pointerup`, `pointerupoutside`, and `click`/`rightclick`/`pointertap` events,
-     * in that order.
+     * Maps the upstream `pointerup` event to downstream `pointerup`, `pointerupoutside`,
+     * and `click`/`rightclick`/`pointertap` events, in that order.
      *
      * The `pointerupoutside` event bubbles from the original `pointerdown` target to the most specific
      * ancestor of the `pointerdown` and `pointerup` targets, which is also the `click` event's target. `touchend`,
