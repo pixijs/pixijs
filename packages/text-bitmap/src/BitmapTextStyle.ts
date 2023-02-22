b2c830ab47b4bafd30b126a52a1d0a118243a81f
Matt Karl <matt@mattkarl.com>
2023-02-22 06:20:23 -0500

    Support ColorSource with Mesh & BitmapText tint (#9181)

--- packages/text-bitmap/src/BitmapTextStyle.ts
+++ packages/text-bitmap/src/BitmapTextStyle.ts
@@ -1,10 +1,11 @@
+import type { ColorSource } from '@pixi/core';
 import type { TextStyleAlign } from '@pixi/text';
 
 export interface IBitmapTextStyle
 {
     fontName: string;
     fontSize: number;
-    tint: number;
+    tint: ColorSource;
     align: TextStyleAlign;
     letterSpacing: number;
     maxWidth: number;
