a3d0b0c6e668b9a074c50a7ee196c330425cc18d
Alex Prokop <alex.prokop@play.co>
2023-03-20 18:24:57 +0000

    just publish everything :)

--- scripts/prepublish.ts
+++ scripts/prepublish.ts
@@ -1,6 +1,5 @@
 import path from 'path';
 import workspacesRun from 'workspaces-run';
-import { bumpDependencies } from './bumpDependencies';
 import { readJSON, writeJSON } from './utils/json';
 
 /** Before publish, let's replace all the relative file links with versions */
@@ -33,6 +32,23 @@ async function main()
 
         await writeJSON(path.join(workspace.dir, 'package.json'), workspace.config);
     });
+}
+
+/**
+ * Update the *dependencies in the package.json
+ * @param dependencies - Dependencies map
+ * @param version - Version to bump to
+ */
+export function bumpDependencies(dependencies: Record<string, string> = {}, version: string)
+{
+    Object.keys(dependencies)
+        // Only consider bumping monorepo packages
+        .filter((n) => dependencies[n].startsWith('file:'))
+        .forEach((n) =>
+        {
+            // replace with exact version
+            dependencies[n] = version;
+        });
 }
 
 main();
