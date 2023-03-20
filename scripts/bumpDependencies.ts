d613a82190881325ca42ec81ee459e00951450b2
Alex Prokop <alex.prokop@play.co>
2023-03-20 16:01:51 +0000

    dynamically generate subpackage dependency urls

--- scripts/bumpDependencies.ts
+++ scripts/bumpDependencies.ts
@@ -1,9 +1,11 @@
+type VersionCallback = (dependencyName: string) => string;
+
 /**
  * Update the *dependencies in the package.json
  * @param dependencies - Dependencies map
  * @param version - Version to bump to
  */
-export function bumpDependencies(dependencies: Record<string, string> = {}, version: string)
+export function bumpDependencies(dependencies: Record<string, string> = {}, version: string | VersionCallback)
 {
     Object.keys(dependencies)
         // Only consider bumping monorepo packages
@@ -11,6 +13,6 @@ export function bumpDependencies(dependencies: Record<string, string> = {}, vers
         .forEach((n) =>
         {
             // replace with exact version
-            dependencies[n] = version;
+            dependencies[n] = typeof version === 'function' ? version(n) : version;
         });
 }
--- scripts/codeSandboxCIRelease.ts
+++ scripts/codeSandboxCIRelease.ts
@@ -6,6 +6,9 @@ import { readJSON, writeJSON } from './utils/json';
 // copy version from package.json to sandbox CI bundles so npm pack command works
 async function main()
 {
+    // eslint-disable-next-line no-console
+    console.log('The GITHUB_SHA is: ', process.env.GITHUB_SHA);
+
     const { version } = await readJSON<{version: string}>(path.join(process.cwd(), 'package.json'));
     const { packages } = await readJSON<{packages: string[]}>(path.join(process.cwd(), '.codesandbox', 'ci.json'));
 
@@ -33,9 +36,13 @@ async function main()
             ...workspace.config,
         };
 
-        bumpDependencies(workspace.config.dependencies, version);
-        bumpDependencies(workspace.config.devDependencies, version);
-        bumpDependencies(workspace.config.peerDependencies, version);
+        // point dependency versions to their local codesandbox versions, weirdly they use a GH sha of length 8
+        const baseUrl = `https://pkg.csb.dev/pixijs/pixijs/commit/${process.env.GITHUB_SHA.substr(0, 8)}/`;
+        const getDependencyUrl = (packageName: string) => `${baseUrl}${packageName}`;
+
+        bumpDependencies(workspace.config.dependencies, getDependencyUrl);
+        bumpDependencies(workspace.config.devDependencies, getDependencyUrl);
+        bumpDependencies(workspace.config.peerDependencies, getDependencyUrl);
 
         await writeJSON(path.join(workspace.dir, 'package.json'), workspace.config);
     });
