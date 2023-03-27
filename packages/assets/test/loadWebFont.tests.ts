94e33704dc3290ad3e878b1b73228705753920c2
SuperSodaSea <bobby825@126.com>
2023-03-27 23:59:42 +0800

    Update tests

--- packages/assets/test/loadWebFont.tests.ts
+++ packages/assets/test/loadWebFont.tests.ts
@@ -4,16 +4,16 @@ describe('loadWebFont', () =>
 {
     it('should get font family name', () =>
     {
-        expect(getFontFamilyName('AbCdEf')).toBe('Abcdef');
-        expect(getFontFamilyName('ABC def-gHi_JkL')).toBe('Abc Def Ghi Jkl');
-        expect(getFontFamilyName('ABC123')).toBe('Abc123');
+        expect(getFontFamilyName('AbCdEf.ttf')).toBe('Abcdef');
+        expect(getFontFamilyName('ABC def-gHi_JkL.otf')).toBe('Abc Def Ghi Jkl');
+        expect(getFontFamilyName('ABC123.woff')).toBe('Abc123');
     });
 
     it('should escape font family name that is not valid CSS ident-token', () =>
     {
         expect(getFontFamilyName('')).toBe('""');
-        expect(getFontFamilyName('123456')).toBe('"123456"');
-        expect(getFontFamilyName('ABC 123')).toBe('"Abc 123"');
-        expect(getFontFamilyName('2nd-Font')).toBe('"2nd Font"');
+        expect(getFontFamilyName('123456.ttf')).toBe('"123456"');
+        expect(getFontFamilyName('ABC 123.otf')).toBe('"Abc 123"');
+        expect(getFontFamilyName('2nd-Font.woff')).toBe('"2nd Font"');
     });
 });
