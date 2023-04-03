0c340c51702fccf2516fe5bfe6df72cafb0a7b7d
dev7355608 <dev7355608@gmail.com>
2023-04-03 14:01:25 +0200

    Add extract pixels and canvas tests

--- packages/extract/test/Extract.tests.ts
+++ packages/extract/test/Extract.tests.ts
@@ -1,4 +1,4 @@
-import { Rectangle, Renderer, RenderTexture, Texture } from '@pixi/core';
+import { ALPHA_MODES, FORMATS, Rectangle, Renderer, RenderTexture, Texture, TYPES } from '@pixi/core';
 import { Extract } from '@pixi/extract';
 import { Sprite } from '@pixi/sprite';
 
@@ -11,6 +11,124 @@ describe('Extract', () =>
         expect(renderer.plugins.extract).toBeInstanceOf(Extract);
         expect(renderer.extract).toBeInstanceOf(Extract);
 
+        renderer.destroy();
+    });
+
+    it('should extract pixels from renderer correctly (without y-flipping)', async () =>
+    {
+        const renderer = new Renderer({ width: 2, height: 2 });
+        const texturePixels = new Uint8Array([
+            255, 0, 0, 255, 0, 255, 0, 153,
+            0, 0, 255, 102, 255, 255, 0, 51
+        ]);
+        const texture = Texture.fromBuffer(texturePixels, 2, 2, {
+            width: 2,
+            height: 2,
+            format: FORMATS.RGBA,
+            type: TYPES.UNSIGNED_BYTE,
+            alphaMode: ALPHA_MODES.UNPACK
+        });
+        const sprite = new Sprite(texture);
+        const extract = renderer.extract;
+
+        renderer.render(sprite);
+
+        const extractedPixels = extract.pixels();
+
+        expect(extractedPixels).toEqual(new Uint8Array([
+            0, 0, 102, 255, 51, 51, 0, 255,
+            255, 0, 0, 255, 0, 153, 0, 255
+        ]));
+
+        texture.destroy(true);
+        sprite.destroy();
+        renderer.destroy();
+    });
+
+    it('should extract canvas from renderer correctly (with y-flipping)', async () =>
+    {
+        const renderer = new Renderer({ width: 2, height: 2 });
+        const texturePixels = new Uint8Array([
+            255, 0, 0, 255, 0, 255, 0, 153,
+            0, 0, 255, 102, 255, 255, 0, 51
+        ]);
+        const texture = Texture.fromBuffer(texturePixels, 2, 2, {
+            width: 2,
+            height: 2,
+            format: FORMATS.RGBA,
+            type: TYPES.UNSIGNED_BYTE,
+            alphaMode: ALPHA_MODES.UNPACK
+        });
+        const sprite = new Sprite(texture);
+        const extract = renderer.extract;
+
+        renderer.render(sprite);
+
+        const canvas = extract.canvas();
+        const context = canvas.getContext('2d');
+        const imageData = context.getImageData(0, 0, 2, 2);
+
+        expect(imageData.data).toEqual(new Uint8ClampedArray([
+            255, 0, 0, 255, 0, 153, 0, 255,
+            0, 0, 102, 255, 51, 51, 0, 255
+        ]));
+
+        texture.destroy(true);
+        sprite.destroy();
+        renderer.destroy();
+    });
+
+    it('should extract pixels from render texture correctly', async () =>
+    {
+        const renderer = new Renderer({ width: 2, height: 2 });
+        const texturePixels = new Uint8Array([
+            255, 0, 0, 255, 0, 255, 0, 153,
+            0, 0, 255, 102, 255, 255, 0, 51
+        ]);
+        const texture = Texture.fromBuffer(texturePixels, 2, 2, {
+            width: 2,
+            height: 2,
+            format: FORMATS.RGBA,
+            type: TYPES.UNSIGNED_BYTE,
+            alphaMode: ALPHA_MODES.UNPACK
+        });
+        const sprite = new Sprite(texture);
+        const extract = renderer.extract;
+
+        const extractedPixels = extract.pixels(sprite);
+
+        expect(extractedPixels).toEqual(texturePixels);
+
+        texture.destroy(true);
+        sprite.destroy();
+        renderer.destroy();
+    });
+
+    it('should extract canvas from render texture correctly', async () =>
+    {
+        const renderer = new Renderer({ width: 2, height: 2 });
+        const texturePixels = new Uint8Array([
+            255, 0, 0, 255, 0, 255, 0, 153,
+            0, 0, 255, 102, 255, 255, 0, 51
+        ]);
+        const texture = Texture.fromBuffer(texturePixels, 2, 2, {
+            width: 2,
+            height: 2,
+            format: FORMATS.RGBA,
+            type: TYPES.UNSIGNED_BYTE,
+            alphaMode: ALPHA_MODES.UNPACK
+        });
+        const sprite = new Sprite(texture);
+        const extract = renderer.extract;
+
+        const canvas = extract.canvas(sprite);
+        const context = canvas.getContext('2d');
+        const imageData = context.getImageData(0, 0, 2, 2);
+
+        expect(imageData.data).toEqual(new Uint8ClampedArray(texturePixels.buffer));
+
+        texture.destroy(true);
+        sprite.destroy();
         renderer.destroy();
     });
 
