db45cff67ab9707e5f76aff26c7c4dcb47159f27
dev7355608 <dev7355608@gmail.com>
2023-04-19 17:56:37 +0200

    Use canPlayVideo instead

--- packages/assets/src/detections/parsers/detectWebm.ts
+++ packages/assets/src/detections/parsers/detectWebm.ts
@@ -8,13 +8,7 @@ export const detectWebm = {
         type: ExtensionType.DetectionParser,
         priority: 0,
     },
-    test: async (): Promise<boolean> =>
-    {
-        // eslint-disable-next-line max-len
-        const webmData = 'data:video/webm;base64,GkXfo59ChoEBQveBAULygQRC84EIQoKEd2VibUKHgQJChYECGFOAZwEAAAAAAAG9EU2bdLpNu4tTq4QVSalmU6yBoU27i1OrhBZUrmtTrIHGTbuMU6uEElTDZ1OsggEXTbuMU6uEHFO7a1OsggGn7AEAAAAAAABZAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAVSalmoCrXsYMPQkBNgIRMYXZmV0GETGF2ZkSJiEBEAAAAAAAAFlSua8yuAQAAAAAAAEPXgQFzxYgAAAAAAAAAAZyBACK1nIN1bmSIgQCGhVZfVlA5g4EBI+ODhAJiWgDglLCBAbqBAZqBAlWwiFWxgQBVuYECElTDZ9Vzc9JjwItjxYgAAAAAAAAAAWfInEWjh0VOQ09ERVJEh49MYXZjIGxpYnZweC12cDlnyKJFo4hEVVJBVElPTkSHlDAwOjAwOjAwLjA0MDAwMDAwMAAAH0O2dbHngQCjrIEAAICiSYNC4AAAAAYAOCQcGEoAACAgABG///+mlH/////TSj/////ppQAAHFO7a5G7j7OBALeK94EB8YIBcfCBAw==';
-
-        return testVideoFormat(webmData);
-    },
+    test: async (): Promise<boolean> => testVideoFormat('video/webm'),
     add: async (formats) => [...formats, 'webm'],
     remove: async (formats) => formats.filter((f) => f !== 'webm'),
 } as FormatDetectionParser;
--- packages/assets/src/detections/utils/testVideoFormat.ts
+++ packages/assets/src/detections/utils/testVideoFormat.ts
@@ -1,28 +1,14 @@
 const inWorker = 'WorkerGlobalScope' in globalThis
     && globalThis instanceof (globalThis as any).WorkerGlobalScope;
 
-export async function testVideoFormat(dataURL: string): Promise<boolean>
+export function testVideoFormat(mimeType: string): boolean
 {
-    if (!dataURL.startsWith('data:video/'))
-    {
-        throw new Error('Invalid video data URL');
-    }
-
     if (inWorker)
     {
         return false;
     }
 
-    return new Promise((resolve) =>
-    {
-        const video = document.createElement('video');
+    const video = document.createElement('video');
 
-        video.autoplay = false;
-        video.crossOrigin = 'anonymous';
-        video.preload = 'auto';
-        video.oncanplay = () => resolve(true);
-        video.onerror = () => resolve(false);
-        video.src = dataURL;
-        video.load();
-    });
+    return video.canPlayType(mimeType) !== '';
 }
