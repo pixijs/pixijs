71ed56c569ebc6b53da19e3c49258a0a84892101
Tianlan Zhou <bobby825@126.com>
2023-10-02 21:03:59 +0800

    Fix: detectAvif/Webp not working in Worker (#9703)

--- packages/assets/src/detections/parsers/detectWebp.ts
+++ packages/assets/src/detections/parsers/detectWebp.ts
@@ -1,4 +1,5 @@
 import { extensions, ExtensionType } from '@pixi/core';
+import { testImageFormat } from '../utils/testImageFormat';
 
 import type { FormatDetectionParser } from '..';
 
@@ -7,25 +8,9 @@ export const detectWebp = {
         type: ExtensionType.DetectionParser,
         priority: 0,
     },
-    test: async (): Promise<boolean> =>
-    {
-        const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
-
-        return new Promise((resolve) =>
-        {
-            const webp = new Image();
-
-            webp.onload = () =>
-            {
-                resolve(true);
-            };
-            webp.onerror = () =>
-            {
-                resolve(false);
-            };
-            webp.src = webpData;
-        });
-    },
+    test: async (): Promise<boolean> => testImageFormat(
+        'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA='
+    ),
     add: async (formats) => [...formats, 'webp'],
     remove: async (formats) => formats.filter((f) => f !== 'webp'),
 } as FormatDetectionParser;
