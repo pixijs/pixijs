749f06b1d696836a9767bf7e22dca6b7179b4ef2
Ali Chamas <ali.chamas@play.co>
2023-10-17 20:23:10 +1100

    Fix SVG path parsing logic to close paths properly, add visual test

--- src/scene/graphics/shared/svg/SVGToGraphicsPath.ts
+++ src/scene/graphics/shared/svg/SVGToGraphicsPath.ts
@@ -1,243 +1,201 @@
+/* eslint-disable no-console */
+import parse from 'parse-svg-path';
 import { warn } from '../../../../utils/logging/warn';
 
 import type { GraphicsPath } from '../path/GraphicsPath';
 
-const commandSizeMap = {
-    a: 7,
-    c: 6,
-    h: 1,
-    l: 2,
-    m: 2,
-    q: 4,
-    s: 4,
-    t: 2,
-    v: 1,
-    z: 0,
-};
+interface SubPath
+{
+    startX: number;
+    startY: number;
+}
 
 // TODO optimise and cache the paths?
 export function SVGToGraphicsPath(svgPath: string, path: GraphicsPath): GraphicsPath
 {
-    // get commands removing spaces at the end
+    const commands = parse(svgPath);
 
-    const commands = svgPath.match(/[a-df-z][^a-df-z]*/gi);
-    // get command data size
-
-    const data = svgPath.match(/[+-]?\d*\.?\d+(?:[eE][+-]?\d+)?/g)?.map(parseFloat);
-
-    const betterCommands: string[] = [];
-
-    commands.forEach((command) =>
-    {
-        const data = command.match(/[+-]?\d*\.?\d+(?:[eE][+-]?\d+)?/g)?.map(parseFloat);
-
-        const type = command[0];
-
-        let totalInstructions = 1;
-
-        if (data)
-        {
-            totalInstructions = data.length / commandSizeMap[type.toLowerCase() as keyof typeof commandSizeMap];
-        }
-
-        for (let i = 0; i < totalInstructions; i++)
-        {
-            betterCommands.push(type);
-        }
-    });
-
-    let dataIndex = 0;
+    const subpaths: SubPath[] = [];
+    let currentSubPath: SubPath | null = null;
 
     let lastX = 0;
     let lastY = 0;
 
-    for (let i = 0; i < betterCommands.length; i++)
+    for (let i = 0; i < commands.length; i++)
     {
-        const type = betterCommands[i];
-
-        // const command = commands[i];
-
-        //        const type = betterCommand.type;
+        const command = commands[i];
+        const [type, ...data] = command;
 
         switch (type)
         {
             case 'M':
-                lastX = data[dataIndex++];
-                lastY = data[dataIndex++];
+                lastX = data[0];
+                lastY = data[1];
 
                 path.moveTo(lastX, lastY);
                 break;
             case 'm':
 
-                lastX += data[dataIndex++];
-                lastY += data[dataIndex++];
+                lastX += data[0];
+                lastY += data[1];
 
                 path.moveTo(lastX, lastY);
                 break;
             case 'H':
-                lastX = data[dataIndex++];
+                lastX = data[0];
 
                 path.lineTo(lastX, lastY);
                 break;
             case 'h':
-                lastX += data[dataIndex++];
+                lastX += data[0];
 
                 path.lineTo(lastX, lastY);
                 break;
             case 'V':
-                lastY = data[dataIndex++];
+                lastY = data[0];
 
                 path.lineTo(lastX, lastY);
                 break;
             case 'v':
-                lastY += data[dataIndex++];
+                lastY += data[0];
 
                 path.lineTo(lastX, lastY);
                 break;
             case 'L':
-                lastX = data[dataIndex++];
-                lastY = data[dataIndex++];
+                lastX = data[0];
+                lastY = data[1];
 
                 path.lineTo(lastX, lastY);
                 break;
             case 'l':
-                lastX += data[dataIndex++];
-                lastY += data[dataIndex++];
+                lastX += data[0];
+                lastY += data[1];
 
                 path.lineTo(lastX, lastY);
                 break;
             case 'C':
 
-                lastX = data[dataIndex + 4];
-                lastY = data[dataIndex + 5];
+                lastX = data[4];
+                lastY = data[5];
 
                 path.bezierCurveTo(
-                    data[dataIndex], data[dataIndex + 1],
-                    data[dataIndex + 2], data[dataIndex + 3],
+                    data[0], data[1],
+                    data[2], data[3],
                     lastX, lastY
                 );
-
-                dataIndex += 6;
                 break;
             case 'c':
-
                 path.bezierCurveTo(
-                    lastX + data[dataIndex], lastY + data[dataIndex + 1],
-                    lastX + data[dataIndex + 2], lastY + data[dataIndex + 3],
-                    lastX + data[dataIndex + 4], lastY + data[dataIndex + 5]
+                    lastX + data[0], lastY + data[1],
+                    lastX + data[2], lastY + data[3],
+                    lastX + data[4], lastY + data[5]
                 );
 
-                lastX += data[dataIndex + 4];
-                lastY += data[dataIndex + 5];
-
-                dataIndex += 6;
+                lastX += data[4];
+                lastY += data[5];
                 break;
             case 'S':
-                lastX = data[dataIndex + 2];
-                lastY = data[dataIndex + 3];
+                lastX = data[2];
+                lastY = data[3];
 
                 path.bezierCurveToShort(
-                    data[dataIndex], data[dataIndex + 1],
+                    data[0], data[1],
                     lastX, lastY
                 );
-
-                dataIndex += 4;
                 break;
             case 's':
                 path.bezierCurveToShort(
-                    lastX + data[dataIndex], lastY + data[dataIndex + 1],
-                    lastX + data[dataIndex + 2], lastY + data[dataIndex + 3],
+                    lastX + data[0], lastY + data[1],
+                    lastX + data[2], lastY + data[3],
                 );
 
-                lastX += data[dataIndex + 2];
-                lastY += data[dataIndex + 3];
-
-                dataIndex += 4;
+                lastX += data[2];
+                lastY += data[3];
                 break;
             case 'Q':
-                lastX = data[dataIndex + 2];
-                lastY = data[dataIndex + 3];
+                lastX = data[2];
+                lastY = data[3];
 
                 path.quadraticCurveTo(
-                    data[dataIndex], data[dataIndex + 1],
+                    data[0], data[1],
                     lastX, lastY
                 );
-
-                dataIndex += 4;
                 break;
             case 'q':
-
                 path.quadraticCurveTo(
-                    lastX + data[dataIndex], lastY + data[dataIndex + 1],
-                    lastX + data[dataIndex + 2], lastY + data[dataIndex + 3]
+                    lastX + data[0], lastY + data[1],
+                    lastX + data[2], lastY + data[3]
                 );
 
-                lastX += data[dataIndex + 2];
-                lastY += data[dataIndex + 3];
-
-                dataIndex += 4;
+                lastX += data[2];
+                lastY += data[3];
                 break;
             case 'T':
-
-                lastX = data[dataIndex++];
-                lastY = data[dataIndex++];
+                lastX = data[0];
+                lastY = data[1];
 
                 path.quadraticCurveToShort(
                     lastX, lastY
                 );
-
                 break;
             case 't':
-
-                lastX += data[dataIndex++];
-                lastY += data[dataIndex++];
+                lastX += data[0];
+                lastY += data[1];
 
                 path.quadraticCurveToShort(
                     lastX, lastY
                 );
-
                 break;
             case 'A':
-
-                lastX = data[dataIndex + 5];
-                lastY = data[dataIndex + 6];
+                lastX = data[5];
+                lastY = data[6];
 
                 path.arcToSvg(
-                    data[dataIndex],
-                    data[dataIndex + 1],
-                    data[dataIndex + 2],
-                    data[dataIndex + 3],
-                    data[dataIndex + 4],
+                    data[0],
+                    data[1],
+                    data[2],
+                    data[3],
+                    data[4],
                     lastX, lastY
                 );
-
-                dataIndex += 7;
-
                 break;
             case 'a':
-                lastX += data[dataIndex + 5];
-                lastY += data[dataIndex + 6];
+                lastX += data[5];
+                lastY += data[6];
 
                 path.arcToSvg(
-                    data[dataIndex],
-                    data[dataIndex + 1],
-                    data[dataIndex + 2],
-                    data[dataIndex + 3],
-                    data[dataIndex + 4],
+                    data[0],
+                    data[1],
+                    data[2],
+                    data[3],
+                    data[4],
                     lastX, lastY
                 );
-
-                dataIndex += 7;
-
                 break;
             case 'Z':
             case 'z':
                 path.closePath();
+                if (subpaths.length > 0)
+                {
+                    currentSubPath = subpaths.pop();
+                    lastX = currentSubPath?.startX ?? 0;
+                    lastY = currentSubPath?.startY ?? 0;
+                }
+                currentSubPath = null;
                 break;
             default:
                 // #if _DEBUG
                 warn(`Unknown SVG path command: ${type}`);
                 // #endif
+        }
+
+        if (type !== 'Z' && type !== 'z')
+        {
+            if (currentSubPath === null)
+            {
+                currentSubPath = { startX: lastX, startY: lastY };
+                subpaths.push({ ...currentSubPath });
+            }
         }
     }
 
