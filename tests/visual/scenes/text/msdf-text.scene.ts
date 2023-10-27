4322676e501c2df49dd72d9d41785030821110c5
Mat Groves <mat.groves@play.co>
2023-10-27 15:56:49 +0100

    added SDF visual test added MSDF visual test slight tweak for how fragments are organised

---
+++ tests/visual/scenes/text/msdf-text.scene.ts
@@ -0,0 +1,26 @@
+import { Assets } from '../../../../src/assets/Assets';
+import { Text } from '../../../../src/scene/text/Text';
+
+import type { Container } from '../../../../src/scene/container/Container';
+import type { TestScene } from '../../types';
+
+export const scene: TestScene = {
+    it: 'should render msdf-text correctly',
+    only: true,
+    create: async (scene: Container) =>
+    {
+        await Assets.load('fonts/msdf/comicmsdf.fnt');
+
+        const text = new Text({
+            text: 'M',
+            style: {
+                fontFamily: 'comicmsdf',
+                fontSize: 150,
+                fill: 'white',
+            }
+        });
+
+        text.position.set(-3, -50);
+        scene.addChild(text);
+    },
+};
