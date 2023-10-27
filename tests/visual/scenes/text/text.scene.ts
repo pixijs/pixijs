737cfef6e872086aa44da277019922df12534aaa
Mat Groves <mat.groves@play.co>
2023-10-27 16:10:56 +0100

    fix text style update bug add visual test

---
+++ tests/visual/scenes/text/text.scene.ts
@@ -0,0 +1,28 @@
+import { Text } from '../../../../src/scene/text/Text';
+
+import type { Renderer } from '../../../../src/rendering/renderers/types';
+import type { Container } from '../../../../src/scene/container/Container';
+import type { TestScene } from '../../types';
+
+export const scene: TestScene = {
+    it: 'should render text correctly if style changes',
+    only: true,
+    create: async (scene: Container, renderer: Renderer) =>
+    {
+        const text = new Text({
+            text: 'I should be red and wrap wrap wrapping',
+            style: {
+                fontSize: 20,
+                fill: 'white',
+            }
+        });
+
+        scene.addChild(text);
+
+        renderer.render(scene);
+
+        text.style.wordWrap = true;
+        text.style.fill = 'red';
+        text.style.wordWrapWidth = 100;
+    },
+};
