1dc9ad8f27b09516e45305c4fc54d8fbca243634
Mat Groves <mat.groves@play.co>
2023-10-30 15:13:51 +0000

    docs for geometry

--- src/rendering/renderers/shared/geometry/utils/buildUvs.ts
+++ src/rendering/renderers/shared/geometry/utils/buildUvs.ts
@@ -1,5 +1,18 @@
 import type { Matrix } from '../../../../../maths/matrix/Matrix';
 
+/**
+ * Takes a vertices array and a matrix and transforms the vertices based on the matrix.
+ * this out put is written to the uvs array
+ * @param vertices - the vertices to calculate uvs from
+ * @param verticesStride - the stride of the vertice
+ * @param verticesOffset - the offset of the vertices
+ * @param uvs - the uvs to fill
+ * @param uvsOffset - the offset of the uvs
+ * @param uvsStride - the stride of the uvs
+ * @param size - the size of the vertices
+ * @param matrix - the matrix to apply to the uvs
+ * @memberof rendering
+ */
 export function buildUvs(
     vertices: number[],
     verticesStride: number,
--- src/rendering/renderers/shared/geometry/utils/transformVertices.ts
+++ src/rendering/renderers/shared/geometry/utils/transformVertices.ts
@@ -1,5 +1,14 @@
 import type { Matrix } from '../../../../../maths/matrix/Matrix';
 
+/**
+ * Transforms the vertices in an array with the given matrix.
+ * @param vertices - the vertices to transform
+ * @param m - the matrix to apply to the vertices
+ * @param offset - the offset of the vertices (defaults to 0)
+ * @param stride - the stride of the vertices (defaults to 2)
+ * @param size - the size of the vertices (defaults to vertices.length / stride - offset)
+ * @memberof rendering
+ */
 export function transformVertices(vertices: number[], m: Matrix, offset?: number, stride?: number, size?: number)
 {
     const a = m.a;
