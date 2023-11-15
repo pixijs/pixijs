c91be15e7a94498b089b1c6b46000c2c4d3b0c00
Zyie <24736175+Zyie@users.noreply.github.com>
2023-11-15 12:08:31 +0000

    Apply suggestions from code review

--- tests/visual/scenes/graphics/holes.scene.ts
+++ tests/visual/scenes/graphics/holes.scene.ts
@@ -5,7 +5,6 @@ import type { TestScene } from '../../types';
 
 export const scene: TestScene = {
     it: 'should cut holes',
-    only: true,
     create: async (scene: Container) =>
     {
         const rect = new Graphics()
