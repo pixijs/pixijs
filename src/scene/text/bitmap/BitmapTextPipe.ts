98ebfb1c071168dd11d03867be46af7aa964cc52
Mat Groves <mat.groves@play.co>
2023-11-17 12:32:46 +0000

    fix up the text update mid render

--- src/scene/text/bitmap/BitmapTextPipe.ts
+++ src/scene/text/bitmap/BitmapTextPipe.ts
@@ -181,8 +181,6 @@ export class BitmapTextPipe implements RenderPipe<TextView>
 
     private _initGpuText(renderable: Renderable<TextView>)
     {
-        renderable.view._style.update();
-
         // TODO we could keep a bunch of contexts around and reuse one that hav the same style!
         const proxyRenderable = BigPool.get(GraphicsProxyRenderable, renderable);
 
