44e86ee19690fb307ab7a5f24ed2e6005c5c2aae
Zyie <24736175+Zyie@users.noreply.github.com>
2023-11-21 09:13:38 +0000

    Update tests/visual/scenes/scene/layer-tint.scene.ts
    Co-authored-by: Ali Chamas <91103690+achamas-playco@users.noreply.github.com>

--- tests/visual/scenes/scene/layer-tint.scene.ts
+++ tests/visual/scenes/scene/layer-tint.scene.ts
@@ -6,7 +6,6 @@ import type { TestScene } from '../../types';
 
 export const scene: TestScene = {
     it: 'should tint layers correctly',
-    only: true,
     create: async (scene: Container) =>
     {
         // layer green container..
