14f6e991ed1d6cfacc4aec0f3eacdd27526a1ce8
Ali Chamas <91103690+achamas-playco@users.noreply.github.com>
2023-11-21 16:42:55 +1100

    Update src/maths/point/Point.ts
    Co-authored-by: Zyie <24736175+Zyie@users.noreply.github.com>

--- src/maths/point/Point.ts
+++ src/maths/point/Point.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-// import type { Dict } from '../../utils/types';
 import type { PointData } from './PointData';
 import type { PointLike } from './PointLike';
 
