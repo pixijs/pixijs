1cb530c8d507bd5076908cf0e1def9792857723e
Mat Groves <mat.groves@play.co>
2023-11-22 16:18:40 +0000

    Refactor layer-tint.scene.ts

--- tests/visual/scenes/scene/layer-tint.scene.ts
+++ tests/visual/scenes/scene/layer-tint.scene.ts
@@ -9,31 +9,35 @@ export const scene: TestScene = {
     only: true,
     create: async (scene: Container) =>
     {
-        // layer green container..
-        const squareContext = new GraphicsContext()
-            .rect(0, 0, 20, 20)
-            .fill('white');
-
-        const greenContainer = new Container({
-            layer: true,
-        });
-
-        greenContainer.addChild(new Graphics(squareContext));
-
-        greenContainer.tint = 'green';
+       
 
         // non layer red container.
         const redContainer = new Container({
             layer: false,
         });
 
-        scene.addChild(greenContainer);
-
-        redContainer.addChild(new Graphics(squareContext));
-    //    redContainer.x = 10;
+     
+        redContainer.x = 30;
         redContainer.tint = 'red';
 
         scene.addChild(redContainer);
+
+         // layer green container..
+         const squareContext = new GraphicsContext()
+         .rect(0, 0, 20, 20)
+         .fill('white');
+
+     const greenContainer = new Container({
+         layer: true,
+     });
+
+     greenContainer.addChild(new Graphics(squareContext));
+
+     greenContainer.tint = 'green';
+
+     scene.addChild(greenContainer);
+
+     redContainer.addChild(new Graphics(squareContext));
 
         const nestedLayer = new Container({
             layer: true,
