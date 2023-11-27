af5814b968d5b83442750f3be7df1114359cc40a
Zyie <24736175+Zyie@users.noreply.github.com>
2023-11-27 14:57:05 +0000

    Update tests/renderering/graphics/Graphics.Bounds.test.ts

--- tests/renderering/graphics/Graphics.Bounds.test.ts
+++ tests/renderering/graphics/Graphics.Bounds.test.ts
@@ -212,8 +212,6 @@ describe('Graphics Bounds', () =>
             expect(graphics.context.containsPoint(new Point(6, 6))).toBe(true);
         });
 
-        // note: Mat to look into, may be bug
-        // ticket: https://github.com/orgs/pixijs/projects/2/views/4?pane=issue&itemId=44799288
         it('should take a matrix into account', () =>
         {
             const g = new Graphics();
