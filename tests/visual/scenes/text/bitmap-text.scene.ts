2770d6adcfaacd3eaf6c539b00987e616289b46b
Ali Chamas <ali.chamas@play.co>
2023-11-29 20:10:07 +1100

    add more visual tests

--- tests/visual/scenes/text/bitmap-text.scene.ts
+++ tests/visual/scenes/text/bitmap-text.scene.ts
@@ -1,4 +1,5 @@
 import { Assets } from '../../../../src/assets/Assets';
+import { AlphaFilter } from '../../../../src/filters/defaults/alpha/AlphaFilter';
 import { Text } from '../../../../src/scene/text/Text';
 
 import type { Container } from '../../../../src/scene/container/Container';
@@ -6,31 +7,38 @@ import type { TestScene } from '../../types';
 
 export const scene: TestScene = {
     it: 'should render both canvas and bitmap text of the same style',
-    pixelMatch: 150,
     create: async (scene: Container) =>
     {
         await Assets.load('fonts/outfit.woff2');
         const textCanvas = new Text({
-            text: 'Im canvas',
+            text: 'Im canvas 🔥',
             style: {
                 fontFamily: 'Outfit',
-                fontSize: 20,
+                fontSize: 12,
                 fill: 'white',
-            }
+                letterSpacing: 5,
+                lineHeight: 20,
+            },
+            renderMode: 'canvas',
         });
 
         const textBitmap = new Text({
-            text: 'Im dynamic \nbitmap',
+            text: 'Im dynamic \nbitmap 🔥',
             style: {
                 fontFamily: 'Outfit',
-                fontSize: 20,
+                fontSize: 16,
                 fill: 'white',
+                letterSpacing: 5,
+                // lineHeight: 20, // todo: https://github.com/orgs/pixijs/projects/2/views/4?pane=issue&itemId=46297431
             },
             y: 30,
+            renderMode: 'bitmap',
         });
 
-        scene.addChild(textCanvas);
+        textBitmap.tint = 0xff0000;
+        textBitmap.filters = [new AlphaFilter({ alpha: 0.5 })];
 
+        scene.addChild(textCanvas);
         scene.addChild(textBitmap);
     },
 };
