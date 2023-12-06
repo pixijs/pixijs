af7673d2f606b13fba16c2b766e84a58a3efce3f
Zyie <24736175+Zyie@users.noreply.github.com>
2023-12-06 10:22:23 +0000

    allow spritesheet scale to be a number

--- src/spritesheet/Spritesheet.ts
+++ src/spritesheet/Spritesheet.ts
@@ -67,7 +67,7 @@ export interface SpritesheetData
             name: string;
             opacity: number;
         }[];
-        scale: string;
+        scale: number | string;
         size?: {
             h: number;
             w: number;
@@ -265,7 +265,7 @@ export class Spritesheet<S extends SpritesheetData = SpritesheetData>
         this.animations = {} as Record<keyof NonNullable<S['animations']>, Texture[]>;
         this.data = data;
 
-        const metaResolution = parseFloat(data.meta.scale);
+        const metaResolution = typeof data.meta.scale === 'number' ? data.meta.scale : parseFloat(data.meta.scale);
 
         if (metaResolution)
         {
