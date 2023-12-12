a0eceb256e25b2b6bbdf79bc52e406453031eb89
Mat Groves <mat.groves@play.co>
2023-12-12 14:57:03 +0000

    add some docs

--- src/scene/container/bounds/getFastGlobalBounds.ts
+++ src/scene/container/bounds/getFastGlobalBounds.ts
@@ -4,6 +4,15 @@ import type { Container } from '../Container';
 
 // TODO could we cache local bounds on the render groups?
 
+/**
+ * Does exactly the same as getGlobalBounds, but does instead makes use of transforming AABBs
+ * of the various children within the scene graph. This is much faster, but less accurate.
+ *
+ * the result will never be smaller - only ever slightly larger (in most cases, it will be the same).
+ * @param target - The target container to get the bounds from
+ * @param bounds - The output bounds object.
+ * @returns The bounds.
+ */
 export function getFastGlobalBounds(target: Container, bounds: Bounds): Bounds
 {
     bounds.clear();
