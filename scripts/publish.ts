ecf1029a98c76c25dc97c3a12532a6ea751dfb33
Zyie <24736175+Zyie@users.noreply.github.com>
2024-01-04 09:07:45 +0000

    update script

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -5,39 +5,33 @@ import { spawn } from './utils/spawn';
 /** This script publishes the package as `pixi.js` and `pixijs` */
 async function main(): Promise<void>
 {
+    let originalPackage: {name: string};
+    // Save the paths to package.json
+    const packagePath = path.join(process.cwd(), 'package.json');
+
     try
     {
-        // Save the paths to package.json and package-lock.json
-        const packagePath = path.join(process.cwd(), 'package.json');
-        const packageLockPath = path.join(process.cwd(), 'package-lock.json');
-
-        // Read the original package.json and package-lock.json
-        const originalPackage = await readJSON<{name: string}>(packagePath);
-        const originalPackageLock = await readJSON<{name: string}>(packageLockPath);
+        // Read the original package.json
+        originalPackage = await readJSON<{name: string}>(packagePath);
 
         // Run npm publish
         await spawn('npm', ['publish']);
 
-        // Update the name in package.json and package-lock.json
-        const updatedPackage = { ...originalPackage, name: 'pixijs' };
-        // Update all instances of the package name in package-lock.json
-        const updatedPackageLock = JSON.parse(JSON.stringify(originalPackageLock)
-            .replace(new RegExp(originalPackage.name, 'g'), 'pixijs'));
+        // Update the name in package.json
+        const updatedPackage = { ...originalPackage, name: 'pixi.js' };
 
         await writeJSON(packagePath, updatedPackage);
-        await writeJSON(packageLockPath, updatedPackageLock);
 
         // Run npm publish again
         await spawn('npm', ['publish']);
-
-        // Restore the original package.json and package-lock.json
-        await writeJSON(packagePath, originalPackage);
-        await writeJSON(packageLockPath, originalPackageLock);
     }
-    catch (err)
+    finally
     {
-        console.error((err as Error).message);
-        process.exit(1);
+        // Restore the original package.json if it exists
+        if (originalPackage)
+        {
+            await writeJSON(packagePath, originalPackage);
+        }
     }
 }
 
