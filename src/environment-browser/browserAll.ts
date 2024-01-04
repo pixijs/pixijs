50f0c7014481dbc13383948c4362b027543d196c
Zyie <24736175+Zyie@users.noreply.github.com>
2024-01-04 08:54:23 +0000

    add nine slice by default

--- src/environment-browser/browserAll.ts
+++ src/environment-browser/browserAll.ts
@@ -9,4 +9,5 @@ import '../scene/text/init';
 import '../scene/text-bitmap/init';
 import '../scene/text-html/init';
 import '../scene/sprite-tiling/init';
+import '../scene/sprite-nine-slice/init';
 import '../filters/init';
