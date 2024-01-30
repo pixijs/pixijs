c1f3ba57db491250803c78e4b9b303631e121961
Zyie <24736175+Zyie@users.noreply.github.com>
2024-01-30 11:30:42 +0000

    Fix: types not building correctly

--- scripts/types/fixTypes.ts
+++ scripts/types/fixTypes.ts
@@ -1,32 +1,91 @@
 /**
- * -------------------------------------------------------
- * Get every d.ts file in the src folder and add a
- * reference path to it in the lib/index.d.ts file
- *
- * This allows us to define our PixiMixins in the src folder
- *
- * e.g. /// <reference path="./app/ApplicationMixins.d.ts" />
- * -------------------------------------------------------
+ * -----------------------------------------------------------------
+ * The file is used to fix various issues with the types
+ * - Add reference paths to the mixins
+ * - Replace the dist triple slash reference with the webgpu types
+ * - Copy over the Shaders.d.ts file to the lib folder for
+ *   proper frag,vert,wgsl support
+ * -----------------------------------------------------------------
  */
 
 import fs from 'fs';
 import glob from 'glob';
 import path from 'path';
 
-const globPath = `${path.join(process.cwd(), './src')}/**/*.d.ts`;
-const files = glob.sync(globPath);
-
-const lines: string[] = [];
-
-files.forEach((file) =>
+/**
+ * Get every d.ts file in the src folder and add a
+ * reference path to it in the lib/index.d.ts file
+ *
+ * This allows us to define our PixiMixins in the src folder
+ * e.g. /// <reference path="./app/ApplicationMixins.d.ts" />
+ */
+function addMixinReferencePaths()
 {
-    const name = file.split(`${process.cwd()}/src`)[1];
+    const globPath = `${path.join(process.cwd(), './src')}/**/*.d.ts`;
+    const files = glob.sync(globPath);
 
-    lines.push(`/// <reference path=".${name}" />`);
-});
+    const lines: string[] = [];
 
-const filePath = path.join(process.cwd(), './lib/index.d.ts');
-const contents = fs.readFileSync(filePath, 'utf8');
-const updatedContents = `${lines.join('\n')}\n${contents}`;
+    files.forEach((file) =>
+    {
+        const name = file.split(`${process.cwd()}/src`)[1];
 
-fs.writeFileSync(filePath, updatedContents);
+        lines.push(`/// <reference path=".${name}" />`);
+    });
+
+    const filePath = path.join(process.cwd(), './lib/index.d.ts');
+    const contents = fs.readFileSync(filePath, 'utf8');
+    const updatedContents = `${lines.join('\n')}\n${contents}`;
+
+    fs.writeFileSync(filePath, updatedContents);
+}
+
+/**
+ * For whatever reason when we build the types we get /// <reference types="dist" />
+ * This is not what we want, we want /// <reference types="@webgpu/types" />
+ * So we replace it here
+ */
+function replaceWebgpuTypes()
+{
+    const globPath = `${path.join(process.cwd(), './lib')}/**/*.d.ts`;
+    const files = glob.sync(globPath);
+
+    files.forEach((file) =>
+    {
+        const contents = fs.readFileSync(file, 'utf8');
+
+        if (contents.includes('/// <reference types="dist" />'))
+        {
+            const updatedContents = contents.replace(
+                '/// <reference types="dist" />',
+                '/// <reference types="@webgpu/types" />'
+            );
+
+            fs.writeFileSync(file, updatedContents);
+        }
+    });
+}
+
+/** Copy the Shaders.d.ts file to the lib folder This is needed for proper frag,vert,wgsl support in the types */
+function copyShaders()
+{
+    const src = path.join(process.cwd(), './types');
+    const lib = path.join(process.cwd(), './lib');
+
+    fs.copyFileSync(`${src}/Shaders.d.ts`, `${lib}/Shaders.d.ts`);
+
+    // add this to the index.d.ts file
+    const filePath = path.join(process.cwd(), './lib/index.d.ts');
+    const contents = fs.readFileSync(filePath, 'utf8');
+
+    if (!contents.includes('/// <reference path="./Shaders.d.ts" />'))
+    {
+        const updatedContents = `/// <reference path="./Shaders.d.ts" />\n${contents}`;
+
+        fs.writeFileSync(filePath, updatedContents);
+    }
+}
+
+replaceWebgpuTypes();
+addMixinReferencePaths();
+copyShaders();
