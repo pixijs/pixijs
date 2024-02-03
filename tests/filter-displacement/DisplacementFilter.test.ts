5b7ede6e72e843b551813dee69edd01a91990fd2
Marvin Schürz <m.schuerz@hautzy.com>
2024-02-03 17:29:25 +0100

    Add test case to make sure `DisplacementFIlter` constructs correctly

---
+++ tests/filter-displacement/DisplacementFilter.test.ts
@@ -0,0 +1,20 @@
+import { DisplacementFilter } from '../../src/filters/defaults/displacement/DisplacementFilter';
+import { Sprite } from '../../src/scene/sprite/Sprite';
+import { Texture } from '../../src/rendering/renderers/shared/texture/Texture';
+
+describe('BlurFilter', () =>
+{   
+    it('should construct filter', () =>
+    {
+        const sprite = new Sprite(Texture.WHITE)
+        const filter = new DisplacementFilter({
+            sprite,
+            scale: { x: 8, y: 10 },
+        });
+
+        expect(filter).toBeInstanceOf(DisplacementFilter);
+        expect(filter.scale.x).toEqual(8);
+        expect(filter.scale.y).toEqual(10);
+        filter.destroy();
+    });
+})
