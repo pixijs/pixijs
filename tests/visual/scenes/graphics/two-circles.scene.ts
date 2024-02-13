b14e3641c541753487c33744a70553f7f0753cd2
Mat Groves <mat.groves@play.co>
2024-02-13 21:06:09 +0000

    remove only from test

--- tests/visual/scenes/graphics/two-circles.scene.ts
+++ tests/visual/scenes/graphics/two-circles.scene.ts
@@ -7,7 +7,6 @@ import type { TestScene } from '../../types';
 export const scene: TestScene = {
     it: 'should correctly swap bind groups and render to non batched circles with a sprite in the middle',
     pixelMatch: 200,
-    only: true,
     create: async (scene: Container) =>
     {
         const createGraphics = () =>
