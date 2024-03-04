f5693ac9f00a4012ecbf8523e35469189a89fc1e
Zyie <24736175+Zyie@users.noreply.github.com>
2024-03-04 20:34:46 +0000

    Chore: update class docs for the letter C (and blend modes)

--- src/advanced-blend-modes/LinearBurnBlend.ts
+++ src/advanced-blend-modes/LinearBurnBlend.ts
@@ -6,9 +6,17 @@ import { BlendModeFilter } from '../filters/blend-modes/BlendModeFilter';
 import type { ExtensionMetadata } from '../extensions/Extensions';
 
 /**
- Looks at the color information in each channel and darkens the base color to
- reflect the blend color by increasing the contrast between the two.
- @memberof filters
+ * Looks at the color information in each channel and darkens the base color to
+ * reflect the blend color by increasing the contrast between the two.
+ *
+ * Available as `container.blendMode = 'linear-burn'` after importing `pixi.js/advanced-blend-modes`.
+ * @example
+ * import 'pixi.js/advanced-blend-modes';
+ * import { Sprite } from 'pixi.js';
+ *
+ * const sprite = Sprite.from('something.png');
+ * sprite.blendMode = 'linear-burn'
+ * @memberof filters
  */
 export class LinearBurnBlend extends BlendModeFilter
 {
