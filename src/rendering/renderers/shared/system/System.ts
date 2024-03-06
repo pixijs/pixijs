9a145ef6a9eb7139e55aca8e689702aeb1fd8e8a
Zyie <24736175+Zyie@users.noreply.github.com>
2024-03-06 14:44:35 +0000

    Fix: type errors

--- src/rendering/renderers/shared/system/System.ts
+++ src/rendering/renderers/shared/system/System.ts
@@ -3,7 +3,7 @@ import type { RendererDestroyOptions } from './AbstractRenderer';
 
 export interface System<INIT_OPTIONS = null, DESTROY_OPTIONS = RendererDestroyOptions>
 {
-    init?: (options?: INIT_OPTIONS) => void;
+    init?: (options: INIT_OPTIONS) => void;
     /** Generic destroy methods to be overridden by the subclass */
     destroy?: (options?: DESTROY_OPTIONS) => void;
 }
