6e9670b5b98883a5eaf487530ca7ee2f18932a1d
Mat Groves <mat.groves@play.co>
2024-03-12 18:17:54 +0000

    Remove "only" flag from tiling sprite scene

--- tests/visual/scenes/sprite/tiling-sprite.scene.ts
+++ tests/visual/scenes/sprite/tiling-sprite.scene.ts
@@ -7,7 +7,6 @@ import type { TestScene } from '../../types';
 
 export const scene: TestScene = {
     it: 'should render tiling sprite',
-    only: true,
     create: async (scene: Container) =>
     {
         const texture = await Assets.load(`${basePath}textures/bunny.png`);
