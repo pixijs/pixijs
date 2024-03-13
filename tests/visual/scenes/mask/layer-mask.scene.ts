eb71582623878392f60d44eb3c20224e1cb6068e
Mat Groves <mat.groves@play.co>
2024-03-13 16:15:07 +0000

    add visual test

---
+++ tests/visual/scenes/mask/layer-mask.scene.ts
@@ -0,0 +1,46 @@
+/* eslint-disable max-len */
+import { Assets } from '../../../../src/assets/Assets';
+import { Container } from '../../../../src/scene/container/Container';
+import { Graphics } from '../../../../src/scene/graphics/shared/Graphics';
+import { Sprite } from '../../../../src/scene/sprite/Sprite';
+
+import type { TestScene } from '../../types';
+
+export const scene: TestScene = {
+    it: 'should render masks if they have multiple layers correctly',
+    only: true,
+    create: async (scene: Container) =>
+    {
+        const wrapper = new Container();
+
+        const width = 128 - 20;
+        const height = 128 - 20;
+        const border = 4;
+        const radius = 4;
+
+        const bg = new Graphics()
+            .roundRect(0, 0, width, height, radius + border)
+            .fill(0xffffff)
+            .roundRect(border, border, width - (border * 2), height - (border * 2), radius)
+            .fill(0x0000ff);
+
+        wrapper.x = 10;
+        wrapper.y = 10;
+
+        const mask = new Graphics()
+            .roundRect(0, 0, width, height, radius + border)
+            .fill(0xff0000)
+            .roundRect(border, border, width - (border * 2), height - (border * 2), radius)
+            .fill(0xffffff);
+
+        const thingOutSideofMask = new Graphics()
+            .rect(0, 0, 1000, 20)
+            .fill(0x00ff00);
+
+        //   mask.y =100;
+        wrapper.addChild(mask, bg, thingOutSideofMask);
+        wrapper.mask = mask;
+
+        scene.addChild(wrapper);
+    },
+};
