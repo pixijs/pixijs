09b6d93dabe55401034042f39319ef4e0311f9e5
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2024-03-18 17:29:26 +0300

    Test for mixColors

---
+++ tests/scene/utils/mixColors.tests.ts
@@ -0,0 +1,32 @@
+import {mixColors, mixStandardAnd32BitColors} from '../../../src/scene/container/utils/mixColors';
+
+describe('mixColors', () =>
+{
+    it('should take other color if one of BGR colors is WHITE', async () =>
+    {
+        const result = mixColors(0xFFFFFF, 0x123456);
+
+        expect(result).toBe(0x123456);
+    });
+
+    it('should take average per-component if one of BGR colors is not WHITE', async () =>
+    {
+        const result = mixColors(0xFEEDDC, 0x123456);
+
+        expect(result).toBe(0x889099);
+    });
+
+    it('should take other color if one of RGB+A, BGRA colors is WHITE', async () =>
+    {
+        const result = mixStandardAnd32BitColors(0xFFFFFF, 0.5, 0x40123456);
+
+        expect(result).toBe(0x20123456);
+    });
+
+    it('should take average per-component if one of RGB+A, BGRA colors is not WHITE', async () =>
+    {
+        const result = mixStandardAnd32BitColors(0xFEEDDC, 0.5, 0x40123456);
+
+        expect(result).toBe(0x207790AA);
+    });
+});
