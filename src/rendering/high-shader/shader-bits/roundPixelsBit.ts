abd7b6956c626f8e4d02ee880866b8d870e54b93
Mat Groves <mat.groves@play.co>
2024-04-02 16:49:50 +0100

    use floor + 0.5 approach

--- src/rendering/high-shader/shader-bits/roundPixelsBit.ts
+++ src/rendering/high-shader/shader-bits/roundPixelsBit.ts
@@ -4,7 +4,7 @@ export const roundPixelsBit = {
         header: /* wgsl */`
             fn roundPixels(position: vec2<f32>, targetSize: vec2<f32>) -> vec2<f32> 
             {
-                return (round((position * 0.5 + 0.5) * targetSize) / targetSize) * 2.0 - 1.0;
+                return ((floor((position * 0.5 + 0.5) * targetSize) / targetSize)+0.5) * 2.0 - 1.0;
             }
         `
     }
@@ -14,18 +14,9 @@ export const roundPixelsBitGl = {
     name: 'round-pixels-bit',
     vertex: {
         header: /* glsl */`   
-            float round(float value) {
-                return value < 0.0 ? ceil(value - 0.5) : floor(value + 0.5);
-            }
-      
             vec2 roundPixels(vec2 position, vec2 targetSize)
-            {      
-                // round..
-                vec2 pixelPosition = (position * 0.5 + 0.5) * targetSize;
-
-                vec2 roundedPosition = vec2(round(pixelPosition.x), round(pixelPosition.y));
-
-                return (roundedPosition / targetSize) * 2.0 - 1.0;
+            {       
+                return ((floor((position * 0.5 + 0.5) * targetSize) + 0.5) / targetSize) * 2.0 - 1.0;
             }
         `
     }
