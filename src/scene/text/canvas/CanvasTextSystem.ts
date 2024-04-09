c8cd64dab373dfa98fdc74e5e7f87a9d1b5ea384
Mat Groves <mat.groves@play.co>
2024-04-09 21:07:31 +0100

    Make the user facing `getTexture` a nicer API and consistent with HTMLText

--- src/scene/text/canvas/CanvasTextSystem.ts
+++ src/scene/text/canvas/CanvasTextSystem.ts
@@ -4,6 +4,7 @@ import { nextPow2 } from '../../../maths/misc/pow2';
 import { CanvasPool } from '../../../rendering/renderers/shared/texture/CanvasPool';
 import { TexturePool } from '../../../rendering/renderers/shared/texture/TexturePool';
 import { getCanvasBoundingBox } from '../../../utils/canvas/getCanvasBoundingBox';
+import { TextStyle } from '../TextStyle';
 import { getPo2TextureFromSource } from '../utils/getPo2TextureFromSource';
 import { CanvasTextMetrics } from './CanvasTextMetrics';
 import { fontStringFromTextStyle } from './utils/fontStringFromTextStyle';
@@ -11,9 +12,11 @@ import { getCanvasFillStyle } from './utils/getCanvasFillStyle';
 
 import type { ICanvas } from '../../../environment/canvas/ICanvas';
 import type { ICanvasRenderingContext2D } from '../../../environment/canvas/ICanvasRenderingContext2D';
+import type { Renderer } from '../../../rendering';
 import type { System } from '../../../rendering/renderers/shared/system/System';
 import type { Texture } from '../../../rendering/renderers/shared/texture/Texture';
-import type { TextStyle } from '../TextStyle';
+import type { TextOptions } from '../AbstractText';
+import type { Text } from '../Text';
 
 interface CanvasAndContext
 {
@@ -43,6 +46,13 @@ export class CanvasTextSystem implements System
         usageCount: number,
     }> = {};
 
+    private readonly _renderer: Renderer;
+
+    constructor(_renderer: Renderer)
+    {
+        this._renderer = _renderer;
+    }
+
     public getTextureSize(text: string, resolution: number, style: TextStyle): { width: number, height: number }
     {
         const measured = CanvasTextMetrics.measureText(text || ' ', style);
@@ -58,14 +68,38 @@ export class CanvasTextSystem implements System
         return { width, height };
     }
 
-    public getTexture(text: string, resolution: number, style: TextStyle, textKey: string)
+    /**
+     * This is a function that will create a texture from a text string, style and resolution.
+     * Useful if you want to make a texture of your text and use if for various other pixi things!
+     * @param options - The options of the text that will be used to generate the texture.
+     * @param options.text - the text to render
+     * @param options.style - the style of the text
+     * @param options.resolution - the resolution of the texture
+     * @returns the newly created texture
+     */
+    public getTexture(options: TextOptions): Texture
     {
-        if (this._activeTextures[textKey])
+        if (!(options.style instanceof TextStyle))
         {
-            this._increaseReferenceCount(textKey);
-
-            return this._activeTextures[textKey].texture;
+            options.style = new TextStyle(options.style);
         }
+
+        const { texture, canvasAndContext } = this.createTextureAndCanvas(
+            options as {text: string, style: TextStyle, resolution?: number}
+        );
+
+        this._renderer.texture.initSource(texture._source);
+
+        CanvasPool.returnCanvasAndContext(canvasAndContext);
+
+        return texture;
+    }
+
+    public createTextureAndCanvas(options: {text: string, style: TextStyle, resolution?: number})
+    {
+        const { text, style } = options;
+
+        const resolution = options.resolution ?? this._renderer.resolution;
 
         // create a canvas with the word hello on it
         const measured = CanvasTextMetrics.measureText(text || ' ', style);
@@ -90,6 +124,22 @@ export class CanvasTextSystem implements System
 
             texture.updateUvs();
         }
+
+        return { texture, canvasAndContext };
+    }
+
+    public getManagedTexture(text: Text)
+    {
+        const textKey = text._getKey();
+
+        if (this._activeTextures[textKey])
+        {
+            this._increaseReferenceCount(textKey);
+
+            return this._activeTextures[textKey].texture;
+        }
+
+        const { texture, canvasAndContext } = this.createTextureAndCanvas(text);
 
         this._activeTextures[textKey] = {
             canvasAndContext,
