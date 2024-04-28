78e5aa5e55da2546247d5b02dfd7437613e23f45
Zyie <24736175+Zyie@users.noreply.github.com>
2024-04-29 00:24:26 +0100

    Update tests/visual/scenes/graphics/graphics-tint.scene.ts

--- tests/visual/scenes/graphics/graphics-tint.scene.ts
+++ tests/visual/scenes/graphics/graphics-tint.scene.ts
@@ -5,7 +5,6 @@ import type { TestScene } from '../../types';
 
 export const scene: TestScene = {
     it: 'should set tint of graphics correctly',
-    only: true,
     create: async (scene: Container) =>
     {
         const rect = new Graphics().rect(0, 0, 100, 100).fill('black');
