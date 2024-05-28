0462664bde4557112c8b1efb69fe132d6977c44b
Zyie <24736175+Zyie@users.noreply.github.com>
2024-05-28 12:36:33 +0100

    update pixel match

--- tests/visual/scenes/text/text-pattern-gradient.scene.ts
+++ tests/visual/scenes/text/text-pattern-gradient.scene.ts
@@ -9,7 +9,7 @@ import type { TestScene } from '../../types';
 
 export const scene: TestScene = {
     it: 'should render FillPattern and FillGradient in text stroke',
-    pixelMatch: 200,
+    pixelMatch: 256,
     create: async (scene: Container) =>
     {
         const gradient = new FillGradient(0, 0, 110, 0)
