c87d9d3804e709c47a64d3dc53e8c79af624fe39
Zyie <24736175+Zyie@users.noreply.github.com>
2024-06-17 14:22:27 +0100

    Update src/utils/global/globalHooks.ts
    Co-authored-by: Matt Karl <matt@mattkarl.com>

--- src/utils/global/globalHooks.ts
+++ src/utils/global/globalHooks.ts
@@ -1,22 +1,18 @@
 import type { Application } from '../../app/Application';
 import type { Renderer } from '../../rendering/renderers/types';
 
-interface Globals
+declare global
 {
-    __PIXI_APP_INIT__?: (arg: Application | Renderer) => void;
-    __PIXI_RENDERER_INIT__?: (arg: Application | Renderer) => void;
+    var __PIXI_APP_INIT__:undefined | ((arg: Application | Renderer) => void);
+    var __PIXI_RENDERER_INIT__: undefined | ((arg: Application | Renderer) => void);
 }
 
 export function pixiRendererCreated(renderer: Renderer)
 {
-    const gThis = globalThis as unknown as Globals;
-
-    gThis.__PIXI_RENDERER_INIT__?.(renderer);
+    globalThis.__PIXI_RENDERER_INIT__?.(renderer);
 }
 
 export function pixiAppCreated(app: Application)
 {
-    const gThis = globalThis as unknown as Globals;
-
-    gThis.__PIXI_APP_INIT__?.(app);
+    globalThis.__PIXI_APP_INIT__?.(app);
 }
