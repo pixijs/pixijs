0d7e236716dfa3c3b95f018ebbc8def2864daf56
Zyie <24736175+Zyie@users.noreply.github.com>
2024-07-04 13:53:40 +0100

    fix lint

--- src/rendering/renderers/types.ts
+++ src/rendering/renderers/types.ts
@@ -10,7 +10,8 @@ export interface RendererOptions extends WebGLOptions, WebGPUOptions {}
 /* eslint-disable @typescript-eslint/indent */
 /**
  * Ids for the different render types.
- * The idea is that you can use bitwise operations to filter whether or not you want to do something in a certain render type.
+ * The idea is that you can use bitwise operations to filter whether or not you want to do something
+ * in a certain render type.
  * Filters for example can be compatible for both webGL or WebGPU but not compatible with canvas.
  * So internally if it works with both we set filter.compatibleRenderers = RendererType.WEBGL | RendererType.WEBGPU
  * if it only works with webgl we set filter.compatibleRenderers = RendererType.WEBGL
