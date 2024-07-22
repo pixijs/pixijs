1d9cd8bf4161dbad87ceaf17e0c1a20d3b7e2d14
Mat Groves <mat.groves@play.co>
2024-07-22 10:30:51 +0100

    fix trimmed bounds - added a test

--- src/utils/data/updateQuadBounds.ts
+++ src/utils/data/updateQuadBounds.ts
@@ -6,10 +6,25 @@ export function updateQuadBounds(
     bounds: BoundsData,
     anchor: ObservablePoint,
     texture: Texture,
-    padding: number
+    padding: number,
 )
 {
     const { width, height } = texture.orig;
+    const trim = texture.trim;
+
+    if (trim)
+    {
+        const sourceWidth = trim.width;
+        const sourceHeight = trim.height;
+
+        bounds.minX = (trim.x) - (anchor._x * width) - padding;
+        bounds.maxX = bounds.minX + sourceWidth;
+
+        bounds.minY = (trim.y) - (anchor._y * height) - padding;
+        bounds.maxY = bounds.minY + sourceHeight;
+
+        return;
+    }
 
     bounds.minX = (-anchor._x * width) - padding;
     bounds.maxX = bounds.minX + width;
