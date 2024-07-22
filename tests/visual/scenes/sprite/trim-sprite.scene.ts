34f496e5bc5ccda46ded98d897fa216dfeeb4988
Mat Groves <mat.groves@play.co>
2024-07-22 12:39:49 +0100

    lint

--- tests/visual/scenes/sprite/trim-sprite.scene.ts
+++ tests/visual/scenes/sprite/trim-sprite.scene.ts
@@ -1,5 +1,5 @@
-import { Texture } from '../../../../src';
 import { Assets } from '../../../../src/assets/Assets';
+import { Texture } from '../../../../src/rendering/renderers/shared/texture/Texture';
 import { Sprite } from '../../../../src/scene/sprite/Sprite';
 
 import type { Container } from '../../../../src/scene/container/Container';
@@ -7,7 +7,6 @@ import type { TestScene } from '../../types';
 
 export const scene: TestScene = {
     it: 'should render a trimmed sprite',
-    only: true,
     pixelMatch: 200,
     create: async (scene: Container) =>
     {
