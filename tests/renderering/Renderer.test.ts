45835df98117126f934bc0187488c42fd8efa2c6
dev7355608 <dev7355608@gmail.com>
2024-07-29 10:28:58 +0200

    feat: add resolution to resize event parameters (#10775)
    * feat: add ShaderSystem#maxTextures (#10713)
    
    * feat: add ShaderSystem#maxTextures
    
    * use gpu.device.limits.maxSampledTexturesPerShaderStage
    
    ---------
    
    Co-authored-by: Zyie <24736175+Zyie@users.noreply.github.com>
    
    * feat: reserve texture units for batching (#10703)
    
    * feat: reserve texture units for batching
    
    * maxTextures instead of reservedTextures
    
    ---------
    
    Co-authored-by: Zyie <24736175+Zyie@users.noreply.github.com>
    
    * feat: add 'min' and 'max' blend modes (#10684)
    
    * feat: add 'min' and 'max' blend modes
    
    * Add visual tests
    
    * Fix variable name
    
    * replace webgl2 test
    
    ---------
    
    Co-authored-by: Zyie <24736175+Zyie@users.noreply.github.com>
    
    * feat: add a renderable GC (#10681)
    
    * add a renderable GC
    
    * lint
    
    * oops
    
    * fix nineslice destroy
    
    * fix up PR
    
    * tweak times for tests
    
    * Update package.json
    
    * remove unneeded extension.add call in TextureGCSystem and RenderableGCSystem
    
    ---------
    
    Co-authored-by: Zyie <24736175+Zyie@users.noreply.github.com>
    
    * feat: add resolution to resize event
    
    ---------
    
    Co-authored-by: Zyie <24736175+Zyie@users.noreply.github.com>
    Co-authored-by: Mat Groves <mat@goodboydigital.com>

--- tests/renderering/Renderer.test.ts
+++ tests/renderering/Renderer.test.ts
@@ -10,10 +10,10 @@ describe('Renderer', () =>
         const spy = jest.fn();
 
         renderer.on('resize', spy);
-        renderer.resize(2, 4);
+        renderer.resize(2, 4, 3);
 
         expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith(2, 4);
+        expect(spy).toHaveBeenCalledWith(2, 4, 3);
 
         renderer.destroy();
     });
