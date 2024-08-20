bf88d27d78b2755cd28b010b53899eed7d24ff76
Mat Groves <mat.groves@play.co>
2024-08-20 16:10:24 +0100

    add dimension to blur

--- src/filters/defaults/blur/gpu/generateBlurProgram.ts
+++ src/filters/defaults/blur/gpu/generateBlurProgram.ts
@@ -17,11 +17,11 @@ export function generateBlurProgram(horizontal: boolean, kernelSize: number)
 
         if (horizontal)
         {
-            blurOutSource[i] = `filteredCord + vec2(${i - halfLength + 1} * strength, 0.0),`;
+            blurOutSource[i] = `filteredCord + vec2(${i - halfLength + 1} * pixelStrength, 0.0),`;
         }
         else
         {
-            blurOutSource[i] = `filteredCord + vec2(0.0, ${i - halfLength + 1} * strength),`;
+            blurOutSource[i] = `filteredCord + vec2(0.0, ${i - halfLength + 1} * pixelStrength),`;
         }
 
         const kernelIndex = i < halfLength ? i : (kernelSize - i - 1);
@@ -38,7 +38,8 @@ export function generateBlurProgram(horizontal: boolean, kernelSize: number)
         .replace('%blur-struct%', blurStruct)
         .replace('%blur-vertex-out%', blurOut)
         .replace('%blur-fragment-in%', blurStruct)
-        .replace('%blur-sampling%', blurSampling);
+        .replace('%blur-sampling%', blurSampling)
+        .replace('%dimension%', horizontal ? 'z' : 'w');
 
     return GpuProgram.from({
         vertex: {
