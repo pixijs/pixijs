15c7aa3747539fbb6660732dc315c2f82d2f3d37
Zyie <24736175+Zyie@users.noreply.github.com>
2024-10-02 15:24:42 +0100

    fix: text fill alpha affecting stroke alpha

---
+++ tests/visual/scenes/text/text-stroke-alpha.scene.ts
@@ -0,0 +1,29 @@
+import { Text } from '../../../../src/scene/text/Text';
+import { TextStyle } from '../../../../src/scene/text/TextStyle';
+import { BitmapText } from '../../../../src/scene/text-bitmap/BitmapText';
+
+import type { Container } from '../../../../src/scene/container/Container';
+import type { TestScene } from '../../types';
+
+export const scene: TestScene = {
+    it: 'should render stroke and fill alpha separately',
+    pixelMatch: 320,
+    create: async (scene: Container) =>
+    {
+        const style = new TextStyle({
+            fill: 'rgba(255, 0, 0, 0.25)',
+            fontSize: 64,
+            stroke: {
+                width: 4,
+            }
+        });
+
+        const text1 = new Text({ text: 'Hi', style });
+        const text2 = new BitmapText({ text: 'Hi', style });
+
+        text2.position.set(0, 64);
+
+        scene.addChild(text1);
+        scene.addChild(text2);
+    },
+};
