293a3b59ff468199214e1be49c68482f1446dffe
Mat Groves <mat.groves@play.co>
2024-11-14 15:11:41 +0000

    fix import

--- src/scene/particle-container/shared/Particle.ts
+++ src/scene/particle-container/shared/Particle.ts
@@ -1,6 +1,6 @@
-import { bgr2rgb } from '../../../bundle.browser';
 import { Color } from '../../../color/Color';
 import { Texture } from '../../../rendering/renderers/shared/texture/Texture';
+import { bgr2rgb } from '../../container/global/getGlobalTint';
 import { assignWithIgnore } from '../../container/utils/assignWithIgnore';
 
 import type { ColorSource } from '../../../color/Color';
