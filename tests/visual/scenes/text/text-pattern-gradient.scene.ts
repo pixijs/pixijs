38be6f51f21709b90606faf8774e571aaa511e83
Mat Groves <mat.groves@play.co>
2024-11-20 12:23:10 +0000

    add color stops, update tests

--- tests/visual/scenes/text/text-pattern-gradient.scene.ts
+++ tests/visual/scenes/text/text-pattern-gradient.scene.ts
@@ -11,10 +11,14 @@ export const scene: TestScene = {
     it: 'should render FillPattern and FillGradient in text stroke',
     create: async (scene: Container) =>
     {
-        const gradient = new FillGradient(0, 0, 1, 0)
-            .addColorStop(0, 0xff0000)
-            .addColorStop(0.5, 0x00ff00)
-            .addColorStop(1, 0x0000ff);
+        const gradient = new FillGradient({
+            colorStops: [
+                { offset: 0, color: 0xff0000 },
+                { offset: 0.5, color: 0x00ff00 },
+                { offset: 1, color: 0x0000ff },
+            ],
+        });
+
         const textGradient = new Text({
             text: 'PixiJS',
             style: {
