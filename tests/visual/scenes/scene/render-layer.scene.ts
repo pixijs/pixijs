44467c2ca971d0aac1eaae2829e09c133b4462e7
Mat Groves <mat.groves@play.co>
2024-11-26 14:03:09 +0000

    remove only

--- tests/visual/scenes/scene/render-layer.scene.ts
+++ tests/visual/scenes/scene/render-layer.scene.ts
@@ -6,7 +6,6 @@ import type { TestScene } from '../../types';
 
 export const scene: TestScene = {
     it: 'should render layers correctly',
-    only: true,
     create: async (scene: Container) =>
     {
         const graphicsBack = new Graphics().rect(0, 0, 100, 100).fill('red');
