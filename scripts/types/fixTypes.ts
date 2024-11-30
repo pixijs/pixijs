d1b5c6388a3a2ebf06eb4a8815f61cc508efe956
Matt Karl <matt@mattkarl.com>
2024-11-29 21:43:01 -0500

    Update fixTypes.ts

--- scripts/types/fixTypes.ts
+++ scripts/types/fixTypes.ts
@@ -20,14 +20,18 @@ import * as path from 'node:path';
  */
 function addMixinReferencePaths()
 {
-    const globPath = `${path.join(process.cwd(), './src')}/**/*.d.ts`;
+    const srcPath = path.resolve(__dirname, '../../src');
+    const globPath = `${srcPath}/**/*.d.ts`;
     const files = glob.sync(globPath);
+
+    // eslint-disable-next-line no-console
+    console.log('Adding reference paths to mixins:', globPath, files);
 
     const lines: string[] = [];
 
     files.forEach((file) =>
     {
-        const name = file.split(`${process.cwd()}/src`)[1];
+        const name = file.split(srcPath)[1];
 
         // eslint-disable-next-line no-console
         console.log('Adding reference path:', `/// <reference path=".${name}" />`);
@@ -35,7 +39,7 @@ function addMixinReferencePaths()
         lines.push(`/// <reference path=".${name}" />`);
     });
 
-    const filePath = path.join(process.cwd(), './lib/index.d.ts');
+    const filePath = path.resolve(__dirname, '../../lib/index.d.ts');
     const contents = fs.readFileSync(filePath, 'utf8');
     const updatedContents = `${lines.join('\n')}\n${contents}`;
 
@@ -45,13 +49,13 @@ function addMixinReferencePaths()
 /** Copy the Shaders.d.ts file to the lib folder This is needed for proper frag,vert,wgsl support in the types */
 function copyShaders()
 {
-    const src = path.join(process.cwd(), './types');
-    const lib = path.join(process.cwd(), './lib');
+    const src = path.resolve(__dirname, '../../types');
+    const lib = path.resolve(__dirname, '../../lib');
 
-    fs.copyFileSync(`${src}/Shaders.d.ts`, `${lib}/Shaders.d.ts`);
+    fs.copyFileSync(path.join(src, 'Shaders.d.ts'), path.join(lib, 'Shaders.d.ts'));
 
     // add this to the index.d.ts file
-    const filePath = path.join(process.cwd(), './lib/index.d.ts');
+    const filePath = path.join(lib, 'index.d.ts');
     const contents = fs.readFileSync(filePath, 'utf8');
 
     if (!contents.includes('/// <reference path="./Shaders.d.ts" />'))
