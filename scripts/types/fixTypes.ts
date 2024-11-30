c3d24a1463a8117ec59db57bb70b758c8afc582b
Matt Karl <matt@mattkarl.com>
2024-11-29 22:10:45 -0500

    cleanup, use minimal changes

--- scripts/types/fixTypes.ts
+++ scripts/types/fixTypes.ts
@@ -21,9 +21,8 @@ import * as path from 'node:path';
 function addMixinReferencePaths()
 {
     // Support windows paths, glob requires only forward slashes
-    const srcPath = path.resolve(__dirname, '../../src').replace(/\\/g, '/');
-    const globPath = `${srcPath}/**/*.d.ts`;
-    const files = glob.sync(globPath);
+    const srcPath = path.join(process.cwd(), 'src').replace(/\\/g, '/');
+    const files = glob.sync(`${srcPath}/**/*.d.ts`);
     const lines: string[] = [];
 
     files.forEach((file) =>
@@ -33,7 +32,7 @@ function addMixinReferencePaths()
         lines.push(`/// <reference path=".${name}" />`);
     });
 
-    const filePath = path.resolve(__dirname, '../../lib/index.d.ts');
+    const filePath = path.join(process.cwd(), './lib/index.d.ts');
     const contents = fs.readFileSync(filePath, 'utf8');
     const updatedContents = `${lines.join('\n')}\n${contents}`;
 
@@ -43,13 +42,13 @@ function addMixinReferencePaths()
 /** Copy the Shaders.d.ts file to the lib folder This is needed for proper frag,vert,wgsl support in the types */
 function copyShaders()
 {
-    const src = path.resolve(__dirname, '../../types');
-    const lib = path.resolve(__dirname, '../../lib');
+    const src = path.join(process.cwd(), './types');
+    const lib = path.join(process.cwd(), './lib');
 
-    fs.copyFileSync(path.join(src, 'Shaders.d.ts'), path.join(lib, 'Shaders.d.ts'));
+    fs.copyFileSync(`${src}/Shaders.d.ts`, `${lib}/Shaders.d.ts`);
 
     // add this to the index.d.ts file
-    const filePath = path.join(lib, 'index.d.ts');
+    const filePath = path.join(process.cwd(), './lib/index.d.ts');
     const contents = fs.readFileSync(filePath, 'utf8');
 
     if (!contents.includes('/// <reference path="./Shaders.d.ts" />'))
