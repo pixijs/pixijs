9e49f767098dd22eb672a1f12f9563bb10ebb336
Matt Karl <matt@mattkarl.com>
2024-12-03 23:46:24 -0500

    chore: Version generated barrel files

---
+++ src/accessibility/index.ts
@@ -0,0 +1,3 @@
+// Auto-generated code, do not edit manually
+export * from './AccessibilitySystem';
+export * from './accessibilityTarget';
---
+++ src/spritesheet/index.ts
@@ -0,0 +1,3 @@
+// Auto-generated code, do not edit manually
+export * from './Spritesheet';
+export * from './spritesheetAsset';
