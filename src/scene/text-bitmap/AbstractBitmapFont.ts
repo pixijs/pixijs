4497cb3dec6da07ffad0030818a2e81544ce9913
Michael Naumov <mnaoumov@gmail.com>
2025-01-12 03:00:18 +0200

    Fix compilation

--- src/scene/text-bitmap/AbstractBitmapFont.ts
+++ src/scene/text-bitmap/AbstractBitmapFont.ts
@@ -70,7 +70,7 @@ export interface BitmapFontData
         type: 'sdf' | 'msdf' | 'none';
         /** Range of the distance field in pixels */
         range: number;
-    };
+    } | undefined;
 }
 
 interface BitmapFontEvents<Type>
