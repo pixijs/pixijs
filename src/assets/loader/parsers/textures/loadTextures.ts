5864258c01231d44a411a84ea9943e2e9564436c
y-kaplun <y-kaplun@redspell.ru>
2025-01-20 13:31:45 +0300

    loadTextures.load: Image.onerror > Promise.reject

--- src/assets/loader/parsers/textures/loadTextures.ts
+++ src/assets/loader/parsers/textures/loadTextures.ts
@@ -135,7 +135,7 @@ export const loadTextures: LoaderParser<Texture, TextureSourceOptions, LoadTextu
         }
         else
         {
-            src = await new Promise((resolve) =>
+            src = await new Promise((resolve, reject) =>
             {
                 src = new Image();
                 src.crossOrigin = this.config.crossOrigin;
@@ -147,11 +147,11 @@ export const loadTextures: LoaderParser<Texture, TextureSourceOptions, LoadTextu
                 }
                 else
                 {
-                    src.onerror = src.onload = (): void =>
+                    src.onload = (): void =>
                     {
-                        src.onerror = src.onload = null;
                         resolve(src);
                     };
+                    src.onerror = reject;
                 }
             });
         }
