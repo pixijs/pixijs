0869f873fe2734694846dab07d320cc08b60f240
Trezy <tre@trezy.com>
2025-01-30 06:54:33 -0600

    feat: add `autoPlay` to `AnimatedSprite` (#11243)
    * test: add tests for new `AnimatedSprite` constructor options
    
    Signed-off-by: Trezy <tre@trezy.com>
    
    * feat: add `autoPlay` constructor option to `AnimatedSprite`
    
    Signed-off-by: Trezy <tre@trezy.com>
    
    ---------
    
    Signed-off-by: Trezy <tre@trezy.com>

--- src/scene/sprite-animated/AnimatedSprite.ts
+++ src/scene/sprite-animated/AnimatedSprite.ts
@@ -16,6 +16,8 @@ export interface AnimatedSpriteOptions extends Omit<SpriteOptions, 'texture'>
 {
     /** The speed that the AnimatedSprite will play at. Higher is faster, lower is slower. */
     animationSpeed?: number;
+    /** Whether to start the animation immediately on creation. */
+    autoPlay?: boolean;
     /** Whether to use Ticker.shared to auto update animation time. */
     autoUpdate?: boolean;
     /** Whether or not the animate sprite repeats after playing. */
@@ -165,6 +167,7 @@ export class AnimatedSprite extends Sprite
 
         const {
             animationSpeed = 1,
+            autoPlay = false,
             autoUpdate = true,
             loop = true,
             onComplete = null,
@@ -199,6 +202,11 @@ export class AnimatedSprite extends Sprite
         this._previousFrame = null;
 
         this.textures = textures;
+
+        if (autoPlay)
+        {
+            this.play();
+        }
     }
 
     /** Stops the AnimatedSprite. */
@@ -362,7 +370,7 @@ export class AnimatedSprite extends Sprite
 
         this.texture = this._textures[currentFrame];
 
-        if (this.updateAnchor)
+        if (this.updateAnchor && this.texture.defaultAnchor)
         {
             this.anchor.copyFrom(this.texture.defaultAnchor);
         }
