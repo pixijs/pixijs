e4b8975bf2ec6639168ff2a0dbbc5971a1431076
Mat Groves <mat.groves@play.co>
2025-02-07 17:33:42 +0000

    Update CanvasTextSystem.ts

--- src/scene/text/canvas/CanvasTextSystem.ts
+++ src/scene/text/canvas/CanvasTextSystem.ts
@@ -173,6 +173,17 @@ export class CanvasTextSystem implements System
         this._activeTextures[textKey].usageCount++;
     }
 
+    public returnTexture(texture: Texture)
+    {
+        const source = texture.source;
+
+        source.resource = null;
+        source.uploadMethodId = 'unknown';
+        source.alphaMode = 'no-premultiply-alpha';
+
+        TexturePool.returnTexture(texture);
+    }
+
     public decreaseReferenceCount(textKey: string)
     {
         const activeTexture = this._activeTextures[textKey];
@@ -182,13 +193,8 @@ export class CanvasTextSystem implements System
         if (activeTexture.usageCount === 0)
         {
             CanvasPool.returnCanvasAndContext(activeTexture.canvasAndContext);
-            TexturePool.returnTexture(activeTexture.texture);
 
-            const source = activeTexture.texture.source;
-
-            source.resource = null;
-            source.uploadMethodId = 'unknown';
-            source.alphaMode = 'no-premultiply-alpha';
+            this.returnTexture(activeTexture.texture);
 
             this._activeTextures[textKey] = null;
         }
