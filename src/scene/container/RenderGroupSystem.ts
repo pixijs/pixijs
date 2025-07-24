bdb092467183a4143e28d86179b0e45635e8fe39
Ilia Malanin <loremipsum.inside@gmail.com>
2025-07-24 21:30:06 +0200

    refactor: improve TextureStyle reset on RenderGroup texture update

--- src/scene/container/RenderGroupSystem.ts
+++ src/scene/container/RenderGroupSystem.ts
@@ -1,6 +1,7 @@
 import { ExtensionType } from '../../extensions/Extensions';
 import { Matrix } from '../../maths/matrix/Matrix';
 import { TexturePool } from '../../rendering/renderers/shared/texture/TexturePool';
+import { TextureStyle } from '../../rendering/renderers/shared/texture/TextureStyle';
 import { Bounds } from './bounds/Bounds';
 import { clearList } from './utils/clearList';
 import { executeInstructions } from './utils/executeInstructions';
@@ -133,7 +134,7 @@ export class RenderGroupSystem implements System
 
                 if (renderGroup.texture)
                 {
-                    TexturePool.returnTexture(renderGroup.texture);
+                    TexturePool.returnTexture(renderGroup.texture, true);
                 }
 
                 const renderer = this._renderer;
@@ -147,7 +148,7 @@ export class RenderGroupSystem implements System
                     antialias
                 );
 
-                texture._source.scaleMode = scaleMode;
+                texture._source.style = new TextureStyle({ scaleMode });
                 renderGroup.texture = texture;
                 renderGroup._textureBounds ||= new Bounds();
                 renderGroup._textureBounds.copyFrom(bounds);
@@ -163,7 +164,7 @@ export class RenderGroupSystem implements System
         }
         else if (renderGroup.texture)
         {
-            TexturePool.returnTexture(renderGroup.texture);
+            TexturePool.returnTexture(renderGroup.texture, true);
             renderGroup.texture = null;
         }
     }
