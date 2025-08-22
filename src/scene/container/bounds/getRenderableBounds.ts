55c33799e28f10694912496cb20a08a3c6ddff2a
Ilia Malanin <loremipsum.inside@gmail.com>
2025-08-22 17:08:57 +0200

    fix: Advanced blending modes within render groups, cached as texture render groups (#11607)
    * fix: fixes global bounds calculation by removing a redundant transform application
    
    * fix: correct bounds calculation for cached render groups and nested transformations
    
    * fix: fixes logic for texture update checks in cached render groups.
    
    * refactor: removed unnecessary reassignment
    
    * fix: ensure parent cache textures are updated when a child texture is modified
    
    * fix: prevent texture unbinding issue in FilterSystem by maintaining proper bind order
    
    * fix: correct filter stack iteration when searching the previous filter data.
    
    * feat: add blend mode stack management and support for RenderGroup blending
    
    * fix: ensure proper blend mode stack handling in ViewContainer rendering
    
    * fix: manage blend mode stack correctly in RenderGroup rendering
    
    * fix: ensure a proper offset during the filter application for RenderGroup cached as texture
    
    * reverted: apply world transformation to bounds in getFastGlobalBoundsMixin
    
    * fix: correct blend mode stack handling and logic in BlendModePipe
    
    * fix: remove redundant blend mode stack operations for non-cached render groups
    
    * fix: retrieve the correct transform for cached RenderGroup
    
    * feat: add visual tests for advanced blend modes and cached containers
    
    * fix: reverted intermediate binding of the render target instead of pushing it

--- src/scene/container/bounds/getRenderableBounds.ts
+++ src/scene/container/bounds/getRenderableBounds.ts
@@ -1,5 +1,14 @@
+import { Matrix } from '../../../maths';
+
 import type { Renderable } from '../../../rendering/renderers/shared/Renderable';
 import type { Bounds } from './Bounds';
+
+/**
+ * This matrix is used for calculations of the bounds for renderables placed inside cacheAsTexture render groups.
+ * @ignore
+ * @internal
+ */
+const tempProjectionMatrix: Matrix = new Matrix();
 
 /**
  * @param renderables
@@ -14,7 +23,7 @@ export function getGlobalRenderableBounds(renderables: Renderable[], bounds: Bou
     // this is a performance hack :D
     // so we need to restore the matrix after we are done
 
-    const tempMatrix = bounds.matrix;
+    const actualMatrix = bounds.matrix;
 
     for (let i = 0; i < renderables.length; i++)
     {
@@ -25,11 +34,28 @@ export function getGlobalRenderableBounds(renderables: Renderable[], bounds: Bou
             continue;
         }
 
-        bounds.matrix = renderable.worldTransform;
+        const renderGroup = renderable.renderGroup ?? renderable.parentRenderGroup;
+
+        if (renderGroup?.isCachedAsTexture)
+        {
+            bounds.matrix = tempProjectionMatrix.copyFrom(renderGroup.textureOffsetInverseTransform)
+                .append(renderable.worldTransform);
+        }
+        else if (renderGroup?._parentCacheAsTextureRenderGroup)
+        {
+            bounds.matrix = tempProjectionMatrix
+                .copyFrom(renderGroup._parentCacheAsTextureRenderGroup.inverseWorldTransform)
+                .append(renderable.groupTransform);
+        }
+        else
+        {
+            bounds.matrix = renderable.worldTransform;
+        }
+
         bounds.addBounds(renderable.bounds);
     }
 
-    bounds.matrix = tempMatrix;
+    bounds.matrix = actualMatrix;
 
     return bounds;
 }
