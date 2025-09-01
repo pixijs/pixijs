3525c434cf03ff54c60ac045eb80e0fe59ec9cbe
Ilia Malanin <loremipsum.inside@gmail.com>
2025-09-01 10:02:59 +0200

    fix: restore `buildGeometryFromPath` functionality (#11623)
    * fix: `buildGeometryFromPath` utility restored
    
    * chore: export `buildGeometryFromPath` utility
    
    * test: add visual test for mesh geometry built from a custom path
    
    * refactor: replace an internal `buildMap` object with `shapeBuilders` for geometry construction

---
+++ tests/visual/scenes/mesh/geometry-from-path.scene.ts
@@ -0,0 +1,22 @@
+import { Texture } from '~/rendering';
+import { buildGeometryFromPath, GraphicsPath, Mesh } from '~/scene';
+
+import type { TestScene } from '../../types';
+import type { Container } from '~/scene';
+
+export const scene: TestScene = {
+    it: 'should render mesh with geometry build from custom path correctly',
+    create: async (scene: Container) =>
+    {
+        const path = new GraphicsPath()
+            .rect(0, 0, 64, 64)
+            .rect(64, 64, 64, 64);
+
+        const mesh = new Mesh({
+            geometry: buildGeometryFromPath({ path }),
+            texture: Texture.WHITE
+        });
+
+        scene.addChild(mesh);
+    }
+};
