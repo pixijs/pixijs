5e3c6cf93b49583828dca9914456a31ba7ab51b1
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-10-10 10:28:12 -0400

    fix mistakes

--- src/filters/defaults/blur/BlurFilter.ts
+++ src/filters/defaults/blur/BlurFilter.ts
@@ -80,7 +80,7 @@ export interface BlurFilterOptions extends FilterOptions
 }
 
 /**
- * Default Options for BlurFilterPass
+ * Default Options for BlurFilter
  * @category filters
  * @internal
  */
@@ -181,7 +181,7 @@ export class BlurFilter extends Filter
      */
     constructor(options?: BlurFilterOptions);
     /** @deprecated since 8.0.0 */
-    constructor(strength?: number, quality?: number, resolution?: number, kernelSize?: number);
+    constructor(strength?: number, quality?: number, resolution?: number | null, kernelSize?: number);
     constructor(...args: [BlurFilterOptions?] | [number?, number?, (number | null)?, number?])
     {
         let options = args[0] ?? {};
