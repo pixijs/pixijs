e44493fd0b1cb3f524d87c70b12cdd3114729cab
Giorgi Maziashvili <giorgim@redberry.ge>
2025-10-17 04:27:11 +0400

    fix: SplitBitmapText requires "fill: 'white'"

--- src/scene/text-split/SplitBitmapText.ts
+++ src/scene/text-split/SplitBitmapText.ts
@@ -170,7 +170,7 @@ export class SplitBitmapText extends AbstractSplitText<BitmapText>
             ...config,
         };
 
-        completeOptions.style ??= completeOptions.style || {};
+        completeOptions.style ??= {};
         completeOptions.style.fill ??= 0xffffff;
 
         super(completeOptions);
