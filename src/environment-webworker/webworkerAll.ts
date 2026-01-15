73d79d61fc26a0cf636ebeb9b843cb20ada5cad3
Zyie <24736175+Zyie@users.noreply.github.com>
2026-01-15 18:37:29 +0000

    fix: improves tree-shaking by optimizing module imports
    Optimizes the module import structure to enable better tree-shaking and reduce bundle size. Specifically:
    
    - Moves module-level initialization imports to individual component files, allowing unused modules to be excluded from the final bundle
    - Streamlines the `browserAll.ts` and `webworkerAll.ts` files to only import necessary modules
    - Dynamically import `BitmapFont` to avoid text being bundled
    
    This change helps improve the tree-shaking capabilities of the library, resulting in smaller bundle sizes for end-users.

--- src/environment-webworker/webworkerAll.ts
+++ src/environment-webworker/webworkerAll.ts
@@ -1,12 +1,3 @@
-import '../app/init';
 import '../spritesheet/init';
 import '../rendering/init';
-import '../scene/graphics/init';
-import '../scene/mesh/init';
-import '../scene/particle-container/init';
-import '../scene/text/init';
-import '../scene/text-bitmap/init';
-import '../scene/text-html/init';
-import '../scene/sprite-tiling/init';
-import '../scene/sprite-nine-slice/init';
 import '../filters/init';
